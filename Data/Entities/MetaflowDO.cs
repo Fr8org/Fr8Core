e4cc696f1225fae85f964c4297113b27f46b8112
alexed <alex@edelstein.org>
2015-07-10 13:23:37 -0700

    add metaflow

---
+++ Data/Entities/MetaflowDO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities
+{
+    public class MetaflowDO : BaseDO
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+
+    }
+}
