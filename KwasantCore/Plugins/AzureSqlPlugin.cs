1c336e9a466d29e6121e946670b58190494492e7
Alex Edelstein <alex@leaseitkeepit.com>
2015-07-11 16:23:57 -0700

    created Plugin and AzureSQLPlugin classes

---
+++ KwasantCore/Plugins/AzureSqlPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KwasantCore.Plugins
+{
+    public class AzureSqlPlugin : Plugin
+    {
+        public string command_sqlWrite ()
+        {
+             
+        }
+    }
+}
