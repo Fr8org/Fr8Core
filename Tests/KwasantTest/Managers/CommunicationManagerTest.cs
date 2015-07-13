9f846bc9f8aa7662319edcf7df8652c92d87256b
alexed <alex@edelstein.org>
2015-07-13 13:59:13 -0700

    removed obsolete tests

--- Tests/KwasantTest/Managers/CommunicationManagerTest.cs
+++ Tests/KwasantTest/Managers/CommunicationManagerTest.cs
@@ -21,48 +21,6 @@ namespace KwasantTest.Managers
             StructureMapBootStrapper.ConfigureDependencies(StructureMapBootStrapper.DependencyType.TEST);
         }
 
-        [Test]
-        public void CanGenetrateBRNeedsProceessingEmail()
-        {
-            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
-            {
-                var fixture = new FixtureData(uow);
-              //  var testBookingRequest = fixture.TestBookingRequest1();
-              //  uow.BookingRequestRepository.Add(testBookingRequest);
-                uow.SaveChanges();
-
-                var configRepository = ObjectFactory.GetInstance<IConfigRepository>();
-
-                var communicationManager = new CommunicationManager(configRepository, new EmailAddress(configRepository));
-              //  communicationManager.BookingRequestNeedsProcessing(testBookingRequest.Id);
-
-                var emailDO = uow.EmailRepository.GetQuery().Where(e => e.Subject == "BookingRequest Needs Processing");
-                Assert.AreEqual("BookingRequest Needs Processing <br/>Subject : Booking request subject", emailDO.First().HTMLText);
-                Assert.AreEqual(1, emailDO.Count());
-                Assert.AreEqual(1, uow.EnvelopeRepository.GetQuery().Where(e => e.EmailID == emailDO.First().Id).Count());
-            }
-        }
-
-        [Test]
-        public void FailsToGenetrateBRNeedsProceessingEmail() 
-        {
-            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
-            {
-                var fixture = new FixtureData(uow);
-              //  var testBookingRequest = fixture.TestBookingRequest1();
-               // uow.BookingRequestRepository.Add(testBookingRequest);
-                uow.SaveChanges();
-
-                var configRepository = ObjectFactory.GetInstance<IConfigRepository>();
-                configRepository.Set("EmailAddress_BrNotify", "");
-
-                var communicationManager = new CommunicationManager(configRepository, new EmailAddress(configRepository));
-
-                Assert.Throws<Exception>(() =>
-                {
-            //        communicationManager.BookingRequestNeedsProcessing(testBookingRequest.Id);
-                });
-            }
-        }
+    
     }
 }
