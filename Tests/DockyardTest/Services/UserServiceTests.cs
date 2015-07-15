9c9d06ebcb600d4325cc6637b6367122e21da2bb
Dmytro Sobornykov <d1984v@gmail.com>
2015-07-15 14:22:31 +0300

    renamed KwasantTest to DockyardTes

---
+++ Tests/DockyardTest/Services/UserServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entities;
+using Data.Interfaces;
+using Core.Services;
+using Core.StructureMap;
+using DockyardTest.Fixtures;
+using NUnit.Framework;
+using StructureMap;
+
+namespace DockyardTest.Services
+{
+    class UserServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            StructureMapBootStrapper.ConfigureDependencies(StructureMapBootStrapper.DependencyType.TEST);
+        }
+    }
+}
