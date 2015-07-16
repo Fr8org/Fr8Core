907b23c738ed43b0c14db5df615ca6854783ebe2
Aleksei Avrutin <alexavrutin@gmail.com>
2015-07-16 03:05:13 +0200

    nUnit upgraded for test projects, sample Web API methods with tests added.

---
+++ pluginAzureSqlServerTests/SampleControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using KwasantTest;
+using NUnit.Framework;
+using System;
+using pluginAzureSqlServer;
+using System.Collections.Generic;
+
+namespace pluginAzureSqlServerTests
+{
+    [TestFixture]
+    public class SampleControllerTest : BaseTest
+    {
+        [Test]
+        [Category("Plugin.AzureSqlServer")]
+        public void SampleController_ReturnsTestDataOnGetRequest()
+        {
+            //Arrange
+            SampleController controller = new SampleController();
+
+            //Act 
+            IEnumerable<string> data = controller.Get();
+
+            //Assert
+            IEnumerator<string> e = data.GetEnumerator();
+            e.MoveNext();
+            Assert.AreEqual("value1", e.Current);
+            e.MoveNext();
+            Assert.AreEqual("value2", e.Current);
+        }
+    }
+}
