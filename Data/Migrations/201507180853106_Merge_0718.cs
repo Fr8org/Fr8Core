f9e4bf17fe2603bbec3bd1e93d56fec749e47464
Max Kostyrkin <mkostyrkin@gmail.com>
2015-07-18 11:54:18 +0300

    Merge migrations

---
+++ Data/Migrations/201507180853106_Merge_0718.cs
@@ -0,0 +1,16 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Merge_0718 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
