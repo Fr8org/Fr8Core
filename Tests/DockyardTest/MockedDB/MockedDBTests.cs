c7bbd7aa9e7ee62d80d4c9c5ca8bbf2fd1ef6c09
alexed <alex@edelstein.org>
2015-07-18 10:32:09 -0700

    uncommented a few useful tests

--- Tests/DockyardTest/MockedDB/MockedDBTests.cs
+++ Tests/DockyardTest/MockedDB/MockedDBTests.cs
@@ -16,33 +16,33 @@ namespace DockyardTest.MockedDB
     public class MockedDBTests : BaseTest
     {
         //This test is to ensure our mocking properly distinguishes between saved and local DbSets (to mimic EF behaviour)
-        [Test, Ignore]
+        [Test]
         public void TestDBMocking()
         {
-            //using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
-            //{
-            //    var brOne = new FixtureData(uow).TestBookingRequest1();
-            //    brOne.Id = 1;
-            //    brOne.Customer = uow.UserRepository.GetOrCreateUser("tempuser@gmail.com");
-            //    uow.BookingRequestRepository.Add(brOne);
+            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
+            {
+                var curUser = new FixtureData(uow).TestUser1();
+                curUser.Id = "1";
+               
+                uow.UserRepository.Add(curUser);
 
-            //    using (var subUow = ObjectFactory.GetInstance<IUnitOfWork>())
-            //    {
-            //        Assert.AreEqual(0, subUow.BookingRequestRepository.GetQuery().Count());
-            //        Assert.AreEqual(0, subUow.BookingRequestRepository.DBSet.Local.Count());
-            //    }
+                using (var subUow = ObjectFactory.GetInstance<IUnitOfWork>())
+                {
+                    Assert.AreEqual(0, subUow.UserRepository.GetQuery().Count());
+                    Assert.AreEqual(0, subUow.UserRepository.DBSet.Local.Count());
+                }
 
-            //    Assert.AreEqual(0, uow.BookingRequestRepository.GetQuery().Count());
-            //    Assert.AreEqual(1, uow.BookingRequestRepository.DBSet.Local.Count());
+                Assert.AreEqual(0, uow.UserRepository.GetQuery().Count());
+                Assert.AreEqual(1, uow.UserRepository.DBSet.Local.Count());
 
-            //    uow.SaveChanges();
+                uow.SaveChanges();
 
-            //    using (var subUow = ObjectFactory.GetInstance<IUnitOfWork>())
-            //    {
-            //        Assert.AreEqual(1, subUow.BookingRequestRepository.GetQuery().Count());
-            //        Assert.AreEqual(0, subUow.BookingRequestRepository.DBSet.Local.Count());
-            //    }
-            //}
+                using (var subUow = ObjectFactory.GetInstance<IUnitOfWork>())
+                {
+                    Assert.AreEqual(1, subUow.UserRepository.GetQuery().Count());
+                    Assert.AreEqual(0, subUow.UserRepository.DBSet.Local.Count());
+                }
+            }
         }
 
         [Test]
@@ -52,15 +52,15 @@ namespace DockyardTest.MockedDB
             //{
             //    var user = new UserDO();
             //    user.State = UserState.Active;
-            //    var brOne = new FixtureData(uow).TestBookingRequest1();
-            //    brOne.Id = 1;
-            //    brOne.CustomerID = user.Id;
-            //    uow.BookingRequestRepository.Add(brOne);
+            //    var curUser = new FixtureData(uow).TestBookingRequest1();
+            //    curUser.Id = 1;
+            //    curUser.CustomerID = user.Id;
+            //    uow.UserRepository.Add(curUser);
             //    uow.UserRepository.Add(user);
 
             //    uow.SaveChanges();
 
-            //    Assert.NotNull(brOne.Customer);
+            //    Assert.NotNull(curUser.Customer);
             //}
         }
 
@@ -101,20 +101,20 @@ namespace DockyardTest.MockedDB
         public void AssertAllTestsImplementBaseTest()
         {
             var failedTypes = new List<Type>();
-            //    foreach (var testClass in GetType().Assembly.GetTypes().Where(t => t.GetCustomAttributes<TestFixtureAttribute>().Any()))
-            //    {
-            //        if (testClass != typeof(BaseTest) && !testClass.IsSubclassOf(typeof(BaseTest)))
-            //            failedTypes.Add(testClass);
-            //    }
-            //    var exceptionMessages = new List<String>();
-            //    foreach (var failedType in failedTypes)
-            //    {
-            //        var testClassName = failedType.Name;
-            //        exceptionMessages.Add(testClassName + " must implement 'BaseTest'");
-            //    }
-            //    if (exceptionMessages.Any())
-            //        Assert.Fail(String.Join(Environment.NewLine, exceptionMessages));
-            //}
+                foreach (var testClass in GetType().Assembly.GetTypes().Where(t => t.GetCustomAttributes<TestFixtureAttribute>().Any()))
+                {
+                    if (testClass != typeof(BaseTest) && !testClass.IsSubclassOf(typeof(BaseTest)))
+                        failedTypes.Add(testClass);
+                }
+                var exceptionMessages = new List<String>();
+                foreach (var failedType in failedTypes)
+                {
+                    var testClassName = failedType.Name;
+                    exceptionMessages.Add(testClassName + " must implement 'BaseTest'");
+                }
+                if (exceptionMessages.Any())
+                    Assert.Fail(String.Join(Environment.NewLine, exceptionMessages));
+            }
 
             //[Test, ExpectedException(ExpectedMessage = "Foreign row does not exist.\nValue '0' on 'NegotiationDO.NegotiationState' pointing to '_NegotiationStateTemplate.Id'")]
             //public void TestForeignKeyEnforced()
@@ -132,4 +132,4 @@ namespace DockyardTest.MockedDB
             //}
         }
     }
-}
+
