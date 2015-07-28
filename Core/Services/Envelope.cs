ed3fb56354fff2e1ce20deecd6fbb77795289237
Nakro <power.cpp@outlook.com>
2015-07-28 22:38:26 +0300

    First part of do-762 (still working on it. - Not tested yet.)

--- Core/Services/Envelope.cs
+++ Core/Services/Envelope.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-using Data.Entities;
+using Utilities;
 
 namespace Core.Services
 {
-    public class Envelope
+    public interface IEnvelope
+    {
+        List<EnvelopeData> GetEnvelopeData(DocuSign.Integrations.Client.Envelope docusignEnvelope);
+    }
+
+    public class Envelope : IEnvelope
     {
         private string _baseUrl;
         
@@ -14,31 +18,20 @@ namespace Core.Services
 
         }
 
-        public static List<EnvelopeData> GetEnvelopeData(EnvelopeDO envelopeDo)
+        public List<EnvelopeData> GetEnvelopeData(DocuSign.Integrations.Client.Envelope docusignEnvelope)
         {
             //TODO orkan: when Dockyard retrieves this envelope data, it immediately normalizes it into a set of EnvelopeData objects. 
             //Each EnvelopeData row is essentially a specific DocuSign "Tab". By normalizing in this way, 
             //other parts of the system can make neat queries against this in-memory table. 
             //As a security policy, we do not intend to persist these DocumentData rows; we will generate them fresh from the retrieved DocuSign data each time we need them.
 
-            //don't failed the tests. This method will be reviewed.
-            return new List<EnvelopeData>();
+            List<EnvelopeData> envelopeDatas = new List<EnvelopeData>();
+
+            //Each EnvelopeData row is essentially a specific DocuSign "Tab".
+
+            return envelopeDatas;
         }
     }
 
     //TODO orkan is asking: where do I need to move EnvelopeData?
-    public class EnvelopeData
-    {
-        public string Name { get; set; }
-
-        public string TabId { get; set; }
-
-        public string RecipientId { get; set; }
-
-        public string EnvelopeId { get; set; }
-
-        public string Value { get; set; }
-
-        public string DocumentName { get; set; }
-    }
 }
--- Tests/DockyardTest/Entities/EnvelopeTests.cs
+++ Tests/DockyardTest/Entities/EnvelopeTests.cs
@@ -1,60 +1,145 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 using Core.Services;
 
 using Data.Entities;
 using Data.Interfaces;
+
+using DocuSign.Integrations.Client;
+
 using NUnit.Framework;
+
 using StructureMap;
+
+using Utilities;
+
+using Account = DocuSign.Integrations.Client.Account;
+using Envelope = DocuSign.Integrations.Client.Envelope;
 
 namespace DockyardTest.Entities
 {
-	[ TestFixture ]
-	public class EnvelopeTests: BaseTest
-	{
-		[ Test ]
-		[ Category( "Envelope" ) ]
-		public void Envelope_Change_Status()
-		{
-			const EnvelopeDO.EnvelopeState newStatus = EnvelopeDO.EnvelopeState.Created;
-			const EnvelopeDO.EnvelopeState updatedStatus = EnvelopeDO.EnvelopeState.Delivered;
-			using( var uow = ObjectFactory.GetInstance< IUnitOfWork >() )
-			{
-				uow.EnvelopeRepository.Add( new EnvelopeDO { Id = 1, Status = newStatus, DocusignEnvelopeId = "23" } );
-				uow.SaveChanges();
+    [TestFixture]
+    public class EnvelopeTests : BaseTest
+    {
+        [Test]
+        [Category("Envelope")]
+        public void Envelope_Change_Status()
+        {
+            const EnvelopeDO.EnvelopeState newStatus = EnvelopeDO.EnvelopeState.Created;
+            const EnvelopeDO.EnvelopeState updatedStatus = EnvelopeDO.EnvelopeState.Delivered;
+            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
+            {
+                uow.EnvelopeRepository.Add(new EnvelopeDO { Id = 1, Status = newStatus, DocusignEnvelopeId = "23" });
+                uow.SaveChanges();
 
-				var createdEnvelope = uow.EnvelopeRepository.GetQuery().FirstOrDefault();
-				Assert.NotNull( createdEnvelope );
-				Assert.AreEqual( newStatus, createdEnvelope.Status );
+                var createdEnvelope = uow.EnvelopeRepository.GetQuery().FirstOrDefault();
+                Assert.NotNull(createdEnvelope);
+                Assert.AreEqual(newStatus, createdEnvelope.Status);
 
-				createdEnvelope.Status = updatedStatus;
-				uow.SaveChanges();
+                createdEnvelope.Status = updatedStatus;
+                uow.SaveChanges();
 
-				var updatedEnvelope = uow.EnvelopeRepository.GetQuery().FirstOrDefault();
-				Assert.NotNull( updatedEnvelope );
-				Assert.AreEqual( updatedStatus, updatedEnvelope.Status );
-			}
-		}
+                var updatedEnvelope = uow.EnvelopeRepository.GetQuery().FirstOrDefault();
+                Assert.NotNull(updatedEnvelope);
+                Assert.AreEqual(updatedStatus, updatedEnvelope.Status);
+            }
+        }
 
         [Test]
         [Category("Envelope")]
         public void Envelope_Can_Normalize_EnvelopeData()
-	    {
-            //TODO orkan:
-            /*
-             * a) programmatically create an Envelope in DocuSign in the developer sandbox account
-             * b) populate it with some Tabs with values. Example "Amount" is a text field with value "45".
-             * c) call Envelope#GetEnvelopeData
-             * d) Verify that the returned List matches expectations.
-             */
+        {
+            Account account = LoginDocusign();
+            Envelope envelope = CreateAndFillEnvelope(account);
 
-            const EnvelopeDO.EnvelopeState newStatus = EnvelopeDO.EnvelopeState.Created;
-            EnvelopeDO envelopeDo = new EnvelopeDO {Id = 1, Status = newStatus, DocusignEnvelopeId = "23"};
+            IEnvelope envelopeService = new Core.Services.Envelope();
+            List<EnvelopeData> envelopeDatas = envelopeService.GetEnvelopeData(envelope);
 
-            List<EnvelopeData> envelopeDatas = Envelope.GetEnvelopeData(envelopeDo);
             Assert.IsNotNull(envelopeDatas);
-            Assert.IsTrue(envelopeDatas.Count > 0);
-	    }
-	}
+            //Assert.IsTrue(envelopeDatas.Count > 0); //Todo orkan: remove back when you completed the EnvelopeService.
+        }
+
+        #region private methods.
+        /// <summary>
+        /// Programmatically create an Envelope in DocuSign in the developer sandbox account.
+        /// </summary>
+        /// <returns>Logged account object ( Docusign.Integrations.Client.Account ).</returns>
+        private static Account LoginDocusign()
+        {
+            // configure application's integrator key and webservice url
+            RestSettings.Instance.IntegratorKey = "TEST-34d0ac9c-89e7-4acc-bc1d-24d6cfb867f2";
+            RestSettings.Instance.DocuSignAddress = "http://demo.docusign.net";
+            RestSettings.Instance.WebServiceUrl = RestSettings.Instance.DocuSignAddress + "/restapi/v2";
+
+            // credentials for sending account
+            Account account = new Account
+                              {
+                                  Email = "5c07c74c-688e-436b-abe4-39b0493f7cb6",
+                                  Password = "q.12345R"
+                              };
+
+            // make the Login API call
+            bool result = account.Login();
+            Assert.IsTrue(result, "We login to docusign. If this is false, check your credential info and integrator key.");
+            return account;
+        }
+
+        /// <summary>
+        /// Create envelope with current account info and fill envelope with some gibberish data, and return it back.
+        /// </summary>
+        /// <param name="account">Docusign Account that includes login info.</param>
+        /// <returns>Envelope of Docusign.</returns>
+        private static Envelope CreateAndFillEnvelope(Account account)
+        {
+            // create envelope object and assign login info
+            Envelope envelope = new Envelope
+                                {
+                                    // assign account info from above
+                                    Login = account,
+                                    // "sent" to send immediately, "created" to save envelope as draft
+                                    Status = "sent"
+                                };
+
+            string fullPathToExampleDocument = Path.Combine(Environment.CurrentDirectory, "App_Data", "docusign_examplephoto.png");
+
+            // create a new DocuSign envelope (i.e. server side)
+            envelope.Create(fullPathToExampleDocument);
+
+            envelope.AddRecipients(new Recipients
+                                   {
+                                       carbonCopies = new[]
+	                                                  {
+	                                                      new Signer
+	                                                      {
+	                                                          email = "hello@orkan.com",
+	                                                          name = "Orkan ARIKAN"
+	                                                      }
+	                                                  }
+                                   });
+
+            List<TextTab> textTabs = new List<TextTab>
+	                                 {
+	                                     new TextTab
+	                                     {
+	                                         xPosition = 200,
+	                                         yPosition = 200,
+	                                         name = "Amount",
+	                                         value = "40"
+	                                     }
+	                                 };
+
+            //populate it with some Tabs with values. Example "Amount" is a text field with value "45".
+            envelope.AddTabs(new TabCollection
+                             {
+                                 textTabs = textTabs
+                             });
+
+            return envelope;
+        }
+        #endregion
+
+    }
 }
