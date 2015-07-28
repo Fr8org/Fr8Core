dfe84a0c528d84da393c1abb117ce6a4b8b0b279
Dmytro Sobornykov <d1984v@gmail.com>
2015-07-28 18:09:40 +0300

    temp commit

--- Core/Services/ProcessService.cs
+++ Core/Services/ProcessService.cs
@@ -1,78 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 using Core.Interfaces;
 using Core.Managers;
 using Data.Entities;
-using Data.Interfaces;
-using Data.States;
-using StructureMap;
-using Utilities.Logging;
 
 namespace Core.Services
 {
-    public class ProcessService : IProcessService
-    {
-        EventReporter _alertReporter;
-        DockyardAccount _userService;
-        IDocusignXml _docusignXml;
+	public class ProcessService: IProcessService
+	{
+		private readonly EventReporter _alertReporter;
+		private readonly DockyardAccount _userService;
+		private readonly IDocusignXml _docusignXml;
 
-        public ProcessService(EventReporter alertReporter, DockyardAccount userService, IDocusignXml docusignXml)
-        {
-            _alertReporter = alertReporter;
-            _userService = userService;
-            _docusignXml = docusignXml;
-        }
+		public ProcessService( EventReporter alertReporter, DockyardAccount userService, IDocusignXml docusignXml )
+		{
+			this._alertReporter = alertReporter;
+			this._userService = userService;
+			this._docusignXml = docusignXml;
+		}
 
-        /// <summary>
-        /// The method processes incoming notifications from DocuSign. 
-        /// </summary>
-        /// <param name="userId">UserId received from DocuSign.</param>
-        /// <param name="xmlPayload">XML content received from DocuSign.</param>
-        public void HandleDocusignNotification(string userId, string xmlPayload)
-        {
-            string envelopeId;
+		/// <summary>
+		/// New Process object
+		/// </summary>
+		/// <param name="processTemplateId"></param>
+		/// <param name="envelopeId"></param>
+		/// <returns></returns>
+		public ProcessDO Create( string processTemplateId, string envelopeId )
+		{
+			return null;
+		}
 
-            if (string.IsNullOrEmpty(userId))
-                throw new ArgumentNullException("userId");
+		/// <summary>
+		/// The method processes incoming notifications from DocuSign. 
+		/// </summary>
+		/// <param name="userId">UserId received from DocuSign.</param>
+		/// <param name="xmlPayload">XML content received from DocuSign.</param>
+		public void HandleDocusignNotification( string userId, string xmlPayload )
+		{
+			string envelopeId;
 
-            if (string.IsNullOrEmpty(xmlPayload))
-                throw new ArgumentNullException("xmlPayload");
+			if( string.IsNullOrEmpty( userId ) )
+				throw new ArgumentNullException( "userId" );
 
-            try
-            {
-                envelopeId = _docusignXml.GetEnvelopeIdFromXml(xmlPayload);
-            }
-            catch (ArgumentException)
-            {
-                string message = "Cannot extract envelopeId from DocuSign notification: UserId {0}, XML Payload\r\n{1}";
-                _alertReporter.ImproperDocusignNotificationReceived(message);
-                throw new ArgumentException();
-            }
+			if( string.IsNullOrEmpty( xmlPayload ) )
+				throw new ArgumentNullException( "xmlPayload" );
 
-            _alertReporter.DocusignNotificationReceived(userId, xmlPayload);
+			try
+			{
+				envelopeId = this._docusignXml.GetEnvelopeIdFromXml( xmlPayload );
+			}
+			catch( ArgumentException )
+			{
+				const string message = "Cannot extract envelopeId from DocuSign notification: UserId {0}, XML Payload\r\n{1}";
+				this._alertReporter.ImproperDocusignNotificationReceived( message );
+				throw new ArgumentException();
+			}
 
-            IEnumerable<ProcessDO> processList = _userService.GetProcessList(userId);
-            foreach (ProcessDO process in processList)
-            {
-                HandleIncomingNotification(userId, envelopeId, process);
-            }
-        }
+			this._alertReporter.DocusignNotificationReceived( userId, xmlPayload );
 
-        /// <summary>
-        /// Handles a notification by DocuSign by an individual Process.
-        /// </summary>
-        /// <param name="userId">UserId received from DocuSign.</param>
-        /// <param name="envelopeId">EnvelopeId received from DocuSign.</param>
-        /// <param name="process">An instance of ProcessDO object for which processing should occur.</param>
-        private void HandleIncomingNotification(string userId, string envelopeId, ProcessDO process)
-        {
-            _alertReporter.AlertProcessProcessing(userId, envelopeId, process.Id);
-            //TODO: all notification processing logic.
-        }
+			var processList = this._userService.GetProcessList( userId );
+			foreach( var process in processList )
+			{
+				this.HandleIncomingNotification( userId, envelopeId, process );
+			}
+		}
 
-    }
+		/// <summary>
+		/// Handles a notification by DocuSign by an individual Process.
+		/// </summary>
+		/// <param name="userId">UserId received from DocuSign.</param>
+		/// <param name="envelopeId">EnvelopeId received from DocuSign.</param>
+		/// <param name="process">An instance of ProcessDO object for which processing should occur.</param>
+		private void HandleIncomingNotification( string userId, string envelopeId, ProcessDO process )
+		{
+			this._alertReporter.AlertProcessProcessing( userId, envelopeId, process.Id );
+			//TODO: all notification processing logic.
+		}
+
+	}
 }
--- Data/Entities/ProcessDO.cs
+++ Data/Entities/ProcessDO.cs
@@ -1,30 +1,28 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity.Infrastructure;
-using Data.Infrastructure;
-using Data.Interfaces;
-using Data.States;
 using Data.States.Templates;
-using Utilities;
 
 namespace Data.Entities
 {
-    public class ProcessDO : BaseDO
-    {
-        public ProcessDO()
-        {
+	public class ProcessDO: BaseDO
+	{
+		public ProcessDO()
+		{
 
-            //Notes = "No additional notes";
-        }
-        [Key]
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set;}
-        public string UserId { get; set; }
-        [Required, ForeignKey("ProcessStateTemplate")]
-        public int ProcessState {get;set;}    
-        public virtual _ProcessStateTemplate ProcessStateTemplate {get;set;}    
+			//Notes = "No additional notes";
+		}
 
-    }
+		[ Key ]
+		public int Id{ get; set; }
+
+		public string Name{ get; set; }
+		public string Description{ get; set; }
+		public string UserId{ get; set; }
+
+		[ Required ]
+		[ ForeignKey( "ProcessStateTemplate" ) ]
+		public int ProcessState{ get; set; }
+
+		public virtual _ProcessStateTemplate ProcessStateTemplate{ get; set; }
+	}
 }
