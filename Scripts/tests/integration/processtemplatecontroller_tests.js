c72377eaa905727a66a2ba1746ead3721b408b09
Aman Thakral <a@thakral.in>
2015-08-04 09:04:11 -0500

    added the jstest controller  and the layout files for the same jasmine is being added from cdn tried testing the processtemplatecontroller, that is failing with the ajax call reverted the web.config back to default connection string

---
+++ Scripts/tests/integration/processtemplatecontroller_tests.js
@@ -0,0 +1,29 @@
+﻿
+describe("Process Template Controller ", function () {
+    var returnedData = null;
+
+    beforeEach(function (done) {
+        $.ajax({
+            type: "GET",
+            url: "/api/processtemplate",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).success(function (data, status) {
+            returnedData = data;
+            done();
+        }).error(function (response) {
+            if (response.status === 401) {
+                console.log("User is not logged in, to run these tests, please login");
+            } else {
+                console.log("Something went wrong");
+            }
+            done();
+        });
+    });
+
+    it("Can Get Data", function () {
+        expect(returnedData).not.toBe(null);
+    });
+
+
+});
