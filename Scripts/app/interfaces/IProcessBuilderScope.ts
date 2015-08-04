a921cfa5eea97af7dc331370cc7ef1e17b9fadce
Aleksei Avrutin <alexavrutin@gmail.com>
2015-08-04 15:52:23 +0200

    Removed node_modules, added a missing JS file

---
+++ Scripts/app/interfaces/IProcessBuilderScope.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="../_all.ts" />
+
+module dockyard.interfaces {
+    export interface IProcessBuilderScope extends ng.IScope {
+        isCriteriaSelected: () => boolean;
+        selectCriteria: (int) => void;
+        selectAction: (criteriaId: number, actionId: number) => void
+        addAction: (criteriaId: number) => void;
+        addCriteria: () => void;
+        removeCriteria: () => void;
+
+        criteria: Array<ICriteria>;
+        selectedCriteria: ICriteria;
+        fields: Array<IField>;
+    }
+} 
