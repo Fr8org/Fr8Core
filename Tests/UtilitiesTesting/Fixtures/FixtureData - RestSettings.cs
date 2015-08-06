d1157319c506eed1591ab87201cbd3904b59f371
Alex Edelstein <alex@edelstein.org>
2015-08-05 17:51:09 -0700

    fix https bug in TestRestSettings1

--- Tests/UtilitiesTesting/Fixtures/FixtureData - RestSettings.cs
+++ Tests/UtilitiesTesting/Fixtures/FixtureData - RestSettings.cs
@@ -13,7 +13,7 @@ namespace UtilitiesTesting.Fixtures
         {
             // configure application's integrator key and webservice url
             RestSettings.Instance.IntegratorKey = "TEST-34d0ac9c-89e7-4acc-bc1d-24d6cfb867f2";
-            RestSettings.Instance.DocuSignAddress = "http://demo.docusign.net";
+            RestSettings.Instance.DocuSignAddress = "https://demo.docusign.net";
             RestSettings.Instance.WebServiceUrl = RestSettings.Instance.DocuSignAddress + "/restapi/v2";
 
             return RestSettings.Instance;
