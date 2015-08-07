097765be0b3323253de525a44768265b2e2c489e
Dmytro Sobornykov <d1984v@gmail.com>
2015-08-07 08:53:05 +0300

    renamed controller, added DTO

--- pluginAzureSqlServer/Controllers/ActionController.cs
+++ pluginAzureSqlServer/Controllers/ActionController.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using Newtonsoft.Json.Linq;
 using pluginAzureSqlServer.Infrastructure;
@@ -11,8 +7,10 @@ using pluginAzureSqlServer.Services;
 
 namespace pluginAzureSqlServer.Controllers
 {
-    public class CommandController : ApiController
+    public class ActionController : ApiController
     {
+        public const string Version = "1.0";
+
         /// <summary>
         /// Insert user data to remote database tables.
         /// </summary>
---
+++ pluginAzureSqlServer/Models/ActionTypeListDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace pluginAzureSqlServer.Models
+{
+    public class ActionTypeListDTO
+    {
+        public string TypeName { get; set; }
+        public string Version { get; set; }
+    }
+}
