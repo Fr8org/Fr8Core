07d0bdbde286011988eee43fe04eb5d022a47038
Vasanthakumar V <adengappaa@hotmail.com>
2015-08-09 22:14:07 +0530

    Completed UT

---
+++ Tests/DockyardTest/Controllers/EventControllerTest.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Linq;
+using System.Web.Http.Results;
+using Core.Managers;
+using Data.Interfaces;
+using NUnit.Framework;
+using StructureMap;
+using UtilitiesTesting;
+using UtilitiesTesting.Fixtures;
+using Web.Controllers;
+
+namespace DockyardTest.Controllers
+{
+    [TestFixture]
+    public class EventControllerTest : BaseTest
+    {
+        [Test]
+        [Category("Controllers.EventController.Event")]
+        public void EventController_Event_WithNonPlugIn_ReturnsBadRequest()
+        {
+            //Arrange with empty event
+            var eventDto = FixtureData.TestEmptyEventDto();
+
+            //Act
+            var controller = new EventController();
+            var result = controller.Event(eventDto);
+
+            //Assert
+            Assert.IsTrue(result is BadRequestErrorMessageResult);
+        }
+
+        [Test]
+        [Category("Controllers.EventController.Event")]
+        public void EventController_Event_WithPluginIncident_ReturnsOK()
+        {
+            //Arrange with plugin incident
+            new IncidentReporter().SubscribeToAlerts();
+            var eventDto = FixtureData.TestPluginIncidentDto();
+
+            //Act
+            var controller = new EventController();
+            var result = controller.Event(eventDto);
+
+            //Assert
+            Assert.IsTrue(result is OkResult);
+
+            var uow = ObjectFactory.GetInstance<IUnitOfWork>();
+            Assert.AreEqual(1, uow.IncidentRepository.GetAll().Count());
+        }
+    }
+}
---
+++ Tests/UtilitiesTesting/Fixtures/FixtureData - Event.cs
@@ -0,0 +1,24 @@
+﻿
+using Data.Entities;
+using Web.ViewModels;
+
+namespace UtilitiesTesting.Fixtures
+{
+    public partial class FixtureData
+    {
+        public static EventDTO TestEmptyEventDto()
+        {
+            return new EventDTO {EventType = string.Empty};
+        }
+
+        public static EventDTO TestPluginIncidentDto()
+        {
+            var eventDto = new EventDTO
+            {
+                EventType = "Plugin Incident",
+                Data = new HistoryItemDO {PrimaryCategory = "PrimaryCategory", SecondaryCategory = "SecondaryCategory"}
+            };
+            return eventDto;
+        }
+    }
+}
