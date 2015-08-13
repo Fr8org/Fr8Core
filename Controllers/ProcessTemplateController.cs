1a52f1090e893f34efd3d0ade48526b9e90a36bb
Yakov Gnusin <y.gnusin@gmail.com>
2015-08-13 10:41:12 +0600

    Fixed issues in DO-841.

--- Controllers/ProcessTemplateController.cs
+++ Controllers/ProcessTemplateController.cs
@@ -56,16 +56,24 @@ namespace Web.Controllers
         {
             var curProcessTemplates = _processTemplate.GetForUser(User.Identity.Name, User.IsInRole(Roles.Admin),id);
 
-            switch (curProcessTemplates.Count)
+            if (curProcessTemplates.Count == 0)
             {
-                case 0:
-                    throw new ApplicationException("Process Template(s) not found for "+ (id != null ? "id {0}".FormatInvariant(id) :"the current user"));
-                case 1:
-                    return Ok(Mapper.Map<ProcessTemplateDTO>(curProcessTemplates.First()));
+                throw new ApplicationException("Process Template(s) not found for "+ (id != null ? "id {0}".FormatInvariant(id) :"the current user"));
             }
-
-            return Ok(curProcessTemplates.Select(Mapper.Map<ProcessTemplateDTO>));
-            
+            else
+            {
+                // Return first record from curProcessTemplates, in case id parameter was provided.
+                // User intentionally wants to receive a single JSON object in response.
+                if (id.HasValue)
+                {
+                    return Ok(Mapper.Map<ProcessTemplateDTO>(curProcessTemplates.First()));
+                }
+                // Return JSON array of objects, in case no id parameter was provided.
+                else
+                {
+                    return Ok(curProcessTemplates.Select(Mapper.Map<ProcessTemplateDTO>));
+                }
+            }            
         }
 
         public IHttpActionResult Post(ProcessTemplateDTO processTemplateDto)
