19f543f9c051a56b300eff5f9a6097739d87809d
Alex Edelstein <alex@edelstein.org>
2015-08-17 21:22:07 -0700

    deleted the nonversioned file too soon it has some code that needs to be cherry picked out Revert "removing file"
    This reverts commit ddd996c0f6f7cf69e7b9fb2dc2e34791dad27c85.

---
+++ Core/PluginRegistrations/AzureSqlPluginRegistration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core.Interfaces;
+using Data.Entities;
+using Data.Interfaces.DataTransferObjects;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Core.PluginRegistrations
+{
+    public class AzureSqlPluginRegistration : BasePluginRegistration
+    {
+
+#if DEBUG
+        public const string baseUrl = "http://localhost:23432";
+#else
+        public const string baseUrl = "http://services.dockyard.company/azure_sql_server/v1";
+#endif
+
+        private const string availableActions = @"[{ ""ActionType"" : ""Write"" , ""Version"": ""1.0""}]";
+
+        public AzureSqlPluginRegistration(IAction action)
+            : base(availableActions, baseUrl)
+        {
+
+        }
+
+        public override JObject GetConfigurationSettings()
+        {
+            return null;
+        }        
+        
+        public async override Task<IEnumerable<string>> GetFieldMappingTargets(ActionDO curAction)
+        {
+            List<string> result;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                var curActionDto = new ActionDTO();
+                Mapper.Map(curAction, curActionDto);
+
+                var contentPost = new StringContent(JsonConvert.SerializeObject(curActionDto), Encoding.UTF8, "application/json");
+                var response = await client.PostAsync(baseUrl + "/actions/write_to_sql_server/field_mappings", contentPost).ContinueWith(postTask => postTask.Result.EnsureSuccessStatusCode());
+
+                var curMappingTargets = await response.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<List<string>>(curMappingTargets);
+            }
+            return result;
+        }
+    }
+}
