02e04ca3bc29fe65eb9851dfe1dea829cc83a505
Alex Edelstein <alex@edelstein.org>
2015-08-22 11:16:46 -0700

    add migration

--- Core/StructureMap/StructureMapBootStrapper.cs
+++ Core/StructureMap/StructureMapBootStrapper.cs
@@ -128,7 +128,7 @@ namespace Core.StructureMap
                 For<IProfileNodeHierarchy>().Use<ProfileNodeHierarchyWithoutCTE>();
                 var mockSegment = new Mock<ITracker>();
                 For<IActionRegistration>().Use<ActionRegistration>();
-                For<ITemplate>().Use<Template>();
+                
                 For<ITracker>().Use(mockSegment.Object);
                 For<IProcess>().Use<Process>();
                 For<ICriteria>().Use<Criteria>();
