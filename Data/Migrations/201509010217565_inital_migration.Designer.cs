bb864ab41b815d188e00bbdabd92ae99afae928c
Alex Edelstein <alex@edelstein.org>
2015-08-31 19:20:26 -0700

    squash again

--- Data/Migrations/201509010217565_inital_migration.Designer.cs
+++ Data/Migrations/201509010217565_inital_migration.Designer.cs
@@ -7,13 +7,13 @@ namespace Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.0-30225")]
-    public sealed partial class UpdateProcessRelation : IMigrationMetadata
+    public sealed partial class inital_migration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(UpdateProcessRelation));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(inital_migration));
         
         string IMigrationMetadata.Id
         {
-            get { return "201508191958517_UpdateProcessRelation"; }
+            get { return "201509010217565_inital_migration"; }
         }
         
         string IMigrationMetadata.Source
