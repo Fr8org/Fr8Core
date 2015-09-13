d3b30db3e5050c85cd515a75e0296ba201997fe3
kiseln <nikita_lk@yahoo.com>
2015-09-13 23:12:42 +0300

    Added CoreActions project, because I had troubles with merging

---
+++ CoreActions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(CoreActions.Startup))]
+
+namespace CoreActions
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
