02042501fbbb0cabbd9f585d3e5f91c60a799143
kiseln <nikita_lk@yahoo.com>
2015-09-14 22:06:23 +0300

    Created Process Template Details page.

---
+++ Scripts/app/controllers/ProcessTemplateDetailsController.ts
@@ -0,0 +1,40 @@
+﻿/// <reference path="../_all.ts" />
+
+module dockyard.controllers {
+    'use strict';
+
+    export interface IProcessTemplateDetailsScope extends ng.IScope {
+        ptvm: interfaces.IProcessTemplateVM;
+        submit: (isValid: boolean) => void;
+        errorMessage: string;
+        processBuilder: any
+    }
+
+    class ProcessTemplateDetailsController {
+        // $inject annotation.
+        // It provides $injector with information about dependencies to be injected into constructor
+        // it is better to have it close to the constructor, because the parameters must match in count and type.
+        // See http://docs.angularjs.org/guide/di
+        public static $inject = [
+            '$rootScope',
+            '$scope',
+            'ProcessTemplateService',
+            '$stateParams'
+        ];
+
+        constructor(
+            private $rootScope: interfaces.IAppRootScope,
+            private $scope: IProcessTemplateDetailsScope,
+            private ProcessTemplateService: services.IProcessTemplateService,
+            private $stateParams: any) {
+            
+            //Load detailed information
+            var id: string = $stateParams.id;
+            if (/^[0-9]+$/.test(id) && parseInt(id) > 0) {
+                $scope.ptvm = ProcessTemplateService.get({ id: $stateParams.id });
+            }
+        }
+    }
+
+    app.controller('ProcessTemplateDetailsController', ProcessTemplateDetailsController);
+}
