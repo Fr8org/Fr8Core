988ead04f976821ad5504ce2549f32396be11093
Naveed Kharadi <naveed_kharadi@yahoo.com>
2015-09-19 01:57:55 +1200

    DO-1017 merge build fix

--- Scripts/app/directives/PaneConfigureAction/ConfigurationField.ts
+++ Scripts/app/directives/PaneConfigureAction/ConfigurationField.ts
@@ -8,7 +8,7 @@ module dockyard.directives.paneConfigureAction {
         filePicker,
         radioGroupButton,
         dropdownlistField,
-        textBlockField
+        textBlockField,
         routing,
     }
 
