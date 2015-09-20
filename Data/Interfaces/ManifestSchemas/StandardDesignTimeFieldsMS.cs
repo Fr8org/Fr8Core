d0ce2c992df1bc1220220ba52616a5161a192fb2
Yakov Gnusin <y.gnusin@gmail.com>
2015-09-20 12:46:34 +0600

    Got WaitForDocuSignEvent and FilterUsingRuntimeData working via new ManifestScheme DTOs

--- Data/Interfaces/ManifestSchemas/StandardDesignTimeFieldsMS.cs
+++ Data/Interfaces/ManifestSchemas/StandardDesignTimeFieldsMS.cs
@@ -5,7 +5,11 @@ namespace Data.Interfaces.ManifestSchemas
 {
     public class StandardDesignTimeFieldsMS
     {
+        public StandardDesignTimeFieldsMS()
+        {
+            Fields = new List<FieldDTO>();
+        }
+
         public List<FieldDTO> Fields { get; set; }
-      
     }
 }
