2c0c6236e42b45e842e5346fc4f7f7dc7feda1fb
nishantdev13 <nishantdev13@gmail.com>
2015-09-23 01:25:22 +0530

    DO-1067: Combine pluginAzureSqlServerTests and pluginBaseTests into pluginTests

---
+++ Tests/UtilitiesTesting/Fixtures/FixtureData - ActivityTemplateDTO.cs
@@ -0,0 +1,24 @@
+﻿﻿using Data.Interfaces.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilitiesTesting.Fixtures
+{
+    partial class FixtureData
+    {
+        public static ActivityTemplateDTO TestActivityTemplateDTO1()
+        {
+            return new ActivityTemplateDTO()
+            {
+                Id = 1,
+                Name = "Write_To_Sql_Server",
+                Version = "1"
+            };
+        }
+
+
+    }
+}
---
+++ pluginTests/PluginBaseTests/Controllers/BasePluginControllerTests.cs
@@ -0,0 +1,42 @@
+﻿﻿using System;
+using UtilitiesTesting;
+using NUnit.Framework;
+using PluginBase.BaseClasses;
+using Data.Interfaces.DataTransferObjects;
+using UtilitiesTesting.Fixtures;
+using System.Threading.Tasks;
+
+namespace pluginTests.PluginBaseTests.Controllers
+{
+    [TestFixture]
+    [Category("BasePluginController")]
+    public class BasePluginControllerTests : BaseTest
+    {
+        BasePluginController _basePluginController;
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            _basePluginController = new BasePluginController();
+        }
+
+        [Test]
+        public void HandleDockyardRequest_PluginTypeIsAzureSqlServer_ResponseInitialConfiguration()
+        {
+            string curPlugin = "pluginAzureSqlServer";
+            string curActionPath = "Configure";
+
+            ActionDTO curActionDTO = FixtureData.TestActionDTO1();
+
+            CrateStorageDTO result = (CrateStorageDTO)_basePluginController.HandleDockyardRequest(curPlugin, curActionPath, curActionDTO);
+
+            Assert.AreEqual("Standard Configuration Controls", result.CrateDTO[0].ManifestType);
+        }
+
+       
+
+
+
+    }
+}
