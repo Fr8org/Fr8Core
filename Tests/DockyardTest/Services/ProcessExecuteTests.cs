06df43a6906c1506db8a87437cd93bad16637001
Farrukh Normuradov <farrukh.normuradov@gmail.com>
2015-09-28 01:08:11 +0500

    Changed Process#Execute tests in accord with project conventions, updated mistake with IProcess to make use of polymorphism

--- Tests/DockyardTest/Services/ProcessExecuteTests.cs
+++ Tests/DockyardTest/Services/ProcessExecuteTests.cs
@@ -17,33 +17,39 @@ namespace DockyardTest.Services
 
     public class ProcessExecuteTests: BaseTest
     {
+        private IProcess _process;
+
+        [SetUp]
+        //constructor method as it is run at the test start
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            _process = ObjectFactory.GetInstance<IProcess>();
+        }
+
         [Test]
 
+        //NUnit framework feature: ability to test for an exception
         [ExpectedException(typeof(ArgumentNullException))]
 
         public void ExecuteFailsIfNullProcessDo()
         {
-            Process processService = ObjectFactory.GetInstance<Process>();
-
-            ProcessDO processDo = null;
-
-            processService.Execute(processDo);
-
+            //null is passed instead of ProcessDo object
+            _process.Execute(null);
+        
         }
         
         [Test]
 
+        //NUnit framework feature: ability to test for an exception
         [ExpectedException(typeof(ArgumentNullException))]
 
         public void ExecuteFailsIfNullProcessDoCurrentActivity()
         {
-            //All static parcial class for already prepared data
-            //The CurrentActivity value is already set to null
-            ProcessDO processDo = FixtureData.TestProcessCurrentActivityNULL();
-
-            Process processService = ObjectFactory.GetInstance<Process>();
-
-            processService.Execute(processDo);
+            //Get ProcessDO entity from static partial class FixtureData for already prepared data
+            //The CurrentActivity value is already set to null and pass it immediately to service
+            _process.Execute(FixtureData.TestProcessCurrentActivityNULL());
 
         }
     }
