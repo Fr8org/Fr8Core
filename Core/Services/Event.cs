af332de85186e069451043d924bf938ca19031cd
Alex Edelstein <alex@edelstein.org>
2015-09-26 17:11:12 -0700

    moved some logic out of eventcontroller

--- Core/Services/Event.cs
+++ Core/Services/Event.cs
@@ -1,6 +1,12 @@
-﻿using Core.Interfaces;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Core.Interfaces;
 using Data.Infrastructure;
 using Data.Interfaces.DataTransferObjects;
+using StructureMap;
 
 namespace Core.Services
 {
@@ -9,6 +15,14 @@ namespace Core.Services
     /// </summary>
     public class Event : IEvent
     {
+
+        private readonly IPlugin _plugin;
+
+        public Event()
+        {
+ 
+            _plugin = ObjectFactory.GetInstance<IPlugin>();
+        }
         /// <see cref="IEvent.HandlePluginIncident"/>
         public void HandlePluginIncident(LoggingData incident)
         {
@@ -19,5 +33,19 @@ namespace Core.Services
         {
             EventManager.ReportPluginEvent(eventData);
         }
+
+        public async Task<string> RequestParsingFromPlugins(HttpRequestMessage request, string pluginName, string pluginVersion)
+
+        {
+            //get required plugin URL by plugin name and its version
+            string curPluginUrl = _plugin.ParsePluginUrlFor(pluginName, pluginVersion, "events");
+
+
+            //make POST with request content
+            await new HttpClient().PostAsync(new Uri(curPluginUrl, UriKind.Absolute), request.Content);
+
+            return "ok";
+        }
     }
 }
+
