8cc470a3551ada5b0d005539a4b4561ced4ae6a0
Alex Edelstein <alex@edelstein.org>
2015-10-07 15:58:23 -0700

    Revert "Do 1090"

--- Tests/UtilitiesTesting/Fixtures/FixtureData - ActionDTO.cs
+++ Tests/UtilitiesTesting/Fixtures/FixtureData - ActionDTO.cs
@@ -1,9 +1,9 @@
 ﻿﻿using Data.Interfaces.DataTransferObjects;
-using Data.Interfaces.ManifestSchemas;
-using Newtonsoft.Json;
-using pluginDocuSign.DataTransferObjects;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace UtilitiesTesting.Fixtures
 {
@@ -14,45 +14,8 @@ namespace UtilitiesTesting.Fixtures
             return new ActionDTO()
             {
                 Name = "test action type",
-                ActivityTemplate = FixtureData.TestActivityTemplateDTO1(),
-                AuthToken = new AuthTokenDTO() { Token = JsonConvert.SerializeObject(TestDocuSignAuthDTO1()) }
-            };
-        }
-        public static ActionDTO TestActionDTO2()
-        {
-            ActionDTO curActionDTO = new ActionDTO()
-            {
-                Name = "test action type",
-                ActivityTemplate = FixtureData.TestActivityTemplateDTO1(),
-                AuthToken = new AuthTokenDTO() { Token = JsonConvert.SerializeObject(TestDocuSignAuthDTO1()) }
-            };
-            curActionDTO.CrateStorage.CrateDTO.Add(CreateStandardConfigurationControls());
-
-            return curActionDTO;
-        }
-
-        public static ActionDTO TestActionDTO3()
-        {
-            ActionDTO curActionDTO = new ActionDTO()
-            {
-                Name = "test action type",
                 ActivityTemplate = FixtureData.TestActivityTemplateDTO1()
             };
-            curActionDTO.CrateStorage.CrateDTO.Add(CreateStandardConfigurationControls());
-            var configurationFields = JsonConvert.DeserializeObject<StandardConfigurationControlsMS>(curActionDTO.CrateStorage.CrateDTO[0].Contents);
-
-            curActionDTO.CrateStorage.CrateDTO.Add(CreateEventSubscriptionCrate(configurationFields));
-
-            return curActionDTO;
-        }
-
-        public static ActionDTO CreateStandardDesignTimeFields()
-        {
-            ActionDTO curActionDTO = new ActionDTO();
-            List<CrateDTO> curCratesDTO = FixtureData.TestCrateDTO2();
-            curActionDTO.CrateStorage.CrateDTO.AddRange(curCratesDTO);
-            curActionDTO.AuthToken = new AuthTokenDTO() { Token = JsonConvert.SerializeObject(TestDocuSignAuthDTO1()) };
-            return curActionDTO;
         }
 
         public static ActionDTO TestActionDTOForSalesforce()
