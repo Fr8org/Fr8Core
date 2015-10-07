995283784762a405277141f5e7fadf7ea0761bba
tehnetaholic <teh.netaholic@gmail.com>
2015-10-08 01:07:13 +0300

    fixed

--- Data/Repositories/MultiTenantObjectRepository.cs
+++ Data/Repositories/MultiTenantObjectRepository.cs
@@ -11,101 +11,73 @@ using System.Threading.Tasks;
 
 namespace Data.Repositories
 {
-    public class MultiTenantObjectRepository : IDisposable
+    public class MultiTenantObjectRepository
     {
-        private IUnitOfWork _uow;
-        public MultiTenantObjectRepository(IUnitOfWork uow)
+        public void Add(IUnitOfWork _uow, BaseMultiTenantObject curMTO)
         {
-            this._uow = uow;
-        }
-
-        public void Add(BaseMultiTenantObject curData)
-        {
-            var curDataType = curData.GetType();
+            var curDataType = curMTO.GetType();
             var curDataProperties = curDataType.GetProperties(System.Reflection.BindingFlags.DeclaredOnly | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public).ToList();
             var typesDict = new Dictionary<Type, MT_FieldType>();
             //get or create MTObject
-            var correspondingMTObject = _uow.MTObjectRepository.FindOne(a => a.MT_OrganizationId == curData.fr8AccountId && a.Name == curData.Name);
-            if (correspondingMTObject == null)
-            {
-                correspondingMTObject = new MT_Object() { MT_OrganizationId = curData.fr8AccountId, Name = curData.Name };
-                var correspongdintDTOrganization = _uow.MTOrganizationRepository.GetByKey(curData.fr8AccountId);
-                correspondingMTObject.MT_Organization = correspongdintDTOrganization;
-                correspondingMTObject.MT_FieldType = GetOrCreateMTFieldType(curDataType, typesDict);
-                _uow.MTObjectRepository.Add(correspondingMTObject);
-            }
+            var fieldWorker = new Infrastructure.MultiTenant.MT_Field();
+            var correspondingMTObject = fieldWorker.GetOrCreateMT_Object(_uow, curMTO, curDataType, typesDict);
 
-            //get or create MTFields
             if (correspondingMTObject.Fields == null)
             {
-                var fieldsList = new List<MT_Field>();
-                //here we can filter what properties should be added and stored
-                for (int i = 1; i < curDataProperties.Count + 1; i++)
-                {
-                    var property = curDataProperties[i - 1];
-                    MT_Field mtField = new MT_Field();
-                    mtField.FieldColumnOffset = i;
-                    mtField.MT_ObjectId = correspondingMTObject.Id;
-                    mtField.Name = property.Name;
-                    mtField.MT_Object = correspondingMTObject;
-                    //get or create FieldType
-                    mtField.MT_FieldType = GetOrCreateMTFieldType(property.PropertyType, typesDict);
-                    fieldsList.Add(mtField);
-                    _uow.MTFieldRepository.Add(mtField);
-                }
-                //have to requery
-                correspondingMTObject.Fields = fieldsList;
+                correspondingMTObject.Fields = GetOrCreateMT_Fields(_uow, curDataProperties, correspondingMTObject, typesDict);
             }
 
             //create MTData and fill values
             var data = new MT_Data();
-            data.Name = curData.Name;
+            data.Name = curMTO.Name;
             data.CreatedAt = DateTime.Now;
             data.UpdatedAt = DateTime.Now;
             data.MT_Object = correspondingMTObject;
-            Map(curData, curDataProperties, data, correspondingMTObject);
+            MapObjectToMTData(curMTO, curDataProperties, data, correspondingMTObject);
             _uow.MTDataRepository.Add(data);
         }
 
-        private MT_FieldType GetOrCreateMTFieldType(Type type, Dictionary<Type, MT_FieldType> newTypes)
+        private List<MT_Field> GetOrCreateMT_Fields(IUnitOfWork _uow, List<PropertyInfo> curDataProperties, MT_Object correspondingMTObject, Dictionary<Type, MT_FieldType> typesDict)
         {
-            var correspondingMTFieldType = _uow.MTFieldTypeRepository.GetAll().Where(a => a.TypeName == type.FullName).FirstOrDefault();
-            // I haven't found a way to get an MTFieldType item from repository, if it wasn't saved yet, so I have a local Dictionary
-            if (correspondingMTFieldType == null)
+            var fieldsList = new List<MT_Field>();
+            int i = 1;
+            foreach (var property in curDataProperties)
             {
-                if (newTypes.ContainsKey(type))
-                    correspondingMTFieldType = newTypes[type];
+                MT_Field mtField = new MT_Field();
+                mtField.FieldColumnOffset = i;
+                mtField.MT_ObjectId = correspondingMTObject.Id;
+                mtField.Name = property.Name;
+                mtField.MT_Object = correspondingMTObject;
+                //get or create FieldType
+                var fieldWorker = new Infrastructure.MultiTenant.MT_Field();
+                mtField.MT_FieldType = fieldWorker.GetOrCreateMT_FieldType(_uow, property.PropertyType, typesDict);
+                fieldsList.Add(mtField);
+                _uow.MTFieldRepository.Add(mtField);
+                i++;
             }
-            if (correspondingMTFieldType == null)
-            {
-                correspondingMTFieldType = new MT_FieldType();
-                correspondingMTFieldType.AssemblyName = type.Assembly.FullName;
-                correspondingMTFieldType.TypeName = type.FullName;
-                _uow.MTFieldTypeRepository.Add(correspondingMTFieldType);
-                newTypes[type] = correspondingMTFieldType;
-            }
-            return correspondingMTFieldType;
+            return fieldsList;
         }
 
+    
 
-        public void Update(BaseMultiTenantObject curData)
+        public void Update(IUnitOfWork _uow, BaseMultiTenantObject curData)
         {
             var correspondingDTObject = _uow.MTObjectRepository.FindOne(a => a.MT_OrganizationId == curData.fr8AccountId && a.Name == curData.Name);
             var correspondingDTFields = _uow.MTFieldRepository.FindList(a => a.MT_ObjectId == correspondingDTObject.Id);
             var curDataProperties = curData.GetType().GetProperties(System.Reflection.BindingFlags.DeclaredOnly | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public).ToList();
             var data = _uow.MTDataRepository.GetByKey(curData.MT_DataId);
             data.UpdatedAt = DateTime.Now;
-            Map(curData, curDataProperties, data, correspondingDTObject);
+            MapObjectToMTData(curData, curDataProperties, data, correspondingDTObject);
         }
 
-        public void Remove(int id)
+        public void Remove(IUnitOfWork _uow, int id)
         {
             var dataObject = _uow.MTDataRepository.GetByKey(id);
             if (dataObject != null)
                 dataObject.IsDeleted = true;
         }
 
-        public BaseMultiTenantObject GetByKey(int id)
+        public BaseMultiTenantObject GetByKey(IUnitOfWork _uow, int id)
         {
             var correspondingMT_Data = _uow.MTDataRepository.GetByKey(id);
             if (correspondingMT_Data == null || correspondingMT_Data.IsDeleted) return null;
@@ -115,12 +87,11 @@ namespace Data.Repositories
 
             var correspondingMT_Fields = _uow.MTFieldRepository.GetQuery().Where(a => a.MT_ObjectId == correspondingMT_Object.Id);
 
-            return MapBack(correspondingMT_Data, correspondingMT_Object);
+            return MapMTDataToObject(correspondingMT_Data, correspondingMT_Object);
         }
 
-
         //maps BaseMTO to MTData
-        private void Map(BaseMultiTenantObject curData, List<PropertyInfo> curDataProperties, MT_Data data, MT_Object correspondingDTObject)
+        private void MapObjectToMTData(BaseMultiTenantObject curData, List<PropertyInfo> curDataProperties, MT_Data data, MT_Object correspondingDTObject)
         {
             var correspondingDTFields = correspondingDTObject.Fields;
             curData.MT_DataId = data.Id;
@@ -138,7 +109,7 @@ namespace Data.Repositories
         }
 
         //instantiate object from MTData
-        private BaseMultiTenantObject MapBack(MT_Data data, MT_Object correspondingDTObject)
+        private BaseMultiTenantObject MapMTDataToObject(MT_Data data, MT_Object correspondingDTObject)
         {
 
             var correspondingDTFields = correspondingDTObject.Fields;
@@ -166,12 +137,6 @@ namespace Data.Repositories
                 correspondingProperty.SetValue(obj, val);
             }
             return obj;
-        }
-
-
-        public void Dispose()
-        {
-            _uow.Dispose();
         }
     }
 }
