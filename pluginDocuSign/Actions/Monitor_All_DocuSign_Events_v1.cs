3fb904788ae81e7aac29d902f90cab89f67234f5
Vasanthakumar V <adengappaa@hotmail.com>
2015-10-08 16:14:58 +0530

    Created action. Created account. Completed Configure_Activate and Deactivate methods

---
+++ pluginDocuSign/Actions/Monitor_All_DocuSign_Events_v1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Data.Interfaces.DataTransferObjects;
+using DocuSign.Integrations.Client;
+using PluginBase.BaseClasses;
+using PluginBase.Infrastructure;
+using pluginDocuSign.Infrastructure;
+using Configuration = DocuSign.Integrations.Client.Configuration;
+
+namespace pluginDocuSign.Actions
+{
+    public class Monitor_All_DocuSign_Events_v1 : BasePluginAction
+    {
+        public async Task<ActionDTO> Configure(ActionDTO curActionDTO)
+        {
+            //For this action, both Initial and Followup configuration requests are same. Hence it returns Initial config request type always.
+            return await ProcessConfigurationRequest(curActionDTO, dto => ConfigurationRequestType.Initial);
+        }
+
+        protected override async Task<ActionDTO> InitialConfigurationResponse(ActionDTO curActionDTO)
+        {
+            //create a Standard Event Subscription crate
+            var curEventSubscriptionCrate = _crate.CreateStandardEventSubscriptionsCrate("Standard Event Subscription", DocuSignEventNames.GetAllEventNames());
+
+            //update crate storage with standard event subscription crate
+            curActionDTO.CrateStorage = new CrateStorageDTO()
+            {
+                CrateDTO = new List<CrateDTO> { curEventSubscriptionCrate }
+            };
+
+            return await Task.FromResult(curActionDTO);
+        }
+
+        public object Activate(ActionDTO curActionDTO)
+        {
+            DocuSignAccount curDocuSignAccount = new DocuSignAccount();
+            var curConnectProfile = curDocuSignAccount.GetDocuSignConnectProfiles();
+
+            var monitorConnectConfiguration = new DocuSign.Integrations.Client.Configuration
+            {
+                envelopeEvents = string.Join(",", DocuSignEventNames.GetEventsFor("Envelope")),
+                recipientEvents = string.Join(",", DocuSignEventNames.GetEventsFor("Recipient")),
+                name = "MonitorAllDocuSignEvents",
+                urlToPublishTo =
+                    Regex.Match(ConfigurationManager.AppSettings["EventWebServerUrl"], @"(\w+://\w+:\d+)").Value +
+                    "/events?dockyard_plugin=pluginDocuSign&version=1"
+            };
+
+            if (Int32.Parse(curConnectProfile.totalRecords) > 0 &&
+                curConnectProfile.configurations.Any(config => config.name.Equals("MonitorAllDocuSignEvents")))
+            {
+                curDocuSignAccount.UpdateDocuSignConnectProfile(monitorConnectConfiguration);
+            }
+            else
+            {
+                curDocuSignAccount.CreateDocuSignConnectProfile(monitorConnectConfiguration);
+            }
+
+            return true;
+        }
+
+        public object Deactivate(ActionDTO curDataPackage)
+        {
+            DocuSignAccount curDocuSignAccount = new DocuSignAccount();
+            var curConnectProfile = curDocuSignAccount.GetDocuSignConnectProfiles();
+
+            if (Int32.Parse(curConnectProfile.totalRecords) > 0 && curConnectProfile.configurations.Any(config => config.name.Equals("MonitorAllDocuSignEvents")))
+            {
+                curDocuSignAccount.DeleteDocuSignConnectProfile("MonitorAllDocuSignEvents");
+            }
+
+            return true;
+        }
+
+        public async Task<PayloadDTO> Execute(ActionDTO actionDto)
+        {
+            return null;
+        }
+    }
+}
--- pluginDocuSign/Infrastructure/DocuSignEventNames.cs
+++ pluginDocuSign/Infrastructure/DocuSignEventNames.cs
@@ -13,12 +13,14 @@ namespace pluginDocuSign.Infrastructure
         public const int EnvelopeCompleted = 4;
         public const int EnvelopeDeclined = 5;
         public const int EnvelopeVoided = 6;
+
         public const int RecipientSent = 7;
         public const int RecipientDelivered = 8;
         public const int RecipientSigned = 9;
         public const int RecipientCompleted = 10;
         public const int RecipientDeclined = 11;
-        public const int RecipientVoided = 12;
+        public const int RecipientAuthenticationFailed = 12;
+        public const int RecipientAutoResponded = 13;
 
         public static int MapEnvelopeExternalEventType(string status)
         {
@@ -40,5 +42,49 @@ namespace pluginDocuSign.Infrastructure
                     return 0;
             }
         }
+
+        public static int MapRecipientExternalEventType(string status)
+        {
+            switch (status)
+            {
+                case "Sent":
+                    return RecipientSent;
+                case "Delivered":
+                    return RecipientDelivered;
+                case "Signed":
+                    return RecipientSigned;
+                case "Completed":
+                    return RecipientCompleted;
+                case "Declined":
+                    return RecipientDeclined;
+                case "AuthenticationFailed":
+                    return RecipientAuthenticationFailed;
+                case "AutoResponded":
+                    return RecipientAutoResponded;
+                default:
+                    return 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets all event names in the form of EnvelopeSent and RecipientSent
+        /// </summary>
+        public static string[] GetAllEventNames()
+        {
+            return typeof(DocuSignEventNames).GetFields().Select(p => p.Name).ToArray();
+        }
+
+        /// <summary>
+        /// Gets events names in the form of Sent and Delivered for the given event type (Envelope or Recipient)
+        /// </summary>
+        public static string[] GetEventsFor(string eventType)
+        {
+            return
+                GetAllEventNames()
+                    .Where(name => name.StartsWith(eventType))
+                    .Select(name => name.Remove(0, eventType.Length))
+                    .ToArray();
+        }
+
     }
 }
