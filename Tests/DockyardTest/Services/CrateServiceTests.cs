e5b852f009a1cd9d6f32578d6e748e026b6f2c47
Aleksei Avrutin <alexavrutin@gmail.com>
2015-10-09 23:14:10 +0200

    A fixture to create crate with all the available configuration controls.

--- Tests/DockyardTest/Services/CrateServiceTests.cs
+++ Tests/DockyardTest/Services/CrateServiceTests.cs
@@ -80,13 +80,13 @@ namespace DockyardTest.Services
             Assert.AreEqual(typeof (DropDownListControlDefinitionDTO), radiobuttonGroup.Radios[1].Controls[0].GetType());
         }
 
-        public void CanSerializeStandardConfigurationControls()
-        {
-            // Arrange
-            var _crate = ObjectFactory.GetInstance<ICrate>();
-            var controls = FixtureData.AllConfigurationControls();
-            var curCrateDTO = _crate.CreateStandardConfigurationControlsCrate("Configuration_Controls", controls.ToArray());
-
-        }
+        //[Test]
+        //public void CanSerializeStandardConfigurationControls()
+        //{
+        //    // Arrange
+        //    var _crate = ObjectFactory.GetInstance<ICrate>();
+        //    var controls = FixtureData.AllConfigurationControls();
+        //    var curCrateDTO = _crate.CreateStandardConfigurationControlsCrate("Configuration_Controls", controls.ToArray());
+        //}
     }
 }
