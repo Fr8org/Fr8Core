38091252ff2bab0920dafeb987f966ad5f73db8f
kggayo <kagaw3000@live.com>
2015-10-17 00:46:35 +0800

    removed pluginTests project and transferred to its own plugin project

---
+++ pluginAzureSqlServer/Tests/Controllers/ActionControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using NUnit.Framework;
+using pluginAzureSqlServer.Controllers;
+
+namespace pluginAzureSqlServer.Tests.Controllers
+{
+    [TestFixture]
+    public class ActionControllerTest : BaseTest
+    {
+
+    }
+}
