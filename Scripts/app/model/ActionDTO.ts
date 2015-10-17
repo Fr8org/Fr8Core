aedd21fe6562de8ffb502f05019ca5ff6e71ad8c
pharrukh <farrukh.normuradov@gmail.com>
2015-10-18 01:32:34 +0500

    Added Labels to all ActivityTemplates of Plugins and make Labels appear on the Action Selection Pane and Action Pane in the Process Builder

---
+++ Scripts/app/model/ActionDTO.ts
@@ -0,0 +1,61 @@
+﻿module dockyard.model {
+    export class ActionDTO implements interfaces.IActionDTO {
+        parentRouteNodeId: number;
+        id: number;
+        isTempId: boolean;
+        name: string;
+        label: string;
+        crateStorage: model.CrateStorage;
+        configurationControls: model.ControlsList;
+        activityTemplateId: number;
+        activityTemplateName: string;
+        currentView: string;
+
+        constructor(
+            parentActivityId: number,
+            id: number,
+            isTempId: boolean
+        ) {
+            this.parentRouteNodeId = parentActivityId;
+            this.id = id;
+            this.isTempId = isTempId;
+            this.configurationControls = new ControlsList();
+        }
+
+        toActionVM(): interfaces.IActionVM {
+            return <interfaces.IActionVM> {
+                id: this.id,
+                isTempId: this.isTempId,
+                parentRouteNodeId: this.parentRouteNodeId,
+                name: this.name,
+                label: this.label,
+                crateStorage: this.crateStorage,
+                configurationControls: this.configurationControls
+            };
+        }
+
+        clone(): ActionDTO {
+            var result = new ActionDTO(this.parentRouteNodeId, this.id, this.isTempId);
+            result.name = this.name;
+            result.name = this.label;
+            return result;
+        }
+
+        static isActionValid(action: interfaces.IActionVM) {
+            return action && action.$resolved && !action.isTempId
+        }
+
+        static create(dataObject: interfaces.IActionDTO): ActionDTO {
+            var result = new ActionDTO(0, 0, false);
+            result.activityTemplateId = dataObject.activityTemplateId;
+            result.crateStorage = dataObject.crateStorage;
+            result.configurationControls = dataObject.configurationControls;
+            result.id = dataObject.id;
+            result.isTempId = dataObject.isTempId;
+            result.name = dataObject.name;
+            result.label = dataObject.label;
+            result.parentRouteNodeId = dataObject.parentRouteNodeId;
+            return result;
+        }
+    }
+}
---
+++ Scripts/app/model/ActivityTemplate.ts
@@ -0,0 +1,37 @@
+﻿module dockyard.model {
+    export class ActivityTemplate {
+        id: number;
+        name: string;
+        label: string;
+        version: string;
+        defaultEndPoint: string;
+        componentActivities: string;
+        category: string;
+
+        constructor(id: number, name: string,
+            version: string, componentActivities: string, category: string, label?:string) {
+
+            this.id = id;
+            this.name = name;
+            this.label = label;
+            this.version = version;
+            this.componentActivities = componentActivities;
+            this.category = category;
+            //this.parentPluginRegistration = parentPluginRegistration;  the client shouldn't know anything about plugins
+        }
+
+        clone(): ActivityTemplate {
+            var result = new ActivityTemplate(
+                this.id,
+                this.name,
+                this.label,
+                this.version,
+                this.componentActivities,
+                this.category
+            // this.parentPluginRegistration
+                );
+
+            return result;
+        }
+    }
+}
