214b81f38d0086c8cbea7293ed65a529a88bee2a
Bahadir Bozdag <bahadir.bb@gmail.com>
2015-10-17 12:45:54 +0300

    Added field Service

---
+++ Core/Services/Field.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class Field
+    {
+        public bool Exists(string manifestType, string label)
+        {
+            
+
+
+            return false;
+        }
+    }
+}
