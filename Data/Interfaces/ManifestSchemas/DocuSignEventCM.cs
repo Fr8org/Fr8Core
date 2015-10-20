4e4388fa5942028a2c480232b743f0498d1f0c5a
Vasanthakumar V <adengappaa@hotmail.com>
2015-10-20 11:58:33 +0530

    Added DocuSign envelope and event manifests

---
+++ Data/Interfaces/ManifestSchemas/DocuSignEventCM.cs
@@ -0,0 +1,18 @@
+﻿namespace Data.Interfaces.ManifestSchemas
+{
+    public class DocuSignEventCM : Manifest
+    {
+
+        public string Object { get; set; }
+        public string Status { get; set; }
+        public string EventId { get; set; }
+        public string EnvelopeId { get; set; }
+        public string RecepientId { get; set; }
+
+        public DocuSignEventCM()
+            : base(Constants.MT.DocuSignEvent)
+        {
+
+        }
+    }
+}
