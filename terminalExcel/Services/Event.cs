45a61fec264045fd4571ff5bc9eb472be7f8b400
Yakov Gnusin <y.gnusin@gmail.com>
2015-10-21 19:06:45 +0600

    Renamed pluginExcel -> terminalExcel.

---
+++ terminalExcel/Services/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace terminalExcel.Services
+{
+    public class Event : IEvent
+    {
+        public void Process(string curExternalEventPayload)
+        {
+            //Process external event payload from plugin
+
+        }
+    }
+}
