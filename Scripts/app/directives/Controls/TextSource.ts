3fc814b84f92a61db6920c70a515474983911128
Aleksei Avrutin <alexavrutin@gmail.com>
2015-10-22 20:05:27 +0200

    TextSource.ts added

---
+++ Scripts/app/directives/Controls/TextSource.ts
@@ -0,0 +1,26 @@
+﻿/// <reference path="../../_all.ts" />
+module dockyard.directives.textSource {
+    'use strict';
+
+    export interface ITextSourceScope extends ng.IScope {
+        field: model.TextSource;
+        change: () => (fieldName: string) => void;
+    }
+
+    export function TextSource(): ng.IDirective {
+        var controller = ['$scope', function ($scope: ITextSourceScope) {
+        }];
+
+        return {
+            restrict: 'E',
+            templateUrl: '/AngularTemplate/TextSource',
+            controller: controller,
+            scope: {
+                field: '=',
+                change: '&'
+            }
+        };
+    }
+
+    app.directive('textSource', TextSource);
+}
