c37b7a513f5c781d8a60a204254627b936a3d734
Dmitry Pashkevich <d.pashkevich@leckerweb.com>
2015-10-23 01:29:11 +0300

    DO-1374 + PusherNotifierService added

---
+++ Scripts/app/services/PusherNotifierService.ts
@@ -0,0 +1,38 @@
+﻿module dockyard.services {
+  
+    app.factory('PusherNotifierService', ['$pusher', $pusher => {
+            var channels;
+            var client = new Pusher('123dd339500fed0ddd78', { encrypted: true });
+            var pusher = $pusher(client);
+
+            return {
+                bindEventToChannel: (channel, event , callback) => {
+                    if (!channel || !event || !callback) {
+                        return false;                  
+                    }
+                    if (!channels[channel]) {
+                        channels[channel] = pusher.subscribe(channel);
+                    }
+                    channels[channel].bind(event, callback);   
+                    return true;
+                },
+                bindEventToAllChannels: (event, callback) => {
+                    if (!event || !callback) {
+                        return false;
+                    }
+                    channels.forEach(channel => {
+                        channel.bind(event, callback);   
+                    });
+                    return true;
+                },
+                unbindEvent: (channel, event) => {
+                    if (channel && event && channels[channel]) {
+                        channels[channel].unbind(event);
+                        return true;
+                    }
+                    return false;
+                }
+            };
+        }
+     ]);
+}  
