9979d975a037037ce7d7eae71049d9bb49170869
Alex Edelstein <alex@edelstein.org>
2015-10-22 11:21:02 -0700

    test

---
+++ ExceptionHandlers/MvcHandleErrorAttribute.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using Core.StructureMap;
+using Data.Entities;
+using Data.Infrastructure;
+
+using Data.Interfaces;
+using StructureMap;
+using Utilities.Logging;
+
+namespace Web.ExceptionHandling
+{
+    public class MvcHandleErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext == null)
+            {
+                throw new ArgumentNullException("filterContext");
+            }
+            if (filterContext.IsChildAction)
+            {
+                return;
+            }
+
+            // If custom errors are disabled, we need to let the normal ASP.NET exception handler
+            // execute so that the user can see useful debugging information.
+            if (filterContext.ExceptionHandled || !filterContext.HttpContext.IsCustomErrorEnabled)
+            {
+                return;
+            }
+
+            Exception exception = filterContext.Exception;
+            
+            if (!ExceptionType.IsInstanceOfType(exception))
+            {
+                return;
+            }
+
+            string controllerName = (string)filterContext.RouteData.Values["controller"];
+            string actionName = (string)filterContext.RouteData.Values["action"];
+
+            var httpException = filterContext.Exception as HttpException;
+            int statusCode = 500;
+            var view = View;
+            if (httpException != null)
+            {
+                statusCode = httpException.GetHttpCode();
+                var viewPath = String.Format("Views/Shared/{0}.cshtml", statusCode);
+                var explicitPath = Path.Combine(Utilities.Server.ServerPhysicalPath, viewPath);
+                if (File.Exists(explicitPath))
+                {
+                    view = statusCode.ToString();
+                }
+            }
+
+            Logger.GetLogger().Error("Critical internal error occured.", exception);
+           
+            HandleErrorInfo model = new HandleErrorInfo(filterContext.Exception, controllerName, actionName);
+            filterContext.Result = new ViewResult
+            {
+                ViewName = view,
+                MasterName = Master,
+                ViewData = new ViewDataDictionary<HandleErrorInfo>(model),
+                TempData = filterContext.Controller.TempData
+            };
+            filterContext.ExceptionHandled = true;
+            filterContext.HttpContext.Response.Clear();
+            filterContext.HttpContext.Response.StatusCode = statusCode;
+
+            // Certain versions of IIS will sometimes use their own error page when
+            // they detect a server error. Setting this property indicates that we
+            // want it to try to render ASP.NET MVC's error page instead.
+            filterContext.HttpContext.Response.TrySkipIisCustomErrors = true;
+
+            SaveUnhandledException(filterContext);
+
+
+        }
+        //Save unhandle exception to incident table
+        private void SaveUnhandledException(ExceptionContext filterContext)
+        {
+            using (var _uow = ContainerObjectFactory.Container.GetInstance<IUnitOfWork>())
+            {
+                IncidentDO incidentDO = new IncidentDO();
+                incidentDO.PrimaryCategory = "Error";
+                if (filterContext.Exception.Message.Contains("Validation failed"))
+                    incidentDO.SecondaryCategory = "ValidationException";
+                else
+                    incidentDO.SecondaryCategory = "ApplicationException";
+                incidentDO.Data = filterContext.Exception.Message;
+                _uow.IncidentRepository.Add(incidentDO);
+                _uow.SaveChanges();
+            }
+        
+        }
+    }
+}
---
+++ ExceptionHandlers/WebApiExceptionFilterAttribute.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http.Filters;
+using Core.Exceptions;
+using Core.Managers;
+using Core.StructureMap;
+using Data.Infrastructure;
+
+using Data.Interfaces.DataTransferObjects;
+using Newtonsoft.Json;
+using PluginBase;
+using StructureMap;
+using Utilities;
+
+namespace Web.ExceptionHandling
+{
+    /// <summary>
+    /// This exception filter handles any non-handled exception. Usually for such 
+    /// exceptions we can provide no specific or useful instructions to user. 
+    /// </summary>
+    public class WebApiExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            ErrorDTO errorDto;
+
+            var alertManager = ContainerObjectFactory.Container.GetInstance<EventReporter>();
+            var ex = context.Exception;
+
+            alertManager.UnhandledErrorCaught(
+                String.Format("Unhandled exception has occurred.\r\nError message: {0}\r\nCall stack:\r\n{1}",
+                ex.Message,
+                ex.Source));
+
+            context.Response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+
+            if (ex is AuthenticationExeception)
+            {
+                errorDto = ErrorDTO.AuthenticationError();
+            }
+            else
+            {
+                errorDto = ErrorDTO.InternalError();
+            }
+
+            errorDto.Message = "Sorry, an unexpected error has occurred while serving your request. Please try again in a few minutes.";
+           
+            // if debugging enabled send back the details of exception as well
+            if (HttpContext.Current.IsDebuggingEnabled)
+            {
+                if (ex is PluginCodedException) 
+                {
+                    var pluginEx = (PluginCodedException)ex;
+                    
+                    errorDto.Details = new
+                    {
+                        errorCode = pluginEx.ErrorCode, 
+                        message = pluginEx.ErrorCode.GetEnumDescription()
+                    };
+                }
+                else 
+                {
+                    errorDto.Details = new {exception = context.Exception};
+                }
+            }
+            
+            context.Response.Content = new StringContent(JsonConvert.SerializeObject(errorDto), Encoding.UTF8, "application/json");
+        }
+    }
+}
