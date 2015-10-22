41932ac30d5d9ab9de54672c8678dfee7d1ca491
Aleksei Avrutin <alexavrutin@gmail.com>
2015-10-23 01:36:13 +0200

    Initial commit.

---
+++ Scripts/app/controllers/SolutionListController.ts
@@ -0,0 +1,33 @@
+﻿/// <reference path="../_all.ts" />
+
+module dockyard.controllers {
+    'use strict';
+
+    export interface ISolutionListScope extends ng.IScope {
+        user: interfaces.IUserDTO;
+    }
+
+    class SolutionListController {
+        // $inject annotation.
+        // It provides $injector with information about dependencies to be injected into constructor
+        // it is better to have it close to the constructor, because the parameters must match in count and type.
+        // See http://docs.angularjs.org/guide/di
+        public static $inject = [
+            '$scope',
+            'UserService',
+            '$state'
+        ];
+
+        constructor(
+            private $scope: IAccountDetailsScope,
+            private UserService: services.IUserService,
+            private $state: ng.ui.IState) {
+
+            UserService.get({ id: $state.params.id }).$promise.then(function (data) {
+                $scope.user = data;
+            });
+        }
+    }
+
+    app.controller('SolutionListController', SolutionListController);
+}
