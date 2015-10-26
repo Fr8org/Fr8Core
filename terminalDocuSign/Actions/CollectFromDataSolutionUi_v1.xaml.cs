8bbc1baeecd8920e595566a7459fa5d60d989ddc
blazingmind <blaizingmind@gmail.com>
2015-10-26 09:26:36 +0300

    add missing file

---
+++ terminalDocuSign/Actions/CollectFromDataSolutionUi_v1.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace terminalDocuSign.Actions
+{
+    public partial class CollectFromDataSolutionUi_v1 
+    {
+        public CollectFromDataSolutionUi_v1()
+        {
+            InitializeComponent();
+        }
+    }
+}
