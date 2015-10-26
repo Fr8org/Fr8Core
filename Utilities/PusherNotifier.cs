1911255e11beb0c3caec7cc05b0ea7399cc1aa01
Dzmitry Pashkevich <d.pashkevich@itransition.com>
2015-10-26 21:10:06 +0300

    DO-1374 * PusherNotifier default values changes

--- Utilities/PusherNotifier.cs
+++ Utilities/PusherNotifier.cs
@@ -11,13 +11,20 @@ namespace Utilities
     {
         private Pusher _pusher { get; set; }
 
-        const string appId = "148580";
-        const string appKey = "123dd339500fed0ddd78";
-        const string appSecret = "598b1fdcdf903325d520";
+        const string defaultAppId = "148580";
+        const string defaultAppKey = "123dd339500fed0ddd78";
+        const string defaultAppSecret = "598b1fdcdf903325d520";
 
-        public PusherNotifier()
+        public PusherNotifier(string appId = null, string appKey = null, string appSecret = null)
         {
-            _pusher = new Pusher(appId, appKey, appSecret, new PusherOptions() { Encrypted = true });
+            if (!appId.IsNullOrEmpty() && !appKey.IsNullOrEmpty() && !appSecret.IsNullOrEmpty())
+            {
+                _pusher = new Pusher(appId, appKey, appSecret, new PusherOptions() { Encrypted = true });
+            }
+            else
+            {
+                _pusher = new Pusher(defaultAppId, defaultAppKey, defaultAppSecret, new PusherOptions() { Encrypted = true });
+            }
         }
 
         public void Notify(string channelName, string eventName, object message)
