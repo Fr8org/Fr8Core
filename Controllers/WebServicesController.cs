daca764634766ba50ecc6ae6805964f6b7443a4e
Aleksei Avrutin <alexavrutin@gmail.com>
2015-11-02 21:20:44 +0100

    Revert 1382-2

--- Controllers/WebServicesController.cs
+++ Controllers/WebServicesController.cs
@@ -1,21 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Description;
 using AutoMapper;
-using Data.Entities;
 using Data.Interfaces;
 using Data.Interfaces.DataTransferObjects;
-using Data.States;
 using StructureMap;
+using Data.Entities;
 
-namespace HubWeb.Controllers
+namespace Web.Controllers
 {
 	[RoutePrefix("webservices")]
 	public class WebServicesController : ApiController
 	{
-		[HttpGet]
-		[Route("")]
-		public IHttpActionResult GetWebServices()
+		[ResponseType(typeof(IEnumerable<WebServiceDTO>))]
+		public IHttpActionResult Get()
 		{
 			using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
 			{
@@ -27,9 +26,8 @@ namespace HubWeb.Controllers
 			}
 		}
 
-		[HttpPost]
-		[Route("")]
-		public IHttpActionResult CreateWebService(WebServiceDTO webService)
+		[ResponseType(typeof(WebServiceDTO))]
+		public IHttpActionResult Post(WebServiceDTO webService)
 		{
 			WebServiceDO entity = Mapper.Map<WebServiceDO>(webService);
 
@@ -43,40 +41,6 @@ namespace HubWeb.Controllers
 			var model = Mapper.Map<WebServiceDTO>(entity);
 
 			return Ok(model);
-		}
-
-		[HttpPost]
-		[Route("actions")]
-		public IHttpActionResult GetActions(ActivityCategory[] categories)
-		{
-			var webServiceList = new List<WebServiceActionSetDTO>();
-
-			using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
-			{
-				// Getting web services and their actions as one set, then filtering that set
-				// to get only those actions whose category matches any of categories provided
-				// resulting set is grouped into batches 1 x web service - n x actions
-				webServiceList = uow.WebServiceRepository.GetQuery()
-						.Join(uow.ActivityTemplateRepository.GetQuery(), ws => ws.Id, at => at.WebServiceId, (ws, at) => new { ws, at })
-						.Where(x => categories.Any(p => p == x.at.Category))
-						.GroupBy(x => x.ws, x => x.at, (key, group) => new
-						{
-							WebService = key,
-							Actions = group.ToList()
-						})
-						.Select(x => new WebServiceActionSetDTO
-						{
-							WebServiceIconPath = x.WebService.IconPath,
-							Actions = x.Actions.Select(p => new ActivityTemplateDTO
-							{
-								Name = p.Name
-							})
-							.ToList()
-						})
-						.ToList();
-			}
-
-			return Ok(webServiceList);
 		}
 	}
 }
--- Scripts/app/services/WebServiceService.ts
+++ Scripts/app/services/WebServiceService.ts
@@ -1,16 +1,8 @@
 ﻿
 module dockyard.services {
-	export interface IWebServiceService extends ng.resource.IResourceClass<interfaces.IWebServiceVM> {
-		getActions: (params: Array<number>) => Array<model.WebServiceActionSetDTO>
-	}
+	export interface IWebServiceService extends ng.resource.IResourceClass<interfaces.IWebServiceVM> { }
 
-	app.factory("WebServiceService", ["$resource", ($resource: ng.resource.IResourceService): IWebServiceService =>
-		<IWebServiceService> $resource("webservices/:id", { id: "@id" }, {
-			getActions: {
-				method: "POST",
-				isArray: true,
-				url: "webservices/actions"
-			}
-		})
+	app.factory('WebServiceService', ['$resource', ($resource: ng.resource.IResourceService): IWebServiceService =>
+		<IWebServiceService> $resource('api/webservices/:id', { id: '@id' }, { })
 	]);
 }
