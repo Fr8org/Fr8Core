847673ac9c98baf0b59a94c073da05e44b0f5a70
Bahadir Bozdag <bahadir.bb@gmail.com>
2015-11-04 13:48:19 +0200

    Fixed unit tests

--- Tests/DockyardTest/Services/ActionServiceTests.cs
+++ Tests/DockyardTest/Services/ActionServiceTests.cs
@@ -160,7 +160,7 @@ namespace DockyardTest.Services
 
             ISubroute subRoute = new Subroute();
             //Delete
-            subRoute.DeleteAction(actionDO.Id);
+            subRoute.DeleteAction(null, actionDO.Id, true);
         }
 
         [Test]
