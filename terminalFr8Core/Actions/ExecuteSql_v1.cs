e4fc012adebf1d05c19b64bec041526f087b6954
Yakov Gnusin <y.gnusin@gmail.com>
2015-11-04 18:22:31 +0600

    Implemented action configuration.

---
+++ terminalFr8Core/Actions/ExecuteSql_v1.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Data.Interfaces.DataTransferObjects;
+using TerminalBase.BaseClasses;
+using TerminalBase.Infrastructure;
+
+namespace terminalFr8Core.Actions
+{
+    public class ExecuteSql_v1 : BasePluginAction
+    {
+        #region Configuration
+
+        public override ConfigurationRequestType ConfigurationEvaluator(ActionDTO curActionDTO)
+        {
+            return ConfigurationRequestType.Initial;
+        }
+
+        protected override Task<ActionDTO> InitialConfigurationResponse(ActionDTO curActionDTO)
+        {
+            AddLabelControl(
+                curActionDTO,
+                "NoConfigLabel",
+                "No configuration",
+                "This action does not require any configuration."
+            );
+
+            return Task.FromResult(curActionDTO);
+        }
+
+        #endregion Configuration
+
+        #region Execution
+
+        public Task<PayloadDTO> Run(ActionDTO curActionDTO)
+        {
+            return Task.FromResult<PayloadDTO>(null);
+        }
+
+        #endregion Execution
+    }
+}
