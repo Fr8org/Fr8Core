510bb2b395825511990ce83ccab5de5941e9163c
blazingmind <blaizingmind@gmail.com>
2015-11-05 12:36:10 +0300

    missing file

---
+++ Data/Crates/ICrateStorageSerializer.cs
@@ -0,0 +1,12 @@
+﻿using Data.Interfaces.DataTransferObjects;
+
+namespace Data.Crates
+{
+    public interface ICrateStorageSerializer
+    {
+        CrateStorageDTO ConvertToProxy(CrateStorage storage);
+        CrateDTO ConvertToProxy(Crate crate);
+        CrateStorage ConvertFromProxy(CrateStorageDTO dto);
+        Crate ConvertFromProxy(CrateDTO dto);
+    }
+}
