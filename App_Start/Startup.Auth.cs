5f375c4353f8cf0c72e1679bbbc5920b593473eb
Bahadir Bozdag <bahadir.bb@gmail.com>
2015-11-06 18:37:55 +0200

    Auto Deploy Test

--- App_Start/Startup.Auth.cs
+++ App_Start/Startup.Auth.cs
@@ -12,6 +12,7 @@ namespace HubWeb
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
+
             // Enable the application to use a cookie to store information for the signed in user
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
