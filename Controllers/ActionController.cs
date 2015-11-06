3798a79b2eee867562305a0f6f841567cb9026bb
Bahadir Bozdag <bahadir.bb@gmail.com>
2015-11-06 17:50:08 +0200

    VSO Test on dev

--- Controllers/ActionController.cs
+++ Controllers/ActionController.cs
@@ -89,8 +89,7 @@ namespace HubWeb.Controllers
 
             using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
             {
-                var activityTemplate = uow.ActivityTemplateRepository.GetAll().
-                    Where(at => at.Name == solutionName).FirstOrDefault();
+                var activityTemplate = uow.ActivityTemplateRepository.GetAll().FirstOrDefault(at => at.Name == solutionName);
                 if (activityTemplate == null)
                 {
                     throw new ArgumentException(String.Format("actionTemplate (solution) name {0} is not found in the database.", solutionName));
