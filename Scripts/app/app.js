536d80f49dab7fb4c4aa128117882c9d878ce4fb
Aleksei Avrutin <alexavrutin@gmail.com>
2015-11-07 01:32:42 +0100

    Revert "Fixed terminal ports"
    This reverts commit 2c43f77727954b37f801b721b75ea7728ac421c4.

--- Scripts/app/app.js
+++ Scripts/app/app.js
@@ -62,7 +62,7 @@ app.controller('FooterController', ['$scope', function ($scope) {
 /* Setup Rounting For All Pages */
 app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $httpProvider) {
         // Install a HTTP request interceptor that causes 'Processing...' message to display
-        $httpProvider.interceptors.push(function ($q, $window) {
+        $httpProvider.interceptors.push(function ($q) {
             return {
                 request: function (config) {
                     // Show page spinner If there is no request parameter suppressSpinner.
@@ -79,10 +79,6 @@ app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function ($
                     return config;
                 },
                 responseError: function (config) {
-                    if (config.status === 403) {
-                        $window.location.href = $window.location.origin + '/DockyardAccount'
-                            + '?returnUrl=/Dashboard' + encodeURIComponent($window.location.hash);
-                    }
                     Metronic.stopPageLoading();
                     return $q.reject(config);
                 }
