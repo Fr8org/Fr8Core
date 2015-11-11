0ba2528c0b12a274d9263284a3c8f23c6563678d
Bahadir Bozdag <bahadir.bb@gmail.com>
2015-11-11 17:13:23 +0200

    initial directive test design

--- Scripts/tests/utils/fixture_ActionDesignDTO.ts
+++ Scripts/tests/utils/fixture_ActionDesignDTO.ts
@@ -9,6 +9,17 @@ module dockyard.tests.utils.fixtures {
             routeState: 1
         };
 
+        public static textField: model.TextBoxControlDefinitionDTO = {
+            required: true,
+            type: 'TextBox',
+            fieldLabel: 'test',
+            name: 'test',
+            events: [],
+            value: 'test'
+        };
+
+        public static textBlock: model.TextBlock = new model.TextBlock('test', 'well well-lg');
+
         public static configurationControls = {
             "fields":
             [
