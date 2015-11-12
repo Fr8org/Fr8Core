e68f7cf4f031635ff0d236514f2edd8f3c12bb78
Maximkaaa <maxim@gritsenko.biz>
2015-11-12 20:08:01 +0300

    DO-1379 Made duration control get configuration parameters, made the counter roll over 0 if possible

--- Scripts/app/directives/Controls/Duration.ts
+++ Scripts/app/directives/Controls/Duration.ts
@@ -15,7 +15,7 @@ module dockyard.directives.duration {
         public templateUrl = '/AngularTemplate/Duration';
         public restrict = 'E';
         public scope = {
-
+            field: '='
         }
 
         constructor() {
@@ -30,6 +30,26 @@ module dockyard.directives.duration {
                 $scope: IDurationScope,
                 $element: ng.IAugmentedJQuery,
                 $attrs: ng.IAttributes) => {
+
+                $scope.$watch(() => {
+                    if ($scope.field.minutes < 0) {
+                        if ($scope.field.hours > 0 || $scope.field.days > 0) {
+                            $scope.field.hours--;
+                            $scope.field.minutes += 60;
+                        } else {
+                            $scope.field.minutes = 0;
+                        }
+                    }
+
+                    if ($scope.field.hours < 0) {
+                        if ($scope.field.days > 0) {
+                            $scope.field.days--;
+                            $scope.field.hours += 24;
+                        } else {
+                            $scope.field.hours = 0;
+                        }
+                    }
+                });
 
             }
 
