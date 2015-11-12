123e33da48e6f76eb2c81043d4fa340e69ea3fad
Bahadir Bozdag <bahadir.bb@gmail.com>
2015-11-12 13:31:25 +0200

    Added tests for dropdownListBox

--- Scripts/tests/unit/directives/controls/DropDownListBoxTests.ts
+++ Scripts/tests/unit/directives/controls/DropDownListBoxTests.ts
@@ -1,6 +1,6 @@
 ﻿/// <reference path="../../../../app/_all.ts" />
 /// <reference path="../../../../typings/angularjs/angular-mocks.d.ts" />
-/*
+
 module dockyard.tests.unit.directives.controls {
     import fx = utils.fixtures; // just an alias
 
@@ -20,5 +20,51 @@ module dockyard.tests.unit.directives.controls {
             //which is myaccount and has template URL with /AngularTemplate/MyAccountPage
             _$httpBackend_.expectGET('/AngularTemplate/MyAccountPage').respond(200, '<div></div>');
         }]);
+
+
+        beforeEach(() => {
+
+            inject((_$compile_, _$rootScope_, _$timeout_) => {
+                $rootScope = _$rootScope_;
+                $compile = _$compile_;
+                $timeout = _$timeout_;
+                
+                scope = $rootScope.$new();
+                scope.onChange = jasmine.createSpy("onChange function");
+                //we copy data to prevent changes on ongoing tests
+                scope.field = angular.copy(fx.ActionDesignDTO.dropDownListBox);
+                element = compileTemplate(scope, directive);
+            });
+        });
+
+        var compileTemplate = (localScope, rawTemplate) => {
+            var template = angular.element(rawTemplate);
+            var elem = $compile(template)(localScope);
+            localScope.$digest();
+            return elem;
+        };
+
+        it('Should have an isolateScope', () => {
+            expect(element.isolateScope()).not.toBe(null);
+        });
+        
+        it('Should set selected item', () => {
+            var dirScope = element.isolateScope();
+            dirScope.setSelectedItem(scope.field.listItems[1]);
+            expect(dirScope.field.value).toBe(scope.field.listItems[1].value);
+            expect(dirScope.selectedItem).toBe(scope.field.listItems[1]);
+        });
+
+        it('Should call onChange function when new item is selected', () => {
+            var dirScope = element.isolateScope();
+            dirScope.setSelectedItem(scope.field.listItems[1]);
+            expect(scope.onChange).toHaveBeenCalled();
+        });
+
+        it('Should set initial selection', () => {
+            var dirScope = element.isolateScope();
+            expect(dirScope.selectedItem).toBe(scope.field.listItems[2]);
+        });
+
     });
-} */
+} 
