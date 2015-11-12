3ed8034e849147bb87cc5657b9d22fdb8a051b5b
pharrukh <farrukh.normuradov@gmail.com>
2015-11-12 21:01:40 +0500

    minor refactoring of terminalGoogle

---
+++ terminalGoogle/Controllers/TerminalController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Data.Entities;
+using Data.Interfaces.Manifests;
+using Data.States;
+using Google.GData.Extensions;
+using Utilities.Configuration.Azure;
+
+namespace terminalGoogle.Controllers
+{
+    [RoutePrefix("terminals")]
+    public class TerminalController : ApiController
+    {
+        /// <summary>
+        /// Plugin discovery infrastructure.
+        /// Action returns list of supported actions by plugin.
+        /// </summary>
+        [HttpGet]
+        [Route("discover")]
+        [ResponseType(typeof(StandardFr8TerminalCM))]
+        public IHttpActionResult DiscoverTerminals()
+        {
+            var terminal = new TerminalDO()
+            {
+                Endpoint = CloudConfigurationManager.GetSetting("TerminalEndpoint"),
+                TerminalStatus = TerminalStatus.Active,
+                Name = "terminalGoogle",
+                Version = "1"
+            };
+
+            var extractDataAction = new ActivityTemplateDO
+            {
+                Name = "Extract_Spreadsheet_Data",
+                Label = "Extract Spreadsheet Data",
+                Version = "1",
+                Category = ActivityCategory.Receivers,
+                Terminal = terminal,
+                AuthenticationType = AuthenticationType.External,
+                MinPaneWidth = 300
+            };
+
+            return Json(new StandardFr8TerminalCM()
+            {
+                Definition = terminal,
+                Actions = new List<ActivityTemplateDO>
+                {
+                    extractDataAction
+                }
+            });    
+        }
+    }
+}
