b4857932d2d28a38b0d2128c7e408ff0b2beda04
Maximkaaa <maxim@gritsenko.biz>
2015-11-16 09:57:33 +0300

    DO-1569 Moved fixtures to a separate file

--- Scripts/tests/unit/services/CrateHelperTests.ts
+++ Scripts/tests/unit/services/CrateHelperTests.ts
@@ -5,7 +5,7 @@
 module dockyard.tests.controller {
 
     import CrateHelper = dockyard.services.CrateHelper;
-
+    import fx = utils.fixtures;
 
 
     describe('CrateHelper', () => {
@@ -13,311 +13,11 @@ module dockyard.tests.controller {
         var ch, crateStorage, emptyStorage, duplicateCrateStorage, controlList, fields;
         beforeEach(() => {
             ch = new CrateHelper();
-
-            crateStorage = {
-                "crates": [
-                    {
-                        "id": "435532ca-b294-44c5-be09-f72338b8dd3e",
-                        "label": "Configuration_Controls",
-                        "contents": {
-                            "Controls": [
-                                {
-                                    "groupName": "SMSNumber_Group",
-                                    "radios": [
-                                        {
-                                            "selected": true,
-                                            "name": "SMSNumberOption",
-                                            "value": "SMS Number",
-                                            "controls": [
-                                                {
-                                                    "name": "SMS_Number",
-                                                    "required": true,
-                                                    "value": null,
-                                                    "label": null,
-                                                    "type": "TextBox",
-                                                    "selected": false,
-                                                    "events": null,
-                                                    "source": null
-                                                }
-                                            ]
-                                        },
-                                        {
-                                            "selected": true,
-                                            "name": "SMSNumberOption",
-                                            "value": "A value from Upstream Crate",
-                                            "controls": [
-                                                {
-                                                    "listItems": [
-
-                                                    ],
-                                                    "name": "upstream_crate",
-                                                    "required": false,
-                                                    "value": null,
-                                                    "label": null,
-                                                    "type": "DropDownList",
-                                                    "selected": false,
-                                                    "events": [
-                                                        {
-                                                            "name": "onChange",
-                                                            "handler": "requestConfig"
-                                                        }
-                                                    ],
-                                                    "source": {
-                                                        "manifestType": "Standard Design-Time Fields",
-                                                        "label": "Available Fields"
-                                                    }
-                                                }
-                                            ]
-                                        }
-                                    ],
-                                    "name": null,
-                                    "required": false,
-                                    "value": null,
-                                    "label": "For the SMS Number use:",
-                                    "type": "RadioButtonGroup",
-                                    "selected": false,
-                                    "events": null,
-                                    "source": null
-                                },
-                                {
-                                    "name": "SMS_Body",
-                                    "required": true,
-                                    "value": null,
-                                    "label": "SMS Body",
-                                    "type": "TextBox",
-                                    "selected": false,
-                                    "events": null,
-                                    "source": null
-                                }
-                            ]
-                        },
-                        "parentCrateId": null,
-                        "manifestType": "Standard Configuration Controls",
-                        "manifestId": 6,
-                        "manufacturer": null,
-                        "createTime": "0001-01-01T00:00:00"
-                    },
-                    {
-                        "id": "97f09d75-6810-45c3-9bdb-56912e4606c8",
-                        "label": "Available Fields",
-                        "contents": {
-                            "Fields": [
-                                {
-                                    "key": "+15005550006",
-                                    "value": "+15005550006"
-                                }
-                            ]
-                        },
-                        "parentCrateId": null,
-                        "manifestType": "Standard Design-Time Fields",
-                        "manifestId": 3,
-                        "manufacturer": null,
-                        "createTime": "0001-01-01T00:00:00"
-                    },
-                    {
-                        "id": "97f09d75-6810-45c3-9bdb-56912e4606c8",
-                        "label": "Available Fields1",
-                        "contents": {
-                            "Fields": [
-                                {
-                                    "key": "Test",
-                                    "value": "test"
-                                }
-                            ]
-                        },
-                        "parentCrateId": null,
-                        "manifestType": "Standard Design-Time Fields1",
-                        "manifestId": 5,
-                        "manufacturer": null,
-                        "createTime": "0001-01-01T00:00:00"
-                    },
-                    {
-                        "id": "97f09d75-6810-45c3-9bdb-56912e4606c8",
-                        "label": "Available Fields2",
-                        "contents": {
-                            "Fields": [
-                                {
-                                    "key": "Test1",
-                                    "value": "test1"
-                                }
-                            ]
-                        },
-                        "parentCrateId": null,
-                        "manifestType": "Standard Design-Time Fields2",
-                        "manifestId": 5,
-                        "manufacturer": null,
-                        "createTime": "0001-01-01T00:00:00"
-                    }
-                ]
-            };
-
-            emptyStorage = {
-                crates: []
-            };
-
-            duplicateCrateStorage = {
-                crates: [
-                    {
-                        "id": "97f09d75-6810-45c3-9bdb-56912e4606c8",
-                        "label": "Available Fields",
-                        "contents": {
-                            "Fields": [
-                                {
-                                    "key": "+15005550006",
-                                    "value": "+15005550006"
-                                }
-                            ]
-                        },
-                        "parentCrateId": null,
-                        "manifestType": "Standard Design-Time Fields",
-                        "manifestId": 3,
-                        "manufacturer": null,
-                        "createTime": "0001-01-01T00:00:00"
-                    },
-                    {
-                        "id": "97f09d75-6810-45c3-9bdb-56912e4606c8",
-                        "label": "Available Fields",
-                        "contents": {
-                            "Fields": [
-                                {
-                                    "key": "+15005550006",
-                                    "value": "+15005550006"
-                                }
-                            ]
-                        },
-                        "parentCrateId": null,
-                        "manifestType": "Standard Design-Time Fields",
-                        "manifestId": 3,
-                        "manufacturer": null,
-                        "createTime": "0001-01-01T00:00:00"
-                    }
-                ]
-            };
-
-            controlList = {
-                fields: [
-                    {
-                        "name": "SMS_Number",
-                        "required": true,
-                        "value": null,
-                        "label": null,
-                        "type": "TextBox",
-                        "selected": false,
-                        "events": null,
-                        "source": null
-                    },
-                    {
-                        "name": "SMS_Body",
-                        "required": true,
-                        "value": null,
-                        "label": "SMS Body",
-                        "type": "TextBox",
-                        "selected": false,
-                        "events": null,
-                        "source": null
-                    }
-                ]
-            };
-
-            fields = [
-                {
-                    "groupName": "SMSNumber_Group",
-                    "radios": [
-                        {
-                            "selected": true,
-                            "name": "SMSNumberOption",
-                            "value": "SMS Number",
-                            "controls": [
-                                {
-                                    "name": "SMS_Number",
-                                    "required": true,
-                                    "value": null,
-                                    "label": null,
-                                    "type": "TextBox",
-                                    "selected": false,
-                                    "events": null,
-                                    "source": null
-                                }
-                            ]
-                        },
-                        {
-                            "selected": true,
-                            "name": "SMSNumberOption",
-                            "value": "A value from Upstream Crate",
-                            "controls": [
-                                {
-                                    "listItems": [],
-                                    "name": "upstream_crate",
-                                    "required": false,
-                                    "value": null,
-                                    "label": null,
-                                    "type": "DropDownList",
-                                    "selected": false,
-                                    "events": [
-                                        {
-                                            "name": "onChange",
-                                            "handler": "requestConfig"
-                                        }
-                                    ],
-                                    "source": {
-                                        "manifestType": "Standard Design-Time Fields",
-                                        "label": "Available Fields"
-                                    }
-                                },
-                                {
-                                    "listItems": [],
-                                    "name": "upstream_crate",
-                                    "required": false,
-                                    "value": null,
-                                    "label": null,
-                                    "type": "TextSource",
-                                    "selected": false,
-                                    "events": [],
-                                    "source": {
-                                        "manifestType": "Standard Design-Time Fields2",
-                                        "label": "Available Fields2"
-                                    },
-                                    "initialLabel": "Initial label",
-                                    "valueSource": "Value source"
-                                }
-
-                            ]
-                        }
-                    ],
-                    "name": null,
-                    "required": false,
-                    "value": null,
-                    "label": "For the SMS Number use:",
-                    "type": "RadioButtonGroup",
-                    "selected": false,
-                    "events": null,
-                    "source": null
-                },
-                {
-                    "name": "SMS_Body",
-                    "required": true,
-                    "value": null,
-                    "label": "SMS Body",
-                    "type": "TextBox",
-                    "selected": false,
-                    "events": null,
-                    "source": null
-                },
-                {
-                    "listItems": [],
-                    "name": "upstream_crate1",
-                    "required": false,
-                    "value": null,
-                    "label": null,
-                    "type": "DropDownList",
-                    "selected": false,
-                    "events": [],
-                    "source": {
-                        "manifestType": "Standard Design-Time Fields1",
-                        "label": "Available Fields1"
-                    }
-                }
-            ];
+            crateStorage = $.extend(true, {}, fx.CrateHelper.sampleStorage);
+            emptyStorage = $.extend(true, {}, fx.CrateHelper.emptyStorage);
+            duplicateCrateStorage = $.extend(true, {}, fx.CrateHelper.duplicateStorage);
+            controlList = $.extend(true, {}, fx.CrateHelper.controlsList);
+            fields = $.extend(true, [], fx.CrateHelper.fields);
         });
 
         describe('.findByLabel()', () => {
@@ -405,7 +105,7 @@ module dockyard.tests.controller {
             });
 
             it('should not change other crates', () => {
-                var copy = $.extend({}, crateStorage);
+                var copy = $.extend(true, {}, crateStorage);
                 ch.mergeControlListCrate(controlList, crateStorage);
                 copy.crates.forEach((crate, index) => {
                     if (crate.manifestType !== 'Standard Configuration Controls') {
@@ -458,7 +158,7 @@ module dockyard.tests.controller {
             });
 
             it('should not change other fields', () => {
-                var copy = $.extend({}, flatFieldList);
+                var copy = $.extend(true, {}, flatFieldList);
                 flatFieldList.forEach((field, index) => {
                     if (field.type !== 'DropDownList' && field.type !== 'TextSource' && !field.radios && !field.controls) {
                         expect(field).toEqual(copy[index]);
