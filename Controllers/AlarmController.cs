562232ae920ae518758da44028c24bf05b4ce616
Dmitry Pashkevich <d.pashkevich@leckerweb.com>
2015-11-24 01:57:21 +0300

    DO-1461  + Added check in parsing of values  * Renamed some methods and fields

--- Controllers/AlarmController.cs
+++ Controllers/AlarmController.cs
@@ -16,31 +16,37 @@ using System.Web.Http;
 
 namespace HubWeb.Controllers
 {
-    [RoutePrefix("api/alarm")]
+    [RoutePrefix("api/alarms")]
     public class AlarmController : ApiController
     {
-        [Route("notify")]
         [HttpPost]
-        public async Task<IHttpActionResult> Notify(AlarmDTO alarmDTO)
+        [Route("")]
+        public async Task<IHttpActionResult> SetAlarm(AlarmDTO alarmDTO)
         {
-            var startTime = DateTimeOffset.Parse(alarmDTO.startTime);
-            var containerId = Guid.Parse(alarmDTO.containerId);
-            var terminalName = alarmDTO.terminalName;
-            var terminalVersion = alarmDTO.terminalVersion;
+            DateTimeOffset startTime;
+            if (!DateTimeOffset.TryParse(alarmDTO.StartTime, out startTime))
+            {
+                return ResponseMessage(
+                    Request.CreateErrorResponse(HttpStatusCode.BadRequest, new ArgumentException("\'StartTime\' has invalid format", "startTime")));
+            }
 
-            return await Notify(startTime, containerId, terminalName, terminalVersion);
+            Guid containerId = alarmDTO.ContainerId;
+            string terminalName = alarmDTO.TerminalName;
+            string terminalVersion = alarmDTO.TerminalVersion;
+            
+            return await SetAlarm(startTime, containerId, terminalName, terminalVersion);
         }
         
-        public async Task<IHttpActionResult> Notify(DateTimeOffset startTime, Guid containerId, string terminalName, string terminalVersion)
+        public async Task<IHttpActionResult> SetAlarm(DateTimeOffset startTime, Guid containerId, string terminalName, string terminalVersion)
         {
-            Expression<Action> action = () => VerifyContainer(startTime, containerId, terminalName, terminalVersion);
+            Expression<Action> action = () => ExecuteTerminalWithLogging(startTime, containerId, terminalName, terminalVersion);
             BackgroundJob.Schedule(action, startTime);
 
             var eventController = new EventController();
             return await eventController.ProcessIncomingEvents(terminalName, terminalVersion);
         }
         
-        public async void VerifyContainer(DateTimeOffset startTime, Guid containerId, string terminalName, string terminalVersion)
+        public async void ExecuteTerminalWithLogging(DateTimeOffset startTime, Guid containerId, string terminalName, string terminalVersion)
         {
             HttpResponseMessage result = null;
             using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
--- Data/Interfaces/DataTransferObjects/AlarmDTO.cs
+++ Data/Interfaces/DataTransferObjects/AlarmDTO.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace Data.Interfaces.DataTransferObjects
 {
     public class AlarmDTO
     {
-        public string startTime { get; set; }
-        public string containerId { get; set; }
-        public string terminalName { get; set; }
-        public string terminalVersion { get; set; }
+        [JsonProperty("startTime")]
+        public string StartTime { get; set; }
+
+        [JsonProperty("containerId")]
+        public Guid ContainerId { get; set; }
+
+        [JsonProperty("terminalName")]
+        public string TerminalName { get; set; }
+
+        [JsonProperty("terminalVersion")]
+        public string TerminalVersion { get; set; }
     }
 }
