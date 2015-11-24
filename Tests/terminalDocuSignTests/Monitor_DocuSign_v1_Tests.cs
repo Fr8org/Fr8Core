50669c1f489c724942f95890514d482ebfab9992
Yakov Gnusin <y.gnusin@gmail.com>
2015-11-24 13:49:00 +0600

    Improvements for TestRunner to run explicit tests and make CI ignore specific tests.

--- Tests/terminalDocuSignTests/Monitor_DocuSign_v1_Tests.cs
+++ Tests/terminalDocuSignTests/Monitor_DocuSign_v1_Tests.cs
@@ -2,11 +2,13 @@
 
 namespace terminalDocuSignTests
 {
+    [Explicit]
     public class Monitor_DocuSign_v1_Tests
     {
         [Test]
         public void SampleTest()
         {
+            throw new System.Exception("Test if it passes CI build");
         }
     }
 }
