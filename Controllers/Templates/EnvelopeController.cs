cfc01a60649a1f9d00557d217494aa66ebc18959
pharrukh <farrukh.normuradov@gmail.com>
2015-11-28 23:09:42 +0500

    DO-1694: upgraded Microsoft.Owin.* packages -> 3.0.1

---
+++ Controllers/Templates/EnvelopeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace HubWeb.Controllers
+{
+    public class EnvelopeController : Controller
+    {
+        // GET: Envelope
+        public ActionResult Index()
+        {
+            var query_params = new Dictionary<string, string>();
+            query_params["from_date"] = "2015-05-11T07:00:00.0000000Z";
+            //var foo = _API.GetEnvelope(query_params);
+            return View();
+        }
+    }
+}
