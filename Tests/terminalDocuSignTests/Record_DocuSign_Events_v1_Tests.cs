7a4e8b1b0ec975b75bcc3c931fce1209b1661cb6
Anton <tarakanov.anton@gmail.com>
2015-12-01 01:30:25 +0400

    Added tests

--- Tests/terminalDocuSignTests/Record_DocuSign_Events_v1_Tests.cs
+++ Tests/terminalDocuSignTests/Record_DocuSign_Events_v1_Tests.cs
@@ -1,7 +1,10 @@
 ﻿using Data.Control;
 using Data.Crates;
+using Data.Interfaces.DataTransferObjects;
 using Data.Interfaces.Manifests;
 using HealthMonitor.Utility;
+using Hub.Managers.APIManagers.Transmitters.Restful;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -32,7 +35,7 @@ namespace terminalDocuSignTests
         {
             Assert.AreEqual(1, control.Controls.Count);
 
-            // Assert that first control is a RadioButtonGroup 
+            // Assert that first control is a TextBlock 
             // with Label == "Monitor All DocuSign events"
             // with Value == "This Action doesn't require any configuration."
             Assert.IsTrue(control.Controls[0] is TextBlock);
@@ -42,7 +45,77 @@ namespace terminalDocuSignTests
 
         private void AssertList(EventSubscriptionCM control)
         {
+            Assert.IsTrue(control.Subscriptions.Count > 0);
+        }
 
+        private void AssertCrateLabel(Crate crate, string label)
+        {
+            Assert.AreEqual(crate.Label, label);
+        }
+
+        /// <summary>
+        /// Validate correct crate-storage structure in initial configuration response.
+        /// </summary>
+        [Test]
+        public async void Record_DocuSign_Events_Initial_Configuration_Check_Crate_Structure()
+        {
+            var configureUrl = GetTerminalConfigureUrl();
+
+            var requestActionDTO = HealthMonitor_FixtureData.Record_Docusign_v1_InitialConfiguration_ActionDTO();
+
+            var responseActionDTO =
+                await HttpPostAsync<ActionDTO, ActionDTO>(
+                    configureUrl,
+                    requestActionDTO
+                );
+
+            Assert.NotNull(responseActionDTO);
+            Assert.NotNull(responseActionDTO.CrateStorage);
+            Assert.NotNull(responseActionDTO.CrateStorage.Crates);
+
+            var crateStorage = Crate.FromDto(responseActionDTO.CrateStorage);
+            AssertCrateTypes(crateStorage);
+            AssertControls(crateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().Single());
+            AssertList(crateStorage.CrateContentsOfType<EventSubscriptionCM>().Single());
+        }
+
+        /// <summary>
+        /// Wait for HTTP-500 exception when Auth-Token is not passed to initial configuration.
+        /// </summary>
+        [Test]
+        [ExpectedException(
+            ExpectedException = typeof(RestfulServiceException),
+            ExpectedMessage = @"{""status"":""terminal_error"",""message"":""One or more errors occurred.""}"
+        )]
+        public async void Record_DocuSign_Events_Initial_Configuration_NoAuth()
+        {
+            var configureUrl = GetTerminalConfigureUrl();
+
+            var requestActionDTO = HealthMonitor_FixtureData.Receive_DocuSign_Envelope_v1_Example_ActionDTO();
+            requestActionDTO.AuthToken = null;
+
+            await HttpPostAsync<ActionDTO, JToken>(
+                configureUrl,
+                requestActionDTO
+            );
+        }
+
+        /// <summary>
+        /// Test run-time without Auth-Token.
+        /// </summary>
+        [Test]
+        [ExpectedException(
+            ExpectedException = typeof(RestfulServiceException),
+            ExpectedMessage = @"{""status"":""terminal_error"",""message"":""No AuthToken provided.""}"
+        )]
+        public async void Record_DocuSign_Events_Run_NoAuth()
+        {
+            var runUrl = GetTerminalRunUrl();
+
+            var requestActionDTO = HealthMonitor_FixtureData.Receive_DocuSign_Envelope_v1_Example_ActionDTO();
+            requestActionDTO.AuthToken = null;
+
+            await HttpPostAsync<ActionDTO, PayloadDTO>(runUrl, requestActionDTO);
         }
     }
 }
