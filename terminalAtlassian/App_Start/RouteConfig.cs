68acf581342625a112ce0436ca616369ea7cca8a
nishantdev13 <nishantdev13@gmail.com>
2015-12-07 01:18:27 +0530

    DO-1245: update cloud settings

--- terminalAtlassian/App_Start/RouteConfig.cs
+++ terminalAtlassian/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using TerminalBase.BaseClasses;
 
 namespace terminalAtlassian
 {
@@ -13,11 +14,7 @@ namespace terminalAtlassian
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            BaseTerminalWebApiConfig.RegisterRoutes("Atlassian", routes);
         }
     }
 }
