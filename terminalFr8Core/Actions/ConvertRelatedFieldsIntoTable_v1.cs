28e26ad0928a969fd487b796baf99b3743f4fb21
Bahadir Bozdag <bahadir.bb@gmail.com>
2015-12-07 12:50:45 +0200

    Initial version of ConvertRelatedFieldsIntoTable

---
+++ terminalFr8Core/Actions/ConvertRelatedFieldsIntoTable_v1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Data.Control;
+using Data.Crates;
+using Data.Entities;
+using Data.Infrastructure;
+using Data.Interfaces.DataTransferObjects;
+using Data.Interfaces.Manifests;
+using Data.States;
+using Hub.Managers;
+using Newtonsoft.Json;
+using TerminalBase.BaseClasses;
+using TerminalBase.Infrastructure;
+using terminalFr8Core.Interfaces;
+
+namespace terminalFr8Core.Actions
+{
+
+    public class ConvertRelatedFieldsIntoTable_v1 : BaseTerminalAction
+    {
+
+        public ConvertRelatedFieldsIntoTable_v1()
+        {
+        }
+
+        /// <summary>
+        /// Action processing infrastructure.
+        /// </summary>
+        public async Task<PayloadDTO> Run(ActionDO curActionDO, Guid containerId, AuthorizationTokenDO authTokenDO)
+        {
+            var curPayloadDTO = await GetProcessPayload(curActionDO, containerId);
+
+            var controlsMS = Action.GetControlsManifest(curActionDO);
+
+            ControlDefinitionDTO filterPaneControl = controlsMS.Controls.FirstOrDefault(x => x.Type == ControlTypes.FilterPane);
+            if (filterPaneControl == null)
+            {
+                throw new ApplicationException("No control found with Type == \"filterPane\"");
+            }
+
+            var valuesCrates = Crate.FromDto(curPayloadDTO.CrateStorage).CrateContentsOfType<StandardPayloadDataCM>();
+            var curValues = new List<FieldDTO>();
+
+            foreach (var valuesCrate in valuesCrates)
+            {
+                curValues.AddRange(valuesCrate.AllValues());
+            }
+
+            return curPayloadDTO;
+        }
+
+        /// <summary>
+        /// Configure infrastructure.
+        /// </summary>
+        public override async Task<ActionDO> Configure(ActionDO curActionDataPackageDO, AuthorizationTokenDO authToken)
+        {
+            return await ProcessConfigurationRequest(curActionDataPackageDO, ConfigurationEvaluator, authToken);
+        }
+
+        private Crate PackCrate_ConfigurationControls()
+        {
+            var fieldSelectPrefix = new TextBox()
+            {
+                Label = "All field data that starts with the prefix",
+                Name = "Selected_Table_Prefix",
+                Required = true,
+                Events = new List<ControlEvent>()
+                {
+                    new ControlEvent("onChange", "requestConfig")
+                }
+            };
+
+            var fieldExplanation = new TextBlock()
+            {
+                Label = "followed by an integer will be grouped into a Table Row. Example field name: 'Line3_TravelExpense",
+                Name = "Field_Explanation",
+            };
+
+
+            return PackControlsCrate(fieldSelectPrefix, fieldExplanation);
+        }
+
+        /// <summary>
+        /// Looks for first Create with Id == "Standard Design-Time" among all upcoming Actions.
+        /// </summary>
+        protected override async Task<ActionDO> InitialConfigurationResponse(ActionDO curActionDO, AuthorizationTokenDO authTokenDO)
+        {
+            
+            if (curActionDO.Id != Guid.Empty)
+            {
+                //this conversion from actiondto to Action should be moved back to the controller edge
+                /*var curUpstreamFields =
+                    (await GetDesignTimeFields(curActionDO, CrateDirection.Upstream))
+                    .Fields
+                    .ToArray();*/
+
+                //2) Pack the merged fields into a new crate that can be used to populate the dropdownlistbox
+                //var queryFieldsCrate = Crate.CreateDesignTimeFieldsCrate("Queryable Criteria", curUpstreamFields);
+
+                //build a controls crate to render the pane
+                var configurationControlsCrate = PackCrate_ConfigurationControls();
+
+                using (var updater = Crate.UpdateStorage(() => curActionDO.CrateStorage))
+                {
+                    updater.CrateStorage = AssembleCrateStorage(/*queryFieldsCrate, */configurationControlsCrate);
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Configuration requires the submission of an Action that has a real ActionId");
+            }
+
+            return curActionDO;
+        }
+
+
+        public override ConfigurationRequestType ConfigurationEvaluator(ActionDO curActionDataPackageDO)
+        {
+            if (Crate.IsStorageEmpty(curActionDataPackageDO))
+            {
+                return ConfigurationRequestType.Initial;
+            }
+            var storage = Crate.GetStorage(curActionDataPackageDO);
+            var hasControlsCrate = storage.CrateContentsOfType<StandardDesignTimeFieldsCM>(x => x.Label == "Configuration_Controls").Any();
+
+            if (hasControlsCrate)
+            {
+                return ConfigurationRequestType.Followup;
+            }
+            
+            return ConfigurationRequestType.Initial;
+            
+        }
+    }
+}
