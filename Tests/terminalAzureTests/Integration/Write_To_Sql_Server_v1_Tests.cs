852e1014817e32144bad799e6caf0a1c9dc4c243
Anton <tarakanov.anton@gmail.com>
2015-12-07 15:23:58 +0300

    Added terminalAzureTests project

---
+++ Tests/terminalAzureTests/Integration/Write_To_Sql_Server_v1_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Data.Control;
+using Data.Crates;
+using Data.Interfaces.DataTransferObjects;
+using Data.Interfaces.Manifests;
+using HealthMonitor.Utility;
+using Hub.Managers;
+using NUnit.Framework;
+
+namespace terminalAzureTests.Integration
+{
+    [Explicit]
+    public class Write_To_Sql_Server_v1_Tests : BaseHealthMonitorTest
+    {
+        public override string TerminalName
+        {
+            get { return "terminalAzure"; }
+        }
+    }
+}
