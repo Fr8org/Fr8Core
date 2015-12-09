1cfef2ec9ffd8de47a6b21aaaaac3210b44fdf7b
pharrukh <farrukh.normuradov@gmail.com>
2015-12-10 02:27:28 +0500

    FR-1733: added .csproj file

---
+++ terminalTwilioTests/Fixture/HealthMonitor_FixtureData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Data.Interfaces.DataTransferObjects;
+
+namespace terminalTwilioTests.Fixture
+{
+    class HealthMonitor_FixtureData
+    {
+        public static ActivityTemplateDTO Send_Via_Twilio_v1_ActivityTemplate()
+        {
+            return new ActivityTemplateDTO()
+            {
+                Id = 1,
+                Name = "Send_Via_Twilio_TEST",
+                Version = "1"
+            };
+        }
+        public static ActionDTO Send_Via_Twilio_v1_InitialConfiguration_ActionDTO()
+        {
+            var activityTemplate = Send_Via_Twilio_v1_ActivityTemplate();
+
+            return new ActionDTO()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Send_Via_Twilio",
+                Label = "Send Via Twilio",
+                AuthToken = Google_AuthToken1(),
+                ActivityTemplate = activityTemplate,
+                ActivityTemplateId = activityTemplate.Id
+            };
+        }
+        public static AuthorizationTokenDTO Google_AuthToken1()
+        {
+            return new AuthorizationTokenDTO()
+            {
+                Token = @"{""AccessToken"":""ya29.OgLf-SvZTHJcdN9tIeNEjsuhIPR4b7KBoxNOuELd0T4qFYEa001kslf31Lme9OQCl6S5"",""RefreshToken"":""1/04H9hNCEo4vfX0nHHEdViZKz1CtesK8ByZ_TOikwVDc"",""Expires"":""2015-11-28T13:29:12.653075+05:00""}"
+            };
+        }
+    }
+}
---
+++ terminalTwilioTests/Integration/Send_Via_Twilio_v1Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using Data.Control;
+using Data.Crates;
+using Data.Interfaces.DataTransferObjects;
+using Data.Interfaces.Manifests;
+using HealthMonitor.Utility;
+using Hub.Managers.APIManagers.Transmitters.Restful;
+using NUnit.Framework;
+using terminalTwilioTests.Fixture;
+
+namespace terminalTwilioTests.Integration
+{
+    /// <summary>
+    /// Mark test case class with [Explicit] attiribute.
+    /// It prevents test case from running when CI is building the solution,
+    /// but allows to trigger that class from HealthMonitor.
+    /// </summary>
+    [Explicit]
+    public class Send_Via_Twilio_v1Tests : BaseHealthMonitorTest
+    {
+        public override string TerminalName
+        {
+            get { return "terminalTwilio"; }
+        }
+        /// <summary>
+        /// Validate correct crate-storage structure in initial configuration response.
+        /// </summary>
+        [Test, Category("Integration.terminalTwilio")]
+        public async void Send_Via_Twilio_Initial_Configuration_Check_Crate_Structure()
+        {
+            //Arrange
+            var configureUrl = GetTerminalRunUrl();
+
+            var requestActionDTO = HealthMonitor_FixtureData.Send_Via_Twilio_v1_InitialConfiguration_ActionDTO();
+            requestActionDTO.AuthToken = null;
+            //Act
+            await HttpPostAsync<ActionDTO, ActionDTO>(
+                    configureUrl,
+                    requestActionDTO
+                    );
+
+            //Assert
+            //Assert.NotNull(responseActionDTO);
+            //Assert.NotNull(responseActionDTO.CrateStorage);
+            //Assert.NotNull(responseActionDTO.CrateStorage.Crates);
+
+        }
+
+
+    }
+}
