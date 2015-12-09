8c31a2a41b55c72cace13ae890a173c4969feefe
pharrukh <farrukh.normuradov@gmail.com>
2015-12-10 02:38:13 +0500

    FR-1733: added .csproj file

--- terminalTwilioTests/Fixture/HealthMonitor_FixtureData.cs
+++ terminalTwilioTests/Fixture/HealthMonitor_FixtureData.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Control;
+using Data.Crates;
 using Data.Interfaces.DataTransferObjects;
+using Data.Interfaces.Manifests;
+using Data.States;
+using Hub.Managers;
 
 namespace terminalTwilioTests.Fixture
 {
     class HealthMonitor_FixtureData
     {
+        protected ICrateManager CrateManager;
+        public HealthMonitor_FixtureData()
+        {
+
+            CrateManager = new CrateManager();
+        }
         public static ActivityTemplateDTO Send_Via_Twilio_v1_ActivityTemplate()
         {
             return new ActivityTemplateDTO()
             {
                 Id = 1,
                 Name = "Send_Via_Twilio_TEST",
-                Version = "1"
+                Version = "1",
+                AuthenticationType = AuthenticationType.None
             };
         }
         public static ActionDTO Send_Via_Twilio_v1_InitialConfiguration_ActionDTO()
@@ -23,16 +40,60 @@ namespace terminalTwilioTests.Fixture
                 Id = Guid.NewGuid(),
                 Name = "Send_Via_Twilio",
                 Label = "Send Via Twilio",
-                AuthToken = Google_AuthToken1(),
+                AuthToken = null,
                 ActivityTemplate = activityTemplate,
                 ActivityTemplateId = activityTemplate.Id
             };
         }
-        public static AuthorizationTokenDTO Google_AuthToken1()
+        public ActionDTO Send_Via_Twilio_v1_Preconfigured_Crate_With_No_SMS_Number()
         {
-            return new AuthorizationTokenDTO()
+            var curActionDTO = Send_Via_Twilio_v1_InitialConfiguration_ActionDTO();
+            using (var updater = CrateManager.UpdateStorage(curActionDTO))
             {
-                Token = @"{""AccessToken"":""ya29.OgLf-SvZTHJcdN9tIeNEjsuhIPR4b7KBoxNOuELd0T4qFYEa001kslf31Lme9OQCl6S5"",""RefreshToken"":""1/04H9hNCEo4vfX0nHHEdViZKz1CtesK8ByZ_TOikwVDc"",""Expires"":""2015-11-28T13:29:12.653075+05:00""}"
+                var curCrate = No_SMS_Number_Controls();
+                updater.CrateStorage.Add(curCrate);
+            }
+            return curActionDTO;
+        }
+        public Crate No_SMS_Number_Controls()
+        {
+            var fieldsDTO = new List<ControlDefinitionDTO>()
+            {
+                new TextSource()
+                {
+                    UpstreamSourceLabel = "Upstream Terminal-Provided Fields",
+                    InitialLabel = "SMS Number",
+                    Name = "SMS_Number",
+                    Value = "15005550006",
+                    Label = "SMS Number",
+                },
+                new TextBox()
+                {
+                    Label = "SMS Body",
+                    Name = "SMS_Body",
+                    Required = true,
+                    Value = "That is the message that we are sending"
+                }
+            };
+            return CrateManager.CreateStandardConfigurationControlsCrate("Configuration_Controls", fieldsDTO.ToArray());
+        }
+
+        public TextSource GetUpstreamCrate()
+        {
+            return new TextSource
+            {
+                UpstreamSourceLabel = "Upsteam Crate",
+                InitialLabel = "SMS Number",
+                ValueSource = "specific",
+                Name = "SMS_Number",
+                Value = "+15005550006",
+                Label = "SMS Number",
+                Source = new FieldSourceDTO()
+                {
+                    Label = "SMS Number",
+                    
+                },
+                Selected = true
             };
         }
     }
--- terminalTwilioTests/Integration/Send_Via_Twilio_v1Tests.cs
+++ terminalTwilioTests/Integration/Send_Via_Twilio_v1Tests.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading;
-using Data.Control;
-using Data.Crates;
-using Data.Interfaces.DataTransferObjects;
-using Data.Interfaces.Manifests;
+﻿using System.Linq;
 using HealthMonitor.Utility;
-using Hub.Managers.APIManagers.Transmitters.Restful;
 using NUnit.Framework;
 using terminalTwilioTests.Fixture;
+using Data.Control;
+using Data.Interfaces.DataTransferObjects;
+using Data.Interfaces.Manifests;
+using Hub.Managers;
 
 namespace terminalTwilioTests.Integration
 {
@@ -20,7 +15,7 @@ namespace terminalTwilioTests.Integration
     /// but allows to trigger that class from HealthMonitor.
     /// </summary>
     [Explicit]
-    public class Send_Via_Twilio_v1Tests : BaseHealthMonitorTest
+    class Send_Via_Twilio_v1Tests : BaseHealthMonitorTest
     {
         public override string TerminalName
         {
@@ -33,23 +28,90 @@ namespace terminalTwilioTests.Integration
         public async void Send_Via_Twilio_Initial_Configuration_Check_Crate_Structure()
         {
             //Arrange
-            var configureUrl = GetTerminalRunUrl();
-
+            var configureUrl = GetTerminalConfigureUrl();
             var requestActionDTO = HealthMonitor_FixtureData.Send_Via_Twilio_v1_InitialConfiguration_ActionDTO();
-            requestActionDTO.AuthToken = null;
             //Act
-            await HttpPostAsync<ActionDTO, ActionDTO>(
+            var responseActionDTO =
+                await HttpPostAsync<ActionDTO, ActionDTO>(
                     configureUrl,
                     requestActionDTO
-                    );
-
+                );
             //Assert
-            //Assert.NotNull(responseActionDTO);
-            //Assert.NotNull(responseActionDTO.CrateStorage);
-            //Assert.NotNull(responseActionDTO.CrateStorage.Crates);
-
+            Assert.NotNull(responseActionDTO);
+            Assert.NotNull(responseActionDTO.CrateStorage);
+            Assert.NotNull(responseActionDTO.CrateStorage.Crates);
+            var crateStorage = Crate.FromDto(responseActionDTO.CrateStorage);
+            var controls = crateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().Single();
+            Assert.NotNull(controls.Controls[0] is TextSource);
+            Assert.NotNull(controls.Controls[1] is TextBox);
+            Assert.AreEqual("SMS Body", controls.Controls[1].Label);
+            Assert.AreEqual("SMS_Body", controls.Controls[1].Name);
+            Assert.AreEqual("Upstream Terminal-Provided Fields", controls.Controls[0].Source.Label);
+            Assert.AreEqual(false, controls.Controls[0].Selected);
         }
-
-
+        /// <summary>
+        /// Expect null when ActionDTO with no StandardConfigurationControlsCM Crate.
+        /// </summary>
+        [Test, Category("Integration.terminalTwilio")]
+        public async void Send_Via_Twilio_Run_With_No_SMS_Number_Provided()
+        {
+            //Arrange
+            var runUrl = GetTerminalRunUrl();
+            var curActionDTO = HealthMonitor_FixtureData.Send_Via_Twilio_v1_InitialConfiguration_ActionDTO();
+            //Act
+            var payloadDTO = await HttpPostAsync<ActionDTO, ActionDTO>(
+                runUrl,
+                curActionDTO
+                );
+            Assert.IsNull(payloadDTO);
+        }
+        /// <summary>
+        /// Test Twilio Service. Preconfigure Crates with testing number.
+        /// Expect that the status of the message is not fail or undelivered.
+        /// </summary>
+        [Test, Category("Integration.terminalTwilio")]
+        public async void Send_Via_Twilio_Run_Send_SMS_With_Correct_Number()
+        {
+            //Arrange
+            var configureUrl = GetTerminalConfigureUrl();
+            var runUrl = GetTerminalRunUrl();
+            var curActionDTO = HealthMonitor_FixtureData.Send_Via_Twilio_v1_InitialConfiguration_ActionDTO();
+            //Act
+            var responceActionDTO = await HttpPostAsync<ActionDTO, ActionDTO>(configureUrl, curActionDTO);
+            var crateManager = new CrateManager();
+            using (var updater = crateManager.UpdateStorage(responceActionDTO))
+            {
+                var curTextSource =
+                    (TextSource)
+                        updater.CrateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().Single().Controls[0];
+                curTextSource.ValueSource = "specific";
+                updater.CrateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().Single().Controls[0].Value =
+                    "+15005550006";
+                updater.CrateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().Single().Controls[1].Value =
+                    "That is the body of the message";
+            }
+            var payloadDTO = await HttpPostAsync<ActionDTO, ActionDTO>(
+                runUrl,
+                responceActionDTO
+                );
+            //Assert
+            //After Configure Test
+            Assert.NotNull(responceActionDTO);
+            Assert.NotNull(responceActionDTO.CrateStorage);
+            Assert.NotNull(responceActionDTO.CrateStorage.Crates);
+            var crateStorage = Crate.FromDto(responceActionDTO.CrateStorage);
+            var controls = crateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().Single();
+            Assert.NotNull(controls.Controls[0] is TextSource);
+            Assert.NotNull(controls.Controls[1] is TextBox);
+            Assert.AreEqual("SMS Body", controls.Controls[1].Label);
+            Assert.AreEqual("SMS_Body", controls.Controls[1].Name);
+            Assert.AreEqual("Upstream Terminal-Provided Fields", controls.Controls[0].Source.Label);
+            Assert.AreEqual(false, controls.Controls[0].Selected);
+            //After Run Test
+            var payload = Crate.FromDto(payloadDTO.CrateStorage).CrateContentsOfType<StandardPayloadDataCM>().Single();
+            Assert.AreEqual("Status", payload.PayloadObjects[0].PayloadObject[0].Key);
+            Assert.AreNotEqual("failed", payload.PayloadObjects[0].PayloadObject[0].Value);
+            Assert.AreNotEqual("undelivered", payload.PayloadObjects[0].PayloadObject[0].Value);
+        }
     }
 }
