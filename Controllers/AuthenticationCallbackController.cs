9e73b33204c0b72472f4f48d634e7ca3a29f77cb
Vasanthakumar V <adengappaa@hotmail.com>
2015-12-17 03:26:48 +0530

    Fixed the issue locally.

--- Controllers/AuthenticationCallbackController.cs
+++ Controllers/AuthenticationCallbackController.cs
@@ -28,7 +28,7 @@ namespace HubWeb.Controllers
         [HttpGet]
         public async Task<ActionResult> ProcessSuccessfulOAuthResponse(
             string terminalName,
-            string terminalVersion, string ss)
+            string terminalVersion, string externalServiceExtraQueryString)
         {
 
             if (string.IsNullOrEmpty(terminalName) || string.IsNullOrEmpty(terminalVersion))
