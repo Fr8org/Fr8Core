c0a520366b16edd835a0545e487b9b1b8d478952
kggayo <kagaw3000@live.com>
2015-12-25 20:17:25 +0800

    unit tests for handlefr8request

--- terminalBaseTests/BaseClasses/BaseTerminalControllerTests.cs
+++ terminalBaseTests/BaseClasses/BaseTerminalControllerTests.cs
@@ -5,6 +5,11 @@ using TerminalBase.BaseClasses;
 using Data.Interfaces.DataTransferObjects;
 using UtilitiesTesting.Fixtures;
 using System.Threading.Tasks;
+using terminalTests.Fixtures;
+using Data.Crates;
+using Hub.Managers;
+using Data.Interfaces.Manifests;
+using System.Linq;
 
 namespace terminalBaseTests.BaseClasses
 {
@@ -14,12 +19,13 @@ namespace terminalBaseTests.BaseClasses
     {
         IDisposable _coreServer;
         BaseTerminalController _baseTerminalController;
-
-
+        string terminalName = "terminalDocuSign";
+        ICrateManager CrateManagerHelper;
         [SetUp]
         public override void SetUp()
         {
             base.SetUp();
+            CrateManagerHelper = new CrateManager();
             _baseTerminalController = new BaseTerminalController();
             _coreServer = terminalBaseTests.Fixtures.FixtureData.CreateCoreServer_ActivitiesController();
         }
@@ -34,5 +40,113 @@ namespace terminalBaseTests.BaseClasses
             }
         }
 
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async void HandleFr8Request_NullActionDTO_ThrowsException()
+        {
+            await _baseTerminalController.HandleFr8Request(terminalName, "", null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public async void HandleFr8Request_NullActivityTemplate_ThrowsException()
+        {
+            await _baseTerminalController.HandleFr8Request(terminalName, "", null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public async void HandleFr8Request_TerminalNotExist_ThrowsException()
+        {
+            ActionDTO actionDTO = new ActionDTO();
+            actionDTO.ActivityTemplate = new ActivityTemplateDTO() { Name = "terminalDummy", Version = "1.1" };
+
+            await _baseTerminalController.HandleFr8Request(terminalName, "", actionDTO);
+        }
+
+        [Test]
+        public async void HandleFr8Request_Configure_ReturnsActionDTO()
+        {
+            var result = await _baseTerminalController.HandleFr8Request("terminalActionMock", "configure", Fixture_HandleRequest.terminalMockActionDTO());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf(typeof(ActionDTO), result);
+
+            var crateStorage = CrateManagerHelper.FromDto(((ActionDTO)result).CrateStorage);
+            var crateResult = crateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().SingleOrDefault();
+
+            Assert.Greater(crateResult.Controls.Where(x => x.Label.ToLower() == "configure").Count(), 0);
+        }
+
+        [Test]
+        public async void HandleFr8Request_Run_ReturnsPayloadDTO()
+        {
+            var result = await _baseTerminalController.HandleFr8Request("terminalActionMock", "run", Fixture_HandleRequest.terminalMockActionDTO());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf(typeof(PayloadDTO), result);
+        }
+
+        [Test]
+        public async void HandleFr8Request_ChildrenExecuted_ReturnsPayloadDTO()
+        {
+            var result = await _baseTerminalController.HandleFr8Request("terminalActionMock", "childrenexecuted", Fixture_HandleRequest.terminalMockActionDTO());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf(typeof(PayloadDTO), result);
+        }
+
+        [Test]
+        public async void HandleFr8Request_InitialConfigurationResponse_ReturnsActionDTO()
+        {
+            var result = await _baseTerminalController.HandleFr8Request("terminalActionMock", "initialconfigurationresponse", Fixture_HandleRequest.terminalMockActionDTO());
+
+            var crateStorage = CrateManagerHelper.FromDto(((ActionDTO)result).CrateStorage);
+            var crateResult = crateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().SingleOrDefault();
+
+            Assert.Greater(crateResult.Controls.Where(x => x.Label.ToLower() == "initialconfigurationresponse").Count(), 0);
+        }
+
+        [Test]
+        public async void HandleFr8Request_FollowupConfigurationResponse_ReturnsActionDTO()
+        {
+            var result = await _baseTerminalController.HandleFr8Request("terminalActionMock", "followupconfigurationresponse", Fixture_HandleRequest.terminalMockActionDTO());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf(typeof(ActionDTO), result);
+
+            var crateStorage = CrateManagerHelper.FromDto(((ActionDTO)result).CrateStorage);
+            var crateResult = crateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().SingleOrDefault();
+
+            Assert.Greater(crateResult.Controls.Where(x => x.Label.ToLower() == "followupconfigurationresponse").Count(), 0);
+        }
+
+        [Test]
+        public async void HandleFr8Request_Activate_ReturnsActionDTO()
+        {
+            var result = await _baseTerminalController.HandleFr8Request("terminalActionMock", "activate", Fixture_HandleRequest.terminalMockActionDTO());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf(typeof(ActionDTO), result);
+
+            var crateStorage = CrateManagerHelper.FromDto(((ActionDTO)result).CrateStorage);
+            var crateResult = crateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().SingleOrDefault();
+
+            Assert.Greater(crateResult.Controls.Where(x => x.Label.ToLower() == "activate").Count(), 0);
+        }
+
+        [Test]
+        public async void HandleFr8Request_Othermethod_ReturnsActionDTO()
+        {
+            var result = await _baseTerminalController.HandleFr8Request("terminalActionMock", "OtherMethod", Fixture_HandleRequest.terminalMockActionDTO());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf(typeof(ActionDTO), result);
+
+            var crateStorage = CrateManagerHelper.FromDto(((ActionDTO)result).CrateStorage);
+            var crateResult = crateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().SingleOrDefault();
+
+            Assert.Greater(crateResult.Controls.Where(x => x.Label.ToLower() == "othermethod").Count(), 0);
+        }
     }
 }
---
+++ terminalBaseTests/Fixtures/FixtureData - HandleFr8Request.cs
@@ -0,0 +1,68 @@
+﻿using Data.Interfaces.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace terminalTests.Fixtures
+{
+    public class Fixture_HandleRequest
+    {
+        public static ActivityTemplateDTO ActivityTemplateTest()
+        {
+            return new ActivityTemplateDTO()
+            {
+                Id = 1,
+                Name = "terminalActionMock_TEST",
+                Version = "1"
+            };
+        }
+
+        public static ActivityTemplateDTO ActivityTemplate()
+        {
+            return new ActivityTemplateDTO()
+            {
+                Id = 1,
+                Name = "terminalActionMock",
+                Version = "1"
+            };
+        }
+
+        public static ActionDTO terminalMockActionDTOTest()
+        {
+            var activityTemplate = ActivityTemplateTest();
+
+            var action = new ActionDTO()
+            {
+                Id = Guid.NewGuid(),
+                Name = "terminalActionMock",
+                Label = "Action Mock",
+                AuthToken = null,
+                ActivityTemplate = activityTemplate,
+                ActivityTemplateId = activityTemplate.Id,
+                ParentRouteNodeId = Guid.NewGuid(),
+            };
+
+            return action;
+        }
+
+        public static ActionDTO terminalMockActionDTO()
+        {
+            var activityTemplate = ActivityTemplate();
+
+            var action = new ActionDTO()
+            {
+                Id = Guid.NewGuid(),
+                Name = "terminalActionMock",
+                Label = "Action Mock",
+                AuthToken = null,
+                ActivityTemplate = activityTemplate,
+                ActivityTemplateId = activityTemplate.Id,
+                ParentRouteNodeId = Guid.NewGuid(),
+            };
+
+            return action;
+        }
+    }
+}
