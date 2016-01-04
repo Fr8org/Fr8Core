d685ae7a9b4bc646412f9a8738d67679f190a12c
Anton <tarakanov.anton@gmail.com>
2016-01-04 23:09:51 +0400

    Added ITag and TagService

---
+++ Hub/Interfaces/ITag.cs
@@ -0,0 +1,32 @@
+﻿using Data.Entities;
+using Data.Interfaces;
+using Data.Utility;
+using Data.Utility.JoinClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hub.Interfaces
+{
+    /// <summary>
+    /// Interface for Tags service
+    /// </summary>
+    public interface ITag
+    {
+        void Add(IUnitOfWork uow, IList<FileTags> fileTags, IList<Tag> tags);
+
+        IList<Tag> GetAllTags();
+
+        Tag GetTag(int id);
+
+        Tag GetTagByKey(string key);
+
+        IList<Tag> GetTags(int fileDoId);
+
+        IList<FileDO> GetFiles(Tag tag);
+
+        IList<FileDO> GetFiles(int tagId);
+    }
+}
---
+++ Hub/Services/TagService.cs
@@ -0,0 +1,58 @@
+﻿using Data.Entities;
+using Data.Interfaces;
+using Data.Utility;
+using Data.Utility.JoinClasses;
+using Hub.Interfaces;
+using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hub.Services
+{
+    public class TagService : ITag
+    {
+
+        public IList<Tag> GetAllTags()
+        {
+            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
+            {
+                List<Tag> tags = new List<Tag>();
+                //tags = uow.FileRepository.FindList(f => f.DockyardAccountID == dockyardAccountId).ToList();
+                return tags;
+            }
+        }
+
+        public Tag GetTag(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tag GetTagByKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Tag> GetTags(int fileDoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<FileDO> GetFiles(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<FileDO> GetFiles(int tagId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(IUnitOfWork uow, IList<FileTags> fileTags, IList<Tag> tags)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
