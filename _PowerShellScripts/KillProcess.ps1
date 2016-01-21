c425bcf94f7352786bb7954a3c57a99016df5000
altafub <altaf.b@outlook.com>
2016-01-21 22:43:31 +0530

    Updated the script to check resources file as well

--- _PowerShellScripts/KillProcess.ps1
+++ _PowerShellScripts/KillProcess.ps1
@@ -1,8 +1,13 @@
 $RootDir = Split-Path -parent (Split-Path -parent $MyInvocation.MyCommand.Path)
-$lockedFile= $RootDir+"\Utilities\obj\Release\Utilities.dll"
-Get-Process | foreach{$processVar = $_;$_.Modules | foreach{if($_.FileName -eq $lockedFile)
+$Path= $RootDir+"\*\obj\Release"
+
+$files = Get-ChildItem $Path -Recurse | where {$_.extension -eq ".resources" -OR $_.Extension -eq ".dll"}
+foreach($file in $files)
+{
+Get-Process | foreach{$processVar = $_;$_.Modules | foreach{if($_.FileName -eq $file)
 {
     $processVar.Name + " PID:" + $processVar.id
     Stop-Process -id $processVar.Id -passThru -Force -Verbose
 }
 }}
+}
