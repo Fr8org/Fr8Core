99baf44af4c3498e41326cd1b144a49ee42f6bb2
Vasanthakumar V <adengappaa@hotmail.com>
2016-01-27 19:52:47 +0530

    Added required properties for Account and Contact objects.

--- terminalSalesforce/Infrastructure/ContactDTO.cs
+++ terminalSalesforce/Infrastructure/ContactDTO.cs
@@ -8,11 +8,27 @@ namespace terminalSalesforce.Infrastructure
     public class ContactDTO
     {
         public string FirstName { get; set; }
-
         public string LastName { get; set; }
 
+        //change Mobile Phone to Mobile
         public string MobilePhone { get; set; }
 
         public string Email { get; set; }
+        public string AccountId { get; set; }
+        public string Title { get; set; }
+        public string Department { get; set; }
+        public string HomePhone { get; set; }
+        public string OtherPhone { get; set; }
+        public string Fax { get; set; }
+        public string MailingStreet { get; set; }
+        public string MailingCity { get; set; }
+        public string MailingState { get; set; }
+        public string MailingPostalCode { get; set; }
+        public string MailingCountry { get; set; }
+        public string OtherStreet { get; set; }
+        public string OtherCity { get; set; }
+        public string OtherState { get; set; }
+        public string OtherPostalCode { get; set; }
+        public string OtherCountry { get; set; }
     }
 }
