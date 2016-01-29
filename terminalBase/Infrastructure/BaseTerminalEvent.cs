d7a3208867baa275a6013fdecc5f4c15102992ca
Vasanthakumar V <adengappaa@hotmail.com>
2016-01-30 01:44:26 +0530

    Reverted changes.

--- terminalBase/Infrastructure/BaseTerminalEvent.cs
+++ terminalBase/Infrastructure/BaseTerminalEvent.cs
@@ -157,7 +157,7 @@ namespace TerminalBase.Infrastructure
                 try
                 {
                     //TODO are we expecting a timeout??
-                    await Task.Run(() => client.PostAsync(url, eventReportCrateDTO));
+                    await client.PostAsync(url, eventReportCrateDTO);
                 }
                 catch (TaskCanceledException)
                 {
