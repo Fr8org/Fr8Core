9155afb4f9950134e3bd84c04768436b6c7aff65
Alexei Avrutin <alexavrutin@gmail.com>
2016-02-01 09:13:27 +0100

    Adding Mail Merge solution test (in progress)

---
+++ Tests/HealthMonitorUtility/BaseHubIntegrationTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using Hub.Interfaces;
+using Hub.Security;
+using Newtonsoft.Json;
+using Data.Crates;
+using Data.Interfaces.DataTransferObjects;
+using Data.Interfaces.Manifests;
+using Hub.Managers;
+using Hub.Managers.APIManagers.Transmitters.Restful;
+using System.Linq;
+using NUnit.Framework;
+using Data.Constants;
+using StructureMap;
+
+namespace HealthMonitor.Utility
+{
+    public abstract class BaseHubIntegrationTest
+    {
+        public ICrateManager Crate { get; set; }
+        public IRestfulServiceClient RestfulServiceClient { get; set; }
+        public IHMACService HMACService { get; set; }
+        private string terminalSecret;
+        private string terminalId;
+        protected string TerminalSecret {
+            get 
+            {
+                return terminalSecret ?? (terminalSecret = ConfigurationManager.AppSettings[TerminalName + "TerminalSecret"]);
+            }
+        }
+        protected string TerminalId {
+            get
+            {
+                return terminalId ?? (terminalId = ConfigurationManager.AppSettings[TerminalName + "TerminalId"]);
+            }
+        }
+        protected string TestUserEmail = "integration_test_runner@fr8.company";
+        protected string TestUserPassword = "fr8#s@lt!";
+
+        public BaseHubIntegrationTest()
+        {
+            ObjectFactory.Initialize();
+            ObjectFactory.Configure(Hub.StructureMap.StructureMapBootStrapper.LiveConfiguration);
+            RestfulServiceClient = new RestfulServiceClient();
+            Crate = new CrateManager();
+            HMACService = new Fr8HMACService();
+        }
+        public abstract string TerminalName { get; }
+
+
+        public string GetTerminalDiscoverUrl()
+        {
+            return GetTerminalUrl() + "/terminals/discover";
+        }
+
+        public string GetTerminalConfigureUrl()
+        {
+            return GetTerminalUrl() + "/actions/configure";
+        }
+
+        public string GetTerminalActivateUrl()
+        {
+            return GetTerminalUrl() + "/actions/activate";
+        }
+
+        public string GetTerminalDeactivateUrl()
+        {
+            return GetTerminalUrl() + "/actions/deactivate";
+        }
+
+        public string GetTerminalRunUrl()
+        {
+            return GetTerminalUrl() + "/actions/run";
+        }
+
+        public string GetTerminalUrl()
+        {
+            return ConfigurationManager.AppSettings[TerminalName + "Url"];
+        }
+
+        public string GetHubApiBaseUrl()
+        {
+            return ConfigurationManager.AppSettings["HubApiBaseUrl"];
+        }
+
+        public void CheckIfPayloadHasNeedsAuthenticationError(PayloadDTO payload)
+        {
+            var storage = Crate.GetStorage(payload);
+            var operationalStateCM = storage.CrateContentsOfType<OperationalStateCM>().Single();
+
+            Assert.AreEqual(ActivityResponse.Error, operationalStateCM.CurrentActivityResponse);
+            Assert.AreEqual(ActionErrorCode.NO_AUTH_TOKEN_PROVIDED, operationalStateCM.CurrentActivityErrorCode);
+            Assert.AreEqual("No AuthToken provided.", operationalStateCM.CurrentActivityErrorMessage);
+
+        }
+        private async Task<Dictionary<string, string>> GetHMACHeader<T>(Uri requestUri, string userId, T content)
+        {
+            return await HMACService.GenerateHMACHeader(requestUri, TerminalId, TerminalSecret, userId, content);
+        }
+        public async Task<TResponse> HttpPostAsync<TRequest, TResponse>(string url, TRequest request)
+        {
+            var uri = new Uri(url);
+            return await RestfulServiceClient.PostAsync<TRequest, TResponse>(uri, request, null, await GetHMACHeader(uri, "testUser", request));
+        }
+        public async Task<TResponse> HttpGetAsync<TResponse>(string url)
+        {
+            var uri = new Uri(url);
+            return await RestfulServiceClient.GetAsync<TResponse>(uri);
+        }
+    }
+}
---
+++ Tests/terminalDocuSignTests/Integration/Mail_Merge_Into_DocuSign_v1_EndToEnd_Tests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using NUnit.Framework;
+using Data.Interfaces.DataTransferObjects;
+using HealthMonitor.Utility;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace terminalDocuSignTests.Integration
+{
+    /// <summary>
+    /// Mark test case class with [Explicit] attiribute.
+    /// It prevents test case from running when CI is building the solution,
+    /// but allows to trigger that class from HealthMonitor.
+    /// </summary>
+    [Explicit]
+    [Category("terminalDocuSignTests.Integration")]
+    public class Mail_Merge_Into_DocuSign_v1_EndToEnd_Tests : BaseHubIntegrationTest
+    {
+        public override string TerminalName
+        {
+            get { return "terminalDocuSign"; }
+        }
+
+        [Test]
+        public async void Mail_Merge_Into_DocuSign_EndToEnd()
+        {
+            var solutionCreateUrl = GetHubApiBaseUrl() + "actions/create?solutionName=Mail_Merge_Into_DocuSign";
+            await LoginUser(TestUserEmail, TestUserPassword);
+
+            // Create solution
+            var routeDto = await HttpGetAsync<RouteFullDTO>(solutionCreateUrl);
+        }
+
+        private async Task LoginUser(string email, string password)
+        {
+            var httpClient = new HttpClient();
+
+            var hubApiBaseUrl = new Uri(GetHubApiBaseUrl());
+            var hubBaseUrl = new Uri(hubApiBaseUrl.Scheme + "://" + hubApiBaseUrl.Host + ":" + hubApiBaseUrl.Port);
+            httpClient.BaseAddress = hubBaseUrl;
+
+            // Get login page and extract request validation token
+            string verificationToken = await GetVerificationToken(httpClient);
+
+            // Login user
+            string authTicket = await Authenticate(email, password, verificationToken);
+        }
+
+        private static async Task<string> Authenticate(string email, string password, string verificationToken)
+        {
+            var authenticationEndpointUrl = "/dockyardaccount/login";
+
+            var postParams = new StringBuilder();
+            postParams.Append("__RequestVerificationToken=");
+            postParams.Append(verificationToken);
+            postParams.Append("&Email=");
+            postParams.Append(email);
+            postParams.Append("&Password=");
+            postParams.Append(password);
+
+            var postContent = new StringContent(postParams.ToString());
+            //var response = await httpClient.PostAsync(authenticationEndpointUrl, postContent);
+            response.EnsureSuccessStatusCode();
+        }
+
+        private static async Task<string> GetVerificationToken(HttpClient httpClient)
+        {
+            var loginFormUrl = "/dockyardaccount";
+            var response = await httpClient.GetAsync(loginFormUrl);
+            response.EnsureSuccessStatusCode();
+            var loginPageText = await response.Content.ReadAsStringAsync();
+            var regEx = new System.Text.RegularExpressions.Regex(@"<input\s+name=""__RequestVerificationToken""\s+type=""hidden""\s+value=\""([\w\d-_]+)""\s+\/>");
+            var matches = regEx.Match(loginPageText);
+            if (matches == null || matches.Groups.Count < 2)
+            {
+                throw new Exception("Unable to find verification token in the login page HTML code.");
+            }
+            return matches.Groups[1].Value;
+        }
+    }
+}
