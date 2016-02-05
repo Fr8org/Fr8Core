ecedc7f11507e55e10e6be04929a5ce74f819472
Bahadır Bozdağ <bahadir.bb@gmail.com>
2016-02-05 18:33:55 +0200

    Added tests to check Authentication
    Added unit tests for authentication

--- Tests/DockyardTest/Infrastructure/Fr8HubWebHMACAuthenticateAttributeTests.cs
+++ Tests/DockyardTest/Infrastructure/Fr8HubWebHMACAuthenticateAttributeTests.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
+using Hub.Interfaces;
+using Moq;
+using StructureMap;
 using UtilitiesTesting;
 
 namespace HubWeb.Infrastructure
@@ -15,35 +18,75 @@ namespace HubWeb.Infrastructure
     [Category("Fr8HubWebHMACAuthenticateAttribute")]
     public class Fr8HubWebHMACAuthenticateAttributeTests : BaseTest
     {
-        private Fr8HubWebHMACAuthenticateAttribute _hmacAttribute;
         private HttpAuthenticationContext _authenticationContext;
 
         [SetUp]
         public override void SetUp()
         {
             base.SetUp();
-            _hmacAttribute = new Fr8HubWebHMACAuthenticateAttribute();
             HttpRequestMessage request = new HttpRequestMessage();
-            HttpControllerContext controllerContext = new HttpControllerContext();
-            controllerContext.Request = request;
-            HttpActionContext context = new HttpActionContext();
-            context.ControllerContext = controllerContext;
+            HttpControllerContext controllerContext = new HttpControllerContext {Request = request};
+            HttpActionContext context = new HttpActionContext {ControllerContext = controllerContext};
             _authenticationContext = new HttpAuthenticationContext(context, null);
             HttpRequestHeaders headers = request.Headers;
             AuthenticationHeaderValue authorization = new AuthenticationHeaderValue("scheme");
             headers.Authorization = authorization;
         }
 
+        private Fr8HubWebHMACAuthenticateAttribute CreateFilter()
+        {
+            return new Fr8HubWebHMACAuthenticateAttribute();
+        }
+
+        /// <summary>
+        /// Mocked HMACAuthenticator returns true by default
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task ShouldSetCurrentUser_WithCorrectAuthentication()
+        {
+            await CreateFilter().AuthenticateAsync(_authenticationContext, CancellationToken.None);
+            Assert.AreEqual(true, _authenticationContext.Principal is Fr8Principle);
+            Assert.AreEqual(true, _authenticationContext.Principal.Identity is Fr8Identity);
+        }
 
         [Test]
-        public async Task ShouldntSetCurrentUser_WithEmptyHMACHeader()
+        public async Task ShouldntSetCurrentUser_WithInCorrectAuthentication()
         {
-            HttpContext.Current.User = null;
-            var sampleTerminalId = Guid.NewGuid();
-            await _hmacAttribute.AuthenticateAsync(_authenticationContext, CancellationToken.None);
-            //check data
-            int a = 12;
-            Assert.AreEqual(null, HttpContext.Current.User);
+            var fr8HMACAuthenticator = new Mock<IHMACAuthenticator>();
+            fr8HMACAuthenticator.Setup(x => x.IsValidRequest(It.IsAny<HttpRequestMessage>(), It.IsAny<string>())).ReturnsAsync(false);
+            var outTerminalId = "testTerminal";
+            var outUserId = "testUser";
+            fr8HMACAuthenticator.Setup(s => s.ExtractTokenParts(It.IsAny<HttpRequestMessage>(), out outTerminalId, out outUserId));
+            ObjectFactory.Configure(o => o.For<IHMACAuthenticator>().Use(fr8HMACAuthenticator.Object));
+
+            await CreateFilter().AuthenticateAsync(_authenticationContext, CancellationToken.None);
+            Assert.AreEqual(null, _authenticationContext.Principal);
+        }
+
+        [Test]
+        public async Task ShouldntSetCurrentUser_WithNullUserId()
+        {
+            var fr8HMACAuthenticator = new Mock<IHMACAuthenticator>();
+            fr8HMACAuthenticator.Setup(x => x.IsValidRequest(It.IsAny<HttpRequestMessage>(), It.IsAny<string>())).ReturnsAsync(true);
+            var outTerminalId = "testTerminal";
+            string outUserId = null;
+            fr8HMACAuthenticator.Setup(s => s.ExtractTokenParts(It.IsAny<HttpRequestMessage>(), out outTerminalId, out outUserId));
+            ObjectFactory.Configure(o => o.For<IHMACAuthenticator>().Use(fr8HMACAuthenticator.Object));
+
+            await CreateFilter().AuthenticateAsync(_authenticationContext, CancellationToken.None);
+            Assert.AreEqual(null, _authenticationContext.Principal);
+        }
+
+        [Test]
+        public async Task ShouldntSetCurrentUser_WithInvalidTerminalId()
+        {
+            var terminalService = new Mock<ITerminal>();
+            terminalService.Setup(x => x.GetTerminalByPublicIdentifier(It.IsAny<string>())).ReturnsAsync(null);
+            ObjectFactory.Configure(o => o.For<ITerminal>().Use(terminalService.Object));
+
+            await CreateFilter().AuthenticateAsync(_authenticationContext, CancellationToken.None);
+            Assert.AreEqual(null, _authenticationContext.Principal);
         }
 
 
