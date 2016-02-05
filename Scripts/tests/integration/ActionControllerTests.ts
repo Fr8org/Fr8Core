1d9462e0e9bc2bb9ae1d457f61a7fc2645fbba1d
Volodymyr Krytskyi <vkrytskyy@gmail.com>
2016-02-05 17:46:19 +0200

    Fix js test data for Activity

--- Scripts/tests/integration/ActionControllerTests.ts
+++ Scripts/tests/integration/ActionControllerTests.ts
@@ -73,7 +73,6 @@ module dockyard.tests.controller {
 
             var actions: interfaces.IActivityDTO =
             {
-                    name: "test action type",
                     configurationControls: fx.ActivityDTO.configurationControls,
                     crateStorage: null,
                     parentRouteNodeId: '89EBF277-0CC4-4D6D-856B-52457F10C686',
@@ -99,7 +98,6 @@ module dockyard.tests.controller {
 
             var currentActionDesignDTO: interfaces.IActivityDTO =
                     {
-                        name: "test action type",
                         configurationControls: fx.ActivityDTO.configurationControls,
                         crateStorage: null,
                         parentRouteNodeId: '89EBF277-0CC4-4D6D-856B-52457F10C686',
