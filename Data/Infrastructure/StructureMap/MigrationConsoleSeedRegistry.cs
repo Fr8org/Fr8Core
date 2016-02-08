dc76a158b535b567ec269d4cddf25f3f50ae8e56
Yakov Gnusin <y.gnusin@gmail.com>
2016-02-08 15:18:41 +0600

    Added additional logging to AuthToken flow.
    Fixed bugs with migration seed methods.

--- Data/Infrastructure/StructureMap/MigrationConsoleSeedRegistry.cs
+++ Data/Infrastructure/StructureMap/MigrationConsoleSeedRegistry.cs
@@ -1,8 +1,9 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using StructureMap.Configuration.DSL;
 using Data.Interfaces;
-using Data.Infrastructure;
 using Data.Repositories;
+using Data.Repositories.Plan;
 
 namespace Data.Infrastructure
 {
@@ -14,6 +15,13 @@ namespace Data.Infrastructure
             For<IDBContext>().Use<DockyardDbContext>();
             For<IAuthorizationTokenRepository>().Use<AuthorizationTokenRepositoryStub>();
             For<IUnitOfWork>().Use<UnitOfWork>();
+            For<IPlanCache>().Use<PlanCache>().Singleton();
+
+            var planCacheExpiration = TimeSpan.FromMinutes(10);
+            For<IPlanCacheExpirationStrategy>().Use(_ => new SlidingExpirationStrategy(planCacheExpiration)).Singleton();
+
+            For<IPlanStorageProvider>().Use<PlanStorageProviderEf>();
+            For<PlanStorage>().Use<PlanStorage>();
         }
     }
 }
