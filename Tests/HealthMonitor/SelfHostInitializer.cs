a125074a7849d50feebac7e0b6f300ea5fa4af23
Alexei Avrutin <alexavrutin@gmail.com>
2016-02-12 19:51:23 +0100

    reduced timeout

--- Tests/HealthMonitor/SelfHostInitializer.cs
+++ Tests/HealthMonitor/SelfHostInitializer.cs
@@ -96,7 +96,7 @@ namespace HealthMonitor
             }
 
             // Waiting for the server to initialize
-            Thread.Sleep(new TimeSpan(0, 1, 15));
+            Thread.Sleep(new TimeSpan(0, 0, 30));
         }
 
         private void _hubProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
