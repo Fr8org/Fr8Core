165b1615fca03471fc8a9e8058434769560d3836
Alexei Avrutin <alexavrutin@gmail.com>
2016-02-13 00:43:37 +0100

    PS script fix

--- _PowerShellScripts/UpdateTerminalHostnameInDb.ps1
+++ _PowerShellScripts/UpdateTerminalHostnameInDb.ps1
@@ -10,10 +10,10 @@ param(
 	[string]$newHostname
 )
 
-Write-Host "Update terminal URLs to ($newHostname)"
+Write-Host "Update terminal URLs to $newHostname"
 $errorMessage = "An error while executing the query. Possibly cannot connect to the database, please check connection string."
 
-$commandText = "UPDATE Terminals SET [Endpoint] = '($newHostname):' + RIGHT([Endpoint], 5)"
+$commandText = "UPDATE $dbName.dbo.Terminals SET [Endpoint] = '$newHostname" + ":' + RIGHT([Endpoint], 5)"
 Write-Host $commandText
 
 $connection = new-object system.data.SqlClient.SQLConnection($connectionString)
