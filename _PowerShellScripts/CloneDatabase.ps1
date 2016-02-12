89b8d89aa32c4003456ff6458c2846861a8d755e
Alexei Avrutin <alexavrutin@gmail.com>
2016-02-12 20:30:12 +0100

    PS script to clone database added.

---
+++ _PowerShellScripts/CloneDatabase.ps1
@@ -0,0 +1,37 @@
+﻿param(
+    [string]$connectionString,
+	[string]$sourceDbName,
+	[string]$targetDbName,
+	[string]$serverName
+)
+
+Write-Host "Deletes old target database if exists and creates a new one from the specified database."
+$errorMessage = "An error while executing the query. Possibly cannot connect to the database to clone it. Please check connection string for CloneDatabase action."
+
+$commandText = "DECLARE @kill varchar(8000) = ''; SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), spid) + ';' FROM master..sysprocesses WHERE dbid = db_id('$($targetDbName)') EXEC(@kill); "
+$commandText += "DROP DATABASE IF EXISTS $($targetDbName)"
+Write-Host $commandText
+
+$connection = new-object system.data.SqlClient.SQLConnection($connectionString)
+$command = new-object system.data.sqlclient.sqlcommand($commandText, $connection)
+$connection.Open()
+$command.CommandTimeout = 300 #5 minutes
+
+if ($command.ExecuteNonQuery() -ne -1)
+{
+	Write-Host $errorMessage
+	exit 1
+}
+Write-Host "Successfully deleted old target database."
+
+$commandText = "CREATE DATABASE $($targetDbName)Staging AS COPY OF $($serverName).$($sourceDbName)"
+Write-Host $commandText
+$command.CommandText = $commandText
+if ($command.ExecuteNonQuery() -ne -1)
+{
+	Write-Host $errorMessage
+	exit 1
+}
+Write-Host "Successfully cloned the database."
+
+$connection.Close()
