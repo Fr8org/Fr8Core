8f69d3fbf487994b7fff5e054aa30374563c2e9c
Bahadır Bozdağ <bahadir.bb@gmail.com>
2016-02-15 09:47:14 +0200

    Fix structuremap error

--- terminalDocuSign/Infrastructure/TerminalDocuSignMapBootstrapper.cs
+++ terminalDocuSign/Infrastructure/TerminalDocuSignMapBootstrapper.cs
@@ -42,9 +42,8 @@ namespace terminalDocuSign.Infrastructure.StructureMap
                 For<IDocuSignFolder>().Use<DocuSignFolder>();
 				For<IDocuSignEnvelope>().Use<DocuSignEnvelope>();
 				For<IDocuSignTemplate>().Use<DocuSignTemplate>();
-                For<IDocuSignRoute>().Use<DocuSignRoute>();
-                //For<IDocuSignRoute>().Use(new Mock<DocuSignRoute>(MockBehavior.Default).Object);
-            }
+                For<IDocuSignRoute>().Use(new Mock<DocuSignRoute>(MockBehavior.Default).Object);
+			}
 		}
 
 	}
