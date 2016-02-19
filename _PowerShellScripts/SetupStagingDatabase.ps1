a8abca9eba1e001721c363930f1d70a51e01c438
Alexei Avrutin <alexavrutin@gmail.com>
2016-02-20 00:43:40 +0100

    PowerShell fix

--- _PowerShellScripts/SetupStagingDatabase.ps1
+++ _PowerShellScripts/SetupStagingDatabase.ps1
@@ -23,7 +23,7 @@ if ($command.ExecuteNonQuery() -ne -1)
 }
 Write-Host "Successfully deleted old staging database."
 
-$commandText = "CREATE DATABASE $($databaseName)Staging AS COPY OF $($serverName).$($databaseName) (SERVICE_OBJECTIVE = 'basic');"
+$commandText = "CREATE DATABASE $($databaseName)Staging AS COPY OF $($serverName).$($databaseName) (EDITION='basic', SERVICE_OBJECTIVE = 'basic');"
 Write-Host $commandText
 $command.CommandText = $commandText
 if ($command.ExecuteNonQuery() -ne -1)
