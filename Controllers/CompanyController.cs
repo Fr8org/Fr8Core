5930808300e046960210d37eafde335ac5575af6
pharrukh <farrukh.normuradov@gmail.com>
2016-02-20 20:56:36 +0500

    FR-2182: preliminary commit

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -9,9 +9,17 @@ namespace HubWeb.Controllers
             return View();
         }
         // GET: Company
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(string id)
         {
+            ViewBag.SectionId = id;
             return View();
         }
+        //[HttpPost]
+        //public ActionResult Index(string SectionId)
+        //{
+            
+        //    return View();
+        //}
     }
 }
