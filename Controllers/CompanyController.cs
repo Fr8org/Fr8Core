53c31528aa199f9b494d5e8828dccf3a3500db0d
pharrukh <farrukh.normuradov@gmail.com>
2016-02-20 23:29:57 +0500

    FR-2182: implemented reference in the dropdown menu

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -4,22 +4,11 @@ namespace HubWeb.Controllers
 {
     public class CompanyController : Controller
     {
-        public ActionResult Jobs()
-        {
-            return View();
-        }
         // GET: Company
         [HttpGet]
-        public ActionResult Index(string id)
+        public ActionResult Index()
         {
-            ViewBag.SectionId = id;
             return View();
         }
-        //[HttpPost]
-        //public ActionResult Index(string SectionId)
-        //{
-            
-        //    return View();
-        //}
     }
 }
