730be3a970fbbaf58467afade3522c2edd94386f
Alexei Avrutin <alexavrutin@gmail.com>
2016-02-26 14:02:27 +0100

    Revert "HealthMonitor build path updated"
    This reverts commit 6978a189527346f689dc679916ca31988d287bc7.

--- _PowerShellScripts/RunIntegrationTests-MASTER.ps1
+++ _PowerShellScripts/RunIntegrationTests-MASTER.ps1
@@ -1,7 +1,7 @@
 $RootDir = Split-Path -parent $PSCommandPath
-$HealthMonitorCmd = "$RootDir\..\Tests\HealthMonitor\bin\Release\HealthMonitor.exe --email-report --app-name VSO-MASTER --ensure-startup"
+$HealthMonitorCmd = "$RootDir\..\Tests\HealthMonitor\bin\Debug\HealthMonitor.exe --email-report --app-name VSO-MASTER --ensure-startup"
 $SrcConfigFile = "$RootDir\MASTER-HealthMonitor.exe.config"
-$DstConfigFile = "$RootDir\..\Tests\HealthMonitor\bin\Release\HealthMonitor.exe.config"
+$DstConfigFile = "$RootDir\..\Tests\HealthMonitor\bin\Debug\HealthMonitor.exe.config"
 
 Write-Host "Copying HealthMonitor config file"
 Copy-Item $SrcConfigFile -Destination $DstConfigFile -Force
