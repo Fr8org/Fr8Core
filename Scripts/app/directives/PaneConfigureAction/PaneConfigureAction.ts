058dabdaab3c989e97f25772726153ecee534cf4
Yakov Gnusin <y.gnusin@gmail.com>
2016-02-26 10:39:10 +0600

    1. Fixed drop-down-list arrow issue.
    2. Renamed ExecuteAfterConfigure to RunImmediately

--- Scripts/app/directives/PaneConfigureAction/PaneConfigureAction.ts
+++ Scripts/app/directives/PaneConfigureAction/PaneConfigureAction.ts
@@ -415,7 +415,7 @@ module dockyard.directives.paneConfigureAction {
                         .then((res: interfaces.IActionVM) => {
                             var childActionsDetected = false;
 
-                            // Detect OperationalState crate with CurrentClientActionName = 'ExecuteAfterConfigure'.
+                            // Detect OperationalState crate with CurrentClientActionName = 'RunImmediately'.
                             if (crateHelper.hasCrateOfManifestType(res.crateStorage, 'Operational State')) {
                                 var operationalStatus = crateHelper
                                     .findByManifestType(res.crateStorage, 'Operational State');
@@ -423,7 +423,7 @@ module dockyard.directives.paneConfigureAction {
                                 var contents = <any>operationalStatus.contents;
 
                                 if (contents.CurrentActivityResponse.type === 'ExecuteClientActivity'
-                                    && (contents.CurrentClientActivityName === 'ExecuteAfterConfigure')
+                                    && (contents.CurrentClientActivityName === 'RunImmediately')
                                     ) {
 
                                     $scope.$emit(MessageType[MessageType.PaneConfigureAction_ExecutePlan]);
