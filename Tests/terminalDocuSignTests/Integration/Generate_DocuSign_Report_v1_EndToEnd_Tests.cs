d72457abbd21afbc23eb59457965b7c1b156320b
Yakov Gnusin <y.gnusin@gmail.com>
2016-02-26 13:22:45 +0600

    Initial implementation of Generate_DocuSign_Report_EndToEnd_Test.

---
+++ Tests/terminalDocuSignTests/Integration/Generate_DocuSign_Report_v1_EndToEnd_Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Data.Interfaces.DataTransferObjects;
+using HealthMonitor.Utility;
+
+namespace terminalDocuSignTests.Integration
+{
+    [Explicit]
+    [Category("terminalDocuSignTests.Integration")]
+    public class Generate_DocuSign_Report_v1_EndToEnd_Tests : BaseHubIntegrationTest
+    {
+        public override string TerminalName
+        {
+            get { return "terminalDocuSign"; }
+        }
+
+        [Test]
+        public async Task Generate_DocuSign_Report_EndToEnd()
+        {
+            try
+            {
+                var plan = await CreateSolution();
+                var solution = ExtractSolution(plan);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private async Task<RouteFullDTO> CreateSolution()
+        {
+            var solutionCreateUrl = _baseUrl + "activities/create?solutionName=Generate_DocuSign_Report";
+            var plan = await HttpPostAsync<string, RouteFullDTO>(solutionCreateUrl, null);
+
+            return plan;
+        }
+
+        private ActivityDTO ExtractSolution(RouteFullDTO plan)
+        {
+            var solution = plan.Subroutes
+                .FirstOrDefault()
+                .Activities
+                .FirstOrDefault();
+
+            return solution;
+        }
+    }
+}
