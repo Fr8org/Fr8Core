26ee9ab6908cf28e33a156b723cd61ee690bc6df
Smit Jawale <sjawale@genius-minds.com>
2016-03-02 20:39:35 +0530

    1. Implemented activity pane resize functionality using jquery-ui and angularjs

---
+++ Scripts/app/directives/Resizable.ts
@@ -0,0 +1,41 @@
+﻿/// <reference path="../_all.ts" />
+
+module dockyard.directives {
+    'use strict';
+
+    export function Resizable(): ng.IDirective {
+        return {
+            restrict: 'A',
+            link: (scope: IResizableScope, elem, attrs) => {
+                elem
+                    .wrap('<div/>')
+                    .css({ 'overflow': 'hidden' })
+                    .parent()
+                    .css({
+                        'display': 'inline-block',
+                        'overflow': 'hidden',
+                        'width': function () { return $(elem, this).width(); },
+                        'height': '380px',
+                        'paddingBottom': '20px',
+                        'min-width': '380px',
+                        'min-height': '380px'
+
+                    }).resizable()
+                    .find(elem)
+                    .css({
+                        overflow: 'auto',
+                        width: '100%',
+                        height: '100%'
+                    });
+            }
+        }
+    }
+
+    export interface IResizableScope extends ng.IScope {
+        callback: any;
+        height: number;
+        resize: (events, ui) => void;
+    }
+
+    app.directive('resizable', dockyard.directives.Resizable);
+}
