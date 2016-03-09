7698e5926dca3b8a116e5c8041aa73e2e3bf9596
Yakov Gnusin <y.gnusin@gmail.com>
2016-03-09 16:11:14 +0600

    Integration test for MonitorAllDocuSignEvents.

---
+++ Tests/terminalDocuSignTests/Integration/MonitorAllDocuSignEvents_Tests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StructureMap;
+using Data.Interfaces;
+using Data.Interfaces.DataTransferObjects;
+using Data.Interfaces.Manifests;
+using terminalDocuSign.Services;
+using terminalDocuSign.Services.New_Api;
+using Utilities.Configuration.Azure;
+
+namespace terminalDocuSignTests.Integration
+{
+    [Explicit]
+    public class MonitorAllDocuSignEvents_Tests
+    {
+        private const string UserAccountName = "y.gnusin@gmail.com";
+        private const int AwaitPeriod = 120000;
+        private const string TemplateName = "Medical_Form_v2";
+
+        private const string ToEmail = "freight.testing@gmail.com";
+        private const string DocuSignEmail = "freight.testing@gmail.com";
+        private const string DocuSignApiPassword = "I6HmXEbCxN";
+
+
+        [SetUp]
+        public void Init()
+        {
+            ObjectFactory.Initialize();
+            ObjectFactory.Configure(Hub.StructureMap.StructureMapBootStrapper.LiveConfiguration);
+        }
+
+        [Test]
+        public async void Test_MonitorAllDocuSignEvents_Plan()
+        {
+            using (var unitOfWork = ObjectFactory.GetInstance<IUnitOfWork>())
+            {
+                var testAccount = unitOfWork.UserRepository
+                    .GetQuery()
+                    .SingleOrDefault(x => x.UserName == UserAccountName);
+
+                await SendDocuSignTestEnvelope();
+
+                await Task.Delay(AwaitPeriod);
+
+                var createDateMinValue = DateTime.Now.AddMilliseconds(-AwaitPeriod);
+
+                var mtData = unitOfWork.MultiTenantObjectRepository
+                    .Query<DocuSignEnvelopeCM>(
+                        testAccount.Id.ToString(),
+                        x => x.CreateDate >= createDateMinValue
+                    );
+
+                Assert.IsTrue(mtData.Any());
+            }
+        }
+
+        private async Task SendDocuSignTestEnvelope()
+        {
+            var endpoint = CloudConfigurationManager.GetSetting("endpoint");
+
+            var authManager = new DocuSignAuthentication();
+            var password = await authManager
+                .ObtainOAuthToken(DocuSignEmail, DocuSignApiPassword, endpoint);
+
+            var templateManager = new DocuSignTemplate();
+            var template = templateManager
+                .GetTemplateNames(
+                    DocuSignEmail,
+                    password
+                )
+                .FirstOrDefault(x => x.Name == TemplateName);
+
+            if (template == null)
+            {
+                throw new ApplicationException(string.Format("Unable to extract {0} template from DocuSign", TemplateName));
+            }
+
+            var loginInfo = DocuSignService.Login(
+                DocuSignEmail,
+                password
+            );
+
+            var rolesList = new List<FieldDTO>()
+            {
+                new FieldDTO()
+                {
+                    Tags = "recipientId:72179268",
+                    Key = "role name",
+                    Value = ToEmail
+                },
+                new FieldDTO()
+                {
+                    Tags = "recipientId:72179268",
+                    Key = "role email",
+                    Value = ToEmail
+                }
+            };
+
+            var fieldsList = new List<FieldDTO>();
+            
+            DocuSignService.SendAnEnvelopeFromTemplate(
+                loginInfo,
+                rolesList,
+                fieldsList,
+                template.Id
+            );
+        }
+    }
+}
