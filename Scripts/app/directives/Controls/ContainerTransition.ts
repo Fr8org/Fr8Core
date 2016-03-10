7c2bb94a66ee2426ed5cf7a341bf4a9e6ef40af7
Bahadır Bozdağ <bahadir.bb@gmail.com>
2016-03-10 17:00:25 +0200

    Created ContainerTransition

---
+++ Scripts/app/directives/Controls/ContainerTransition.ts
@@ -0,0 +1,31 @@
+﻿/// <reference path="../../_all.ts" />
+module dockyard.directives.containerTransition {
+    'use strict';
+
+    interface IContainerTransitionScope extends ng.IScope {
+        route: model.Route;
+        
+    }
+
+    //More detail on creating directives in TypeScript: 
+    //http://blog.aaronholmes.net/writing-angularjs-directives-as-typescript-classes/
+    export function ContainerTransition(): ng.IDirective {
+        
+        var controller = ['$scope', '$element', '$attrs', ($scope: IContainerTransitionScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
+            
+            
+        }];
+
+        //The factory function returns Directive object as per Angular requirements
+        return {
+            restrict: 'E',
+            templateUrl: '/AngularTemplate/ContainerTransition',
+            controller: controller,
+            scope: {
+                route: '='
+            }
+        };
+    }
+
+    app.directive('containerTransition', ContainerTransition);
+}
