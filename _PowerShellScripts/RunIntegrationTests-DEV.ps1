76cd62031231b6bbcb79b7a059ac178210a8fef7
Alexei Avrutin <alexavrutin@gmail.com>
2016-03-12 02:20:38 +0200

    Disabled config file copying

--- _PowerShellScripts/RunIntegrationTests-DEV.ps1
+++ _PowerShellScripts/RunIntegrationTests-DEV.ps1
@@ -1,12 +1,17 @@
+param(
+	[Parameter(Mandatory = $false)]
+	[int]$delay = 90
+)
+
 $RootDir = Split-Path -parent $PSCommandPath
 $HealthMonitorCmd = "$RootDir\..\Tests\HealthMonitor\bin\Dev\HealthMonitor.exe --app-name VSO-DEV --ensure-startup"
-$SrcConfigFile = "$RootDir\DEV-HealthMonitor.exe.config"
-$DstConfigFile = "$RootDir\..\Tests\HealthMonitor\bin\Dev\HealthMonitor.exe.config"
+#$SrcConfigFile = "$RootDir\DEV-HealthMonitor.exe.config"
+#$DstConfigFile = "$RootDir\..\Tests\HealthMonitor\bin\Dev\HealthMonitor.exe.config"
 
-Write-Host "Copying HealthMonitor config file"
-Copy-Item $SrcConfigFile -Destination $DstConfigFile -Force
+#Write-Host "Copying HealthMonitor config file"
+#Copy-Item $SrcConfigFile -Destination $DstConfigFile -Force
 
 Write-Host $HealthMonitorCmd
 
-Start-Sleep -s 100
+Start-Sleep -s $delay
 Invoke-Expression $HealthMonitorCmd
