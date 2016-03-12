62928955a6b2810c22bf366e8c8b23a5e5205439
Vasanthakumar V <adengappaa@hotmail.com>
2016-03-13 01:22:00 +0530

    Implemented test for WebServices Controller in HubWeb

---
+++ HubTests/Fixtures/WebServicesControllerFixtureData.cs
@@ -0,0 +1,27 @@
+﻿using Data.Entities;
+using Data.Interfaces.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubTests.Fixtures
+{
+    public partial class FixtureData
+    {
+        public static WebServiceDO BasicWebServiceDO()
+        {
+            var webServiceDO = new WebServiceDO { Name = "IntegrationTestWebService", IconPath = "IntegrationTestIconPath", Id = 1 };
+
+            return webServiceDO;
+        }
+
+        public static WebServiceDTO BasicWebServiceDTOWithoutId()
+        {
+            var webServiceDTO = new WebServiceDTO { Name = "IntegrationTestWebService", IconPath = "IntegrationTestIconPath" };
+
+            return webServiceDTO;
+        }
+    }
+}
