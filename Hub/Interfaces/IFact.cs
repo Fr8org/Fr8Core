9aae26c16cc02f3835ef5034d1a52362d4c009d9
npadbidri <npadbidri@genius-minds.com>
2016-03-15 18:52:08 +0530

    FR-2535: 1. Modified Container Details page:    1. Added Content Button: On click it will show Container JSON    2. Added History Button: On click it will show all Facts after sorting them in descending order of Creation Date

---
+++ Hub/Interfaces/IFact.cs
@@ -0,0 +1,11 @@
+﻿using Data.Entities;
+using Data.Interfaces;
+using System.Collections.Generic;
+
+namespace Hub.Interfaces
+{
+    public interface IFact
+    {
+        IList<FactDO> GetByObjectId(IUnitOfWork unitOfWork, string id);
+    }
+}
---
+++ Hub/Services/Fact.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entities;
+using Data.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hub.Services
+{
+    public class Fact : Hub.Interfaces.IFact
+    {
+        public IList<FactDO> GetByObjectId(IUnitOfWork unitOfWork, string id)
+        {
+            var query = unitOfWork.FactRepository.GetQuery();
+            return query.Where(fact => fact.ObjectId == id).ToList();
+        }
+    }
+}
