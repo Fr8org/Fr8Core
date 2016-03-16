e7cc5e1c7acc426c831b7aa41c920d3b76cc9255
Andrey Chaplygin <demarsch@yandex.ru>
2016-03-16 18:00:17 +0100

    Reintroduced IDocusSignManager and restored unit-tests

--- Tests/terminalDocuSignTests/Activities/Get_DocuSign_Template_v1_Tests.cs
+++ Tests/terminalDocuSignTests/Activities/Get_DocuSign_Template_v1_Tests.cs
@@ -6,66 +6,72 @@ using Data.Entities;
 using Data.Interfaces.Manifests;
 using Hub.Managers;
 using NUnit.Framework;
+using StructureMap;
 using terminalDocuSign;
 using terminalDocuSign.Actions;
+using terminalDocuSign.Services.New_Api;
 using terminalDocuSignTests.Fixtures;
 using UtilitiesTesting.Fixtures;
 
 namespace terminalDocuSignTests.Activities
 {
-    //[TestFixture]
-    //[Category("Get_DocuSign_Template_v1")]
-    //public class Get_DocuSign_Template_v1_Tests : BaseTest
-    //{
-    //    [Test]
-    //    public void ActivityIsValid_WhenIsNotConfigured_ReturnsFalse()
-    //    {
-    //        var target = new Get_DocuSign_Template_v1();
-    //        var result = target.ValidateActivityInternal(FixtureData.TestActivity1());
-    //        Assert.AreNotEqual(ValidationResult.Success, result);
-    //        Assert.AreEqual(DocuSignValidationUtils.ControlsAreNotConfiguredErrorMessage, result.ErrorMessage);
-    //    }
+    [TestFixture]
+    [Category("Get_DocuSign_Template_v1")]
+    public class Get_DocuSign_Template_v1_Tests : BaseTest
+    {
+        [Test]
+        public void ActivityIsValid_WhenIsNotConfigured_ReturnsFalse()
+        {
+            ObjectFactory.Configure(x => x.For<IDocuSignManager>().Use(DocuSignActivityFixtureData.DocuSignManagerWithoutTemplates()));
+            var target = new Get_DocuSign_Template_v1();
+            var result = target.ValidateActivityInternal(FixtureData.TestActivity1());
+            Assert.AreNotEqual(ValidationResult.Success, result);
+            Assert.AreEqual(DocuSignValidationUtils.ControlsAreNotConfiguredErrorMessage, result.ErrorMessage);
+        }
 
-    //    [Test]
-    //    public void ActivityIsValid_WhenThereAreNoTemplates_ReturnsFalse()
-    //    {
-    //        var target = new Get_DocuSign_Template_v1(DocuSignActivityFixtureData.DocuSignManagerWithoutTemplates());
-    //        var activityDO = FixtureData.TestActivity1();
-    //        activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
-    //        var result = target.ValidateActivityInternal(activityDO);
-    //        Assert.AreNotEqual(ValidationResult.Success, result);
-    //        Assert.AreEqual(DocuSignValidationUtils.NoTemplateExistsErrorMessage, result.ErrorMessage);
-    //    }
+        [Test]
+        public void ActivityIsValid_WhenThereAreNoTemplates_ReturnsFalse()
+        {
+            ObjectFactory.Configure(x => x.For<IDocuSignManager>().Use(DocuSignActivityFixtureData.DocuSignManagerWithoutTemplates()));
+            var target = new Get_DocuSign_Template_v1();
+            var activityDO = FixtureData.TestActivity1();
+            activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
+            var result = target.ValidateActivityInternal(activityDO);
+            Assert.AreNotEqual(ValidationResult.Success, result);
+            Assert.AreEqual(DocuSignValidationUtils.NoTemplateExistsErrorMessage, result.ErrorMessage);
+        }
 
-    //    [Test]
-    //    public void ActivityIsValid_WhenTemplateIsNotSelected_ReturnsFalse()
-    //    {
-    //        var target = new Get_DocuSign_Template_v1(DocuSignActivityFixtureData.DocuSignManagerWithTemplates());
-    //        var activityDO = FixtureData.TestActivity1();
-    //        activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
-    //        var result = target.ValidateActivityInternal(activityDO);
-    //        Assert.AreNotEqual(ValidationResult.Success, result);
-    //        Assert.AreEqual(DocuSignValidationUtils.TemplateIsNotSelectedErrorMessage, result.ErrorMessage);
-    //    }
-    //    [Test]
-    //    public void ActivityIsValid_WhenTemplatetIsSelected_ReturnsTrue()
-    //    {
-    //        var target = new Get_DocuSign_Template_v1(DocuSignActivityFixtureData.DocuSignManagerWithTemplates());
-    //        var activityDO = FixtureData.TestActivity1();
-    //        activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
-    //        SelectTemplate(activityDO);
-    //        var result = target.ValidateActivityInternal(activityDO);
-    //        Assert.AreEqual(ValidationResult.Success, result);
-    //    }
+        [Test]
+        public void ActivityIsValid_WhenTemplateIsNotSelected_ReturnsFalse()
+        {
+            ObjectFactory.Configure(x => x.For<IDocuSignManager>().Use(DocuSignActivityFixtureData.DocuSignManagerWithTemplates()));
+            var target = new Get_DocuSign_Template_v1();
+            var activityDO = FixtureData.TestActivity1();
+            activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
+            var result = target.ValidateActivityInternal(activityDO);
+            Assert.AreNotEqual(ValidationResult.Success, result);
+            Assert.AreEqual(DocuSignValidationUtils.TemplateIsNotSelectedErrorMessage, result.ErrorMessage);
+        }
+        [Test]
+        public void ActivityIsValid_WhenTemplatetIsSelected_ReturnsTrue()
+        {
+            ObjectFactory.Configure(x => x.For<IDocuSignManager>().Use(DocuSignActivityFixtureData.DocuSignManagerWithTemplates()));
+            var target = new Get_DocuSign_Template_v1();
+            var activityDO = FixtureData.TestActivity1();
+            activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
+            SelectTemplate(activityDO);
+            var result = target.ValidateActivityInternal(activityDO);
+            Assert.AreEqual(ValidationResult.Success, result);
+        }
 
-    //    private void SelectTemplate(ActivityDO activity)
-    //    {
-    //        using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
-    //        {
-    //            var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
-    //            var templateList = configControls.Controls.OfType<DropDownList>().FirstOrDefault();
-    //            templateList.selectedKey = "First";
-    //        }
-    //    }
-    //}
+        private void SelectTemplate(ActivityDO activity)
+        {
+            using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
+            {
+                var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
+                var templateList = configControls.Controls.OfType<DropDownList>().FirstOrDefault();
+                templateList.selectedKey = "First";
+            }
+        }
+    }
 }
--- Tests/terminalDocuSignTests/Activities/Monitor_DocuSign_Envelope_Activity_v1_Tests.cs
+++ Tests/terminalDocuSignTests/Activities/Monitor_DocuSign_Envelope_Activity_v1_Tests.cs
@@ -6,160 +6,168 @@ using Data.Entities;
 using Data.Interfaces.Manifests;
 using Hub.Managers;
 using NUnit.Framework;
+using StructureMap;
 using terminalDocuSign;
 using terminalDocuSign.Actions;
+using terminalDocuSign.Services.New_Api;
 using terminalDocuSignTests.Fixtures;
 using UtilitiesTesting.Fixtures;
 
 namespace terminalDocuSignTests.Activities
 {
-    //[TestFixture]
-    //[Category("Monitor_DocuSign_Envelope_Activity_v1")]
-    //public class Monitor_DocuSign_Envelope_Activity_v1_Tests : BaseTest
-    //{
-    //    [Test]
-    //    public void ActivityIsValid_WhenIsNotConfigured_ReturnsFalse()
-    //    {
-    //        var target = new Monitor_DocuSign_Envelope_Activity_v1();
-    //        var result = target.ValidateActivityInternal(FixtureData.TestActivity1());
-    //        Assert.AreNotEqual(ValidationResult.Success, result);
-    //        Assert.AreEqual(DocuSignValidationUtils.ControlsAreNotConfiguredErrorMessage, result.ErrorMessage);
-    //    }
+    [TestFixture]
+    [Category("Monitor_DocuSign_Envelope_Activity_v1")]
+    public class Monitor_DocuSign_Envelope_Activity_v1_Tests : BaseTest
+    {
+        [Test]
+        public void ActivityIsValid_WhenIsNotConfigured_ReturnsFalse()
+        {
+            ObjectFactory.Configure(x => x.For<IDocuSignManager>().Use(DocuSignActivityFixtureData.DocuSignManagerWithoutTemplates()));
+            var target = new Monitor_DocuSign_Envelope_Activity_v1();
+            var result = target.ValidateActivityInternal(FixtureData.TestActivity1());
+            Assert.AreNotEqual(ValidationResult.Success, result);
+            Assert.AreEqual(DocuSignValidationUtils.ControlsAreNotConfiguredErrorMessage, result.ErrorMessage);
+        }
 
-    //    [Test]
-    //    public void ActivityIsValid_WhenNoNotificationIsSelected_ReturnsFalse()
-    //    {
-    //        var target = new Monitor_DocuSign_Envelope_Activity_v1(DocuSignActivityFixtureData.DocuSignManagerWithoutTemplates());
-    //        var activityDO = FixtureData.TestActivity1();
-    //        activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
-    //        SetRecipientConditionSelected(activityDO);
-    //        SetRecipientText(activityDO);
-    //        var result = target.ValidateActivityInternal(activityDO);
-    //        Assert.AreNotEqual(ValidationResult.Success, result);
-    //        Assert.AreEqual("At least one notification option must be selected", result.ErrorMessage);
-    //    }
+        [Test]
+        public void ActivityIsValid_WhenNoNotificationIsSelected_ReturnsFalse()
+        {
+            ObjectFactory.Configure(x => x.For<IDocuSignManager>().Use(DocuSignActivityFixtureData.DocuSignManagerWithoutTemplates()));
+            var target = new Monitor_DocuSign_Envelope_Activity_v1();
+            var activityDO = FixtureData.TestActivity1();
+            activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
+            SetRecipientConditionSelected(activityDO);
+            SetRecipientText(activityDO);
+            var result = target.ValidateActivityInternal(activityDO);
+            Assert.AreNotEqual(ValidationResult.Success, result);
+            Assert.AreEqual("At least one notification option must be selected", result.ErrorMessage);
+        }
 
-    //    [Test]
-    //    public void ActivityIsValid_WhenNoEnvelopeConditionIsSelected_ReturnsFalse()
-    //    {
-    //        var target = new Monitor_DocuSign_Envelope_Activity_v1(DocuSignActivityFixtureData.DocuSignManagerWithoutTemplates());
-    //        var activityDO = FixtureData.TestActivity1();
-    //        activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
-    //        SetNotificationSelected(activityDO);
-    //        var result = target.ValidateActivityInternal(activityDO);
-    //        Assert.AreNotEqual(ValidationResult.Success, result);
-    //        Assert.AreEqual("At least one envelope option must be selected", result.ErrorMessage);
-    //    }
+        [Test]
+        public void ActivityIsValid_WhenNoEnvelopeConditionIsSelected_ReturnsFalse()
+        {
+            ObjectFactory.Configure(x => x.For<IDocuSignManager>().Use(DocuSignActivityFixtureData.DocuSignManagerWithoutTemplates()));
+            var target = new Monitor_DocuSign_Envelope_Activity_v1();
+            var activityDO = FixtureData.TestActivity1();
+            activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
+            SetNotificationSelected(activityDO);
+            var result = target.ValidateActivityInternal(activityDO);
+            Assert.AreNotEqual(ValidationResult.Success, result);
+            Assert.AreEqual("At least one envelope option must be selected", result.ErrorMessage);
+        }
 
-    //    [Test]
-    //    public void ActivityIsValid_WhenTemplateMustBeSetButThereAreNoTemplates_ReturnsFalse()
-    //    {
-    //        var target = new Monitor_DocuSign_Envelope_Activity_v1(DocuSignActivityFixtureData.DocuSignManagerWithoutTemplates());
-    //        var activityDO = FixtureData.TestActivity1();
-    //        activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
-    //        SetNotificationSelected(activityDO);
-    //        SetTemplateConditionSelected(activityDO);
-    //        var result = target.ValidateActivityInternal(activityDO);
-    //        Assert.AreNotEqual(ValidationResult.Success, result);
-    //        Assert.AreEqual(DocuSignValidationUtils.NoTemplateExistsErrorMessage, result.ErrorMessage);
-    //    }
+        [Test]
+        public void ActivityIsValid_WhenTemplateMustBeSetButThereAreNoTemplates_ReturnsFalse()
+        {
+            ObjectFactory.Configure(x => x.For<IDocuSignManager>().Use(DocuSignActivityFixtureData.DocuSignManagerWithoutTemplates()));
+            var target = new Monitor_DocuSign_Envelope_Activity_v1();
+            var activityDO = FixtureData.TestActivity1();
+            activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
+            SetNotificationSelected(activityDO);
+            SetTemplateConditionSelected(activityDO);
+            var result = target.ValidateActivityInternal(activityDO);
+            Assert.AreNotEqual(ValidationResult.Success, result);
+            Assert.AreEqual(DocuSignValidationUtils.NoTemplateExistsErrorMessage, result.ErrorMessage);
+        }
 
-    //    [Test]
-    //    public void ActivityIsValid_WhenTemplateMustBeSetButItIsNot_ReturnsFalse()
-    //    {
-    //        var target = new Monitor_DocuSign_Envelope_Activity_v1(DocuSignActivityFixtureData.DocuSignManagerWithTemplates());
-    //        var activityDO = FixtureData.TestActivity1();
-    //        activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
-    //        SetNotificationSelected(activityDO);
-    //        SetTemplateConditionSelected(activityDO);
-    //        var result = target.ValidateActivityInternal(activityDO);
-    //        Assert.AreNotEqual(ValidationResult.Success, result);
-    //        Assert.AreEqual(DocuSignValidationUtils.TemplateIsNotSelectedErrorMessage, result.ErrorMessage);
-    //    }
-    //    [Test]
-    //    public void ActivityIsValid_WhenAllFieldsAreSet_ReturnsTrue()
-    //    {
-    //        var target = new Monitor_DocuSign_Envelope_Activity_v1(DocuSignActivityFixtureData.DocuSignManagerWithTemplates());
-    //        var activityDO = FixtureData.TestActivity1();
-    //        activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
-    //        SetNotificationSelected(activityDO);
-    //        SetTemplateConditionSelected(activityDO);
-    //        SetTemplate(activityDO);
-    //        var result = target.ValidateActivityInternal(activityDO);
-    //        Assert.AreEqual(ValidationResult.Success, result);
-    //    }
+        [Test]
+        public void ActivityIsValid_WhenTemplateMustBeSetButItIsNot_ReturnsFalse()
+        {
+            ObjectFactory.Configure(x => x.For<IDocuSignManager>().Use(DocuSignActivityFixtureData.DocuSignManagerWithTemplates()));
+            var target = new Monitor_DocuSign_Envelope_Activity_v1();
+            var activityDO = FixtureData.TestActivity1();
+            activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
+            SetNotificationSelected(activityDO);
+            SetTemplateConditionSelected(activityDO);
+            var result = target.ValidateActivityInternal(activityDO);
+            Assert.AreNotEqual(ValidationResult.Success, result);
+            Assert.AreEqual(DocuSignValidationUtils.TemplateIsNotSelectedErrorMessage, result.ErrorMessage);
+        }
+        [Test]
+        public void ActivityIsValid_WhenAllFieldsAreSet_ReturnsTrue()
+        {
+            ObjectFactory.Configure(x => x.For<IDocuSignManager>().Use(DocuSignActivityFixtureData.DocuSignManagerWithTemplates()));
+            var target = new Monitor_DocuSign_Envelope_Activity_v1();
+            var activityDO = FixtureData.TestActivity1();
+            activityDO = target.Configure(activityDO, FixtureData.AuthToken_TerminalIntegration()).Result;
+            SetNotificationSelected(activityDO);
+            SetTemplateConditionSelected(activityDO);
+            SetTemplate(activityDO);
+            var result = target.ValidateActivityInternal(activityDO);
+            Assert.AreEqual(ValidationResult.Success, result);
+        }
 
-    //    private void SetNotificationSelected(ActivityDO activity)
-    //    {
-    //        using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
-    //        {
-    //            var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
-    //            configControls.Controls
-    //                .Where(c => c.Type == ControlTypes.CheckBox)
-    //                .First(x => x.Name == "RecipientSigned")
-    //                .Selected = true;
-    //        }
-    //    }
+        private void SetNotificationSelected(ActivityDO activity)
+        {
+            using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
+            {
+                var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
+                configControls.Controls
+                    .Where(c => c.Type == ControlTypes.CheckBox)
+                    .First(x => x.Name == "RecipientSigned")
+                    .Selected = true;
+            }
+        }
 
-    //    private void SetRecipientConditionSelected(ActivityDO activity)
-    //    {
-    //        using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
-    //        {
-    //            var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
-    //            configControls.Controls
-    //                          .OfType<RadioButtonGroup>()
-    //                          .First()
-    //                          .Radios
-    //                          .First(x => x.Name == "recipient")
-    //                          .Selected = true;
-    //        }
-    //    }
+        private void SetRecipientConditionSelected(ActivityDO activity)
+        {
+            using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
+            {
+                var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
+                configControls.Controls
+                              .OfType<RadioButtonGroup>()
+                              .First()
+                              .Radios
+                              .First(x => x.Name == "recipient")
+                              .Selected = true;
+            }
+        }
 
-    //    private void SetRecipientText(ActivityDO activity)
-    //    {
-    //        using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
-    //        {
-    //            var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
-    //            configControls.Controls
-    //                          .OfType<RadioButtonGroup>()
-    //                          .First()
-    //                          .Radios
-    //                          .First(x => x.Name == "recipient")
-    //                          .Controls
-    //                          .First()
-    //                          .Value = "foo@bar.com";
-    //        }
-    //    }
+        private void SetRecipientText(ActivityDO activity)
+        {
+            using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
+            {
+                var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
+                configControls.Controls
+                              .OfType<RadioButtonGroup>()
+                              .First()
+                              .Radios
+                              .First(x => x.Name == "recipient")
+                              .Controls
+                              .First()
+                              .Value = "foo@bar.com";
+            }
+        }
 
-    //    private void SetTemplateConditionSelected(ActivityDO activity)
-    //    {
-    //        using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
-    //        {
-    //            var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
-    //            configControls.Controls
-    //                          .OfType<RadioButtonGroup>()
-    //                          .First()
-    //                          .Radios
-    //                          .First(x => x.Name == "template")
-    //                          .Selected = true;
-    //        }
-    //    }
+        private void SetTemplateConditionSelected(ActivityDO activity)
+        {
+            using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
+            {
+                var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
+                configControls.Controls
+                              .OfType<RadioButtonGroup>()
+                              .First()
+                              .Radios
+                              .First(x => x.Name == "template")
+                              .Selected = true;
+            }
+        }
 
-    //    private void SetTemplate(ActivityDO activity)
-    //    {
-    //        using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
-    //        {
-    //            var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
-    //            configControls.Controls
-    //                          .OfType<RadioButtonGroup>()
-    //                          .First()
-    //                          .Radios
-    //                          .First(x => x.Name == "template")
-    //                          .Controls
-    //                          .OfType<DropDownList>()
-    //                          .First()
-    //                          .selectedKey = "First";
-    //        }
-    //    }
-    //}
+        private void SetTemplate(ActivityDO activity)
+        {
+            using (var crateStorage = new CrateManager().GetUpdatableStorage(activity))
+            {
+                var configControls = crateStorage.FirstCrate<StandardConfigurationControlsCM>(c => true).Content;
+                configControls.Controls
+                              .OfType<RadioButtonGroup>()
+                              .First()
+                              .Radios
+                              .First(x => x.Name == "template")
+                              .Controls
+                              .OfType<DropDownList>()
+                              .First()
+                              .selectedKey = "First";
+            }
+        }
+    }
 }
