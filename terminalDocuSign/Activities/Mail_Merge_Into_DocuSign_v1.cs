e25ac3b6ae23fc665cc4aaa7af6e4443b07b72be
Andrey Chaplygin <demarsch@yandex.ru>
2016-03-16 15:12:56 +0100

    Fixed Mail_Merge_Into_DocuSign_v1.ValidateActivityInternal to return proper result

--- terminalDocuSign/Activities/Mail_Merge_Into_DocuSign_v1.cs
+++ terminalDocuSign/Activities/Mail_Merge_Into_DocuSign_v1.cs
@@ -144,11 +144,10 @@ namespace terminalDocuSign.Actions
             var control = (T)controls.FindByName(name);
             return control;
         }
-        /*
+        
         protected internal override ValidationResult ValidateActivityInternal(ActivityDO curActivityDO)
         {
             var errorMessages = new List<string>();
-            //validate DocuSignTemplate for present selected template 
             using (var crateStorage = CrateManager.GetUpdatableStorage(curActivityDO))
             {
                 var templateList = GetStdConfigurationControl<DropDownList>(crateStorage, "DocuSignTemplate");
@@ -174,9 +173,9 @@ namespace terminalDocuSign.Actions
                             : "Data source is not selected"
                         : "No data source exists");
             }
+            errorMessages.RemoveAll(string.IsNullOrEmpty);
             return errorMessages.Count == 0 ? ValidationResult.Success : new ValidationResult(string.Join(Environment.NewLine, errorMessages.Where(x => !string.IsNullOrEmpty(x))));
         }
-        */
         /// <summary>
         /// If there's a value in select_file field of the crate, then it is a followup call.
         /// </summary>
