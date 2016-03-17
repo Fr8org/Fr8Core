bf2c7536d5d21f21a264f4b092abac79971bb541
Alexei Avrutin <alexavrutin@gmail.com>
2016-03-17 17:17:04 +0200

    PS script update

--- _PowerShellScripts/UpdateTerminalUrl.ps1
+++ _PowerShellScripts/UpdateTerminalUrl.ps1
@@ -6,5 +6,5 @@
 $rootDir = Split-Path -parent (Split-Path -parent $MyInvocation.MyCommand.Path)
 $terminalList = $rootDir+"\fr8terminals.txt"
 Write-Host "Updating $terminalList"
-$terminals = Get-Content $terminalList  |  % { $_ -replace "localhost", $webHost }
+$terminals = Get-Content $terminalList  |  % { $_ -replace ($_.Substring(0, $_.IndexOf(":"))), $webHost }
 Set-Content $terminalList $terminals
