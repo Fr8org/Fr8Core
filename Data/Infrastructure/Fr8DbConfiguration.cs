85fd89f1260968a99c0a470ac08d77092bc48e88
Yakov Gnusin <y.gnusin@gmail.com>
2016-03-25 15:09:26 +0600

    Added SqlAzureExecutionStrategy strategy to EF db-context.

---
+++ Data/Infrastructure/Fr8DbConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace Data.Infrastructure
+{
+    public class Fr8DbConfiguration : DbConfiguration
+    {
+        public Fr8DbConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
