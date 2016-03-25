de281d8ba37d4fc7c3a4381eb4f4e98943b47ed0
makigjuro <maki.gjuroski@gmail.com>
2016-03-25 11:04:36 +0100

    test fix

--- terminalGoogleTests/Integration/Query_DocuSign_Into_Google_Sheet_Tests.cs
+++ terminalGoogleTests/Integration/Query_DocuSign_Into_Google_Sheet_Tests.cs
@@ -28,7 +28,6 @@ namespace terminalGoogleTests.Integration
         [Test, Category("Integration.terminalGoogle")]
         public async Task Query_DocuSign_Into_Google_Sheet_End_To_End()
         {
-            Debugger.Launch();
             var activityConfigurator = new ActivityConfigurator(this);
             //await RevokeTokens();
            // var googleAuthTokenId = await ExtractGoogleDefaultToken();
