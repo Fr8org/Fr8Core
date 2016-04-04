64c7d437b1deabd026d4508ab2862c4644c9ba87
Volodymyr Krytskyi <vkrytskyy@gmail.com>
2016-04-04 17:50:23 -0400

    Fix git config

--- _PowerShellScripts/MergePRSourcesIntoDev.ps1
+++ _PowerShellScripts/MergePRSourcesIntoDev.ps1
@@ -8,6 +8,10 @@ param(
     [string]$sourceBranchName = $env:BUILD_SOURCEBRANCHNAME
 )
 
+Invoke-Expression 'git config --global user.email "fr8admin@fr8.co"'
+Invoke-Expression 'git config --global user.name "Fr8 Admin"'
+Invoke-Expression 'git config --global -e'
+
 $github_username = "fr8admin"
 $github_password = "ulysses3"
 
