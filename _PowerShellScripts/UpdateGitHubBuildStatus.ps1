fce0dc1be67197196ac6753978f6fb1586e9e447
Volodymyr Krytskyi <vkrytskyy@gmail.com>
2016-04-06 23:11:48 -0400

    Made possible additional check

--- _PowerShellScripts/UpdateGitHubBuildStatus.ps1
+++ _PowerShellScripts/UpdateGitHubBuildStatus.ps1
@@ -111,8 +111,7 @@ else
     UpdateGitHubBuildStatus -message $failure
 }
 
-Invoke-Expression "git checkout . 2> $tempFileName"
-Invoke-Expression "git checkout $branchName 2> $tempFileName"
+Invoke-Expression "git checkout $branchName"
 if ($LastExitCode -ne 0)
 {
 	Write-Host "Failed checkout to $branchName branch."
