e44867553a25d09cc728b38a7303c0aa391b67ae
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2016-04-08 13:57:06 +0600

    WIP! Get_File_List_v1 improvement.

--- terminalDropbox/Activities/Get_File_List_v1.cs
+++ terminalDropbox/Activities/Get_File_List_v1.cs
@@ -6,32 +6,52 @@ using Newtonsoft.Json;
 using StructureMap;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Data.Control;
+using Data.Interfaces.Manifests;
+using terminalDropbox.Interfaces;
 using TerminalBase.BaseClasses;
 using TerminalBase.Infrastructure;
 using terminalDropbox.Services;
 
 namespace terminalDropbox.Actions
 {
-    public class Get_File_List_v1 : BaseTerminalActivity
+    public class Get_File_List_v1 : EnhancedTerminalActivity<Get_File_List_v1.ActivityUi>
     {
-        private readonly DropboxService _dropboxService;
-        protected ICrateManager _crateManager;
-
-        public Get_File_List_v1()
+        public class ActivityUi : StandardConfigurationControlsCM
         {
-            _dropboxService = ObjectFactory.GetInstance<DropboxService>();
+            public DropDownList FileList { get; set; }
+
+            public ActivityUi()
+            {
+                FileList = new DropDownList
+                {
+                    Label = "Select a file",
+                    Name = nameof(FileList),
+                    Required = true,
+                    Events = new List<ControlEvent> { ControlEvent.RequestConfig }
+                };
+                Controls.Add(FileList);
+            }
+        }
+
+        private const string RunTimeCrateLabel = "Files from Dropbox";
+
+        private readonly IDropboxService _dropboxService;
+        private readonly ICrateManager _crateManager;
+
+        public Get_File_List_v1() : base(true)
+        {
+            _dropboxService = ObjectFactory.GetInstance<IDropboxService>();
             _crateManager = ObjectFactory.GetInstance<ICrateManager>();
         }
 
-        public override async Task<ActivityDO> Configure(ActivityDO curActivityDO, AuthorizationTokenDO authTokenDO)
+        protected override async Task Configure(RuntimeCrateManager runtimeCrateManager)
         {
-            if (CheckAuthentication(curActivityDO, authTokenDO))
-            {
-                return curActivityDO;
-            }
-
-            return await ProcessConfigurationRequest(curActivityDO, ConfigurationEvaluator, authTokenDO);
+            var fileNames = await _dropboxService.GetFileList(GetDropboxAuthToken());
+            ConfigurationControls.FileList.ListItems = fileNames.Select(x => new ListItem { Key = x, Value = x }).ToList();
+            runtimeCrateManager.MarkAvailableAtRuntime<StandardFileListCM>(RunTimeCrateLabel);
         }
 
         public async Task<PayloadDTO> Run(ActivityDO curActivityDO, Guid containerId, AuthorizationTokenDO authTokenDO)
@@ -47,20 +67,53 @@ namespace terminalDropbox.Actions
 
             using (var crateStorage = _crateManager.GetUpdatableStorage(payloadCrates))
             {
-                crateStorage.Add(PackCrate_DropboxFileList(fileNames));
+                crateStorage.AddRange(PackCrate_DropboxFileList(fileNames));
             }
 
             return Success(payloadCrates);
         }
 
-        private Crate PackCrate_DropboxFileList(List<string> fileNames)
+        protected override async Task Initialize(RuntimeCrateManager runtimeCrateManager)
         {
-            return Data.Crates.Crate.FromJson("Dropbox File List", JsonConvert.SerializeObject(fileNames));
+            var fileNames = await _dropboxService.GetFileList(GetDropboxAuthToken());
+            ConfigurationControls.FileList.ListItems = fileNames.Select(x => new ListItem { Key = x, Value = x }).ToList();
+            runtimeCrateManager.MarkAvailableAtRuntime<StandardFileListCM>(RunTimeCrateLabel);
         }
 
-        public override ConfigurationRequestType ConfigurationEvaluator(ActivityDO curActivityDO)
+        private AuthorizationTokenDO GetDropboxAuthToken(AuthorizationTokenDO authTokenDO = null)
         {
-            return ConfigurationRequestType.Initial;
+            return authTokenDO ?? AuthorizationToken;
         }
+
+        protected override Task RunCurrentActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<Crate> PackCrate_DropboxFileList(List<string> fileNames)
+        {
+            return fileNames.Select(fileName => Crate.FromContent("File", new StandardFileDescriptionCM()
+            {
+                Filename = fileName
+            }));
+        }
+
+        //public override ConfigurationRequestType ConfigurationEvaluator(ActivityDO curActivityDO)
+        //{
+        //    return ConfigurationRequestType.Initial;
+        //}
+
+        //protected override async Task<ActivityDO> InitialConfigurationResponse(ActivityDO curActivityDO, AuthorizationTokenDO authTokenDO)
+        //{
+        //    var fileNames = await _dropboxService.GetFileList(authTokenDO);
+
+        //    using (var crateStorage = CrateManager.GetUpdatableStorage(curActivityDO))
+        //    {
+        //        crateStorage.Clear();
+        //        crateStorage.Add(PackCrate_DropboxFileList(fileNames));
+        //    }
+
+        //    return await Task.FromResult<ActivityDO>(curActivityDO);
+        //}
     }
 }
