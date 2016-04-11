dd11dc8a77d020007234bf31adfbd438f06f0e13
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2016-04-11 22:03:19 +0600

    Refactor DropboxService according to new API

--- terminalDropbox/Services/DropboxService.cs
+++ terminalDropbox/Services/DropboxService.cs
@@ -15,30 +15,36 @@ namespace terminalDropbox.Services
     public class DropboxService : IDropboxService
     {
         private const string Path = "";
+        private const string UserAgent = "DockyardApp";
+        private const int ReadWriteTimeout = 10*1000;
+        private const int Timeout = 20;
 
         public async Task<List<string>> GetFileList(AuthorizationTokenDO authorizationTokenDO)
         {
-            var client = CreateDropboxClient(authorizationTokenDO.Token);
+            var client = new DropboxClient(authorizationTokenDO.Token, CreateDropboxClientConfig(UserAgent));
 
             var result = await client.Files.ListFolderAsync(Path);
 
             return result.Entries.Select(x => x.Name).ToList();
         }
 
+        private static DropboxClientConfig CreateDropboxClientConfig(string userAgent)
+        {
+            return new DropboxClientConfig
+            {
+                UserAgent = userAgent,
+                HttpClient = CreateHttpClient()
+            };
+        }
+
         private static HttpClient CreateHttpClient()
         {
-            var httpClient = new HttpClient(new WebRequestHandler { ReadWriteTimeout = 10 * 1000 })
+            return new HttpClient(new WebRequestHandler { ReadWriteTimeout = ReadWriteTimeout })
             {
                 // Specify request level timeout which decides maximum time that can be spent on
                 // download/upload files.
-                Timeout = TimeSpan.FromMinutes(20)
+                Timeout = TimeSpan.FromMinutes(Timeout)
             };
-            return httpClient;
-        }
-
-        private static DropboxClient CreateDropboxClient(string token)
-        {
-            return new DropboxClient(token, userAgent: "DockyardApp", httpClient: CreateHttpClient());
         }
     }
 }
