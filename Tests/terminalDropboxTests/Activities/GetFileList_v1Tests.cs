cfbb010a88173bdd1548872990c235d3fb9a25db
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2016-04-13 09:49:58 +0600

    Add Autofixture to create more random test fixture data.

--- Tests/terminalDropboxTests/Activities/GetFileList_v1Tests.cs
+++ Tests/terminalDropboxTests/Activities/GetFileList_v1Tests.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Data.Crates;
+using Data.Entities;
 using Hub.Interfaces;
 using Moq;
 using NUnit.Framework;
 using StructureMap;
 using Data.Interfaces.DataTransferObjects;
 using Hub.Managers.APIManagers.Transmitters.Restful;
+using Ploeh.AutoFixture;
 using terminalDropbox.Actions;
 using terminalDropbox.Interfaces;
 using terminalDropbox.Services;
@@ -23,6 +25,7 @@ namespace terminalDropboxTests.Actions
     public class Get_File_List_v1Tests : BaseTest
     {
         private Get_File_List_v1 _getFileList_v1;
+        private static Fixture _fixture;
 
         public override void SetUp()
         {
@@ -33,21 +36,40 @@ namespace terminalDropboxTests.Actions
             restfulServiceClient.Setup(r => r.GetAsync<PayloadDTO>(It.IsAny<Uri>(), It.IsAny<string>(), It.IsAny<Dictionary<string, string>>()))
                 .Returns(Task.FromResult(FixtureData.FakePayloadDTO));
             ObjectFactory.Configure(cfg => cfg.For<IRestfulServiceClient>().Use(restfulServiceClient.Object));
-            
+
             _getFileList_v1 = ObjectFactory.GetInstance<Get_File_List_v1>();
             _getFileList_v1.HubCommunicator.Configure("terminalDropbox");
+            _fixture = new Fixture();
+            _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            _fixture.Register(() => new AuthorizationTokenDO
+            {
+                Token = "bLgeJYcIkHAAAAAAAAAAFf6hjXX_RfwsFNTfu3z00zrH463seBYMNqBaFpbfBmqf"
+            });
         }
 
         [Test]
         public void Initialize_ReturnsActivityDto()
         {
             //Arrange
-            var curActivityDO = FixtureData.GetFileListTestActivityDO1();
+            ActivityTemplateDO activityTemplateDO = _fixture.Build<ActivityTemplateDO>()
+                .With(x => x.Id)
+                .With(x => x.Name)
+                .With(x => x.Version)
+                .OmitAutoProperties()
+                .Create();
+            ActivityDO curActivityDO = _fixture.Build<ActivityDO>()
+                .With(x => x.Id)
+                .With(x => x.ActivityTemplate, activityTemplateDO)
+                .With(x => x.CrateStorage, string.Empty)
+                .OmitAutoProperties()
+                .Create();
 
+            AuthorizationTokenDO tokenDO = _fixture.Create<AuthorizationTokenDO>();
 
             //Act
-            var activityDto = _getFileList_v1.Configure(curActivityDO, FixtureData.DropboxAuthorizationToken()).Result;
-            
+            var activityDto = _getFileList_v1.Configure(curActivityDO, tokenDO).Result;
+
             // Assert
             Assert.NotNull(activityDto);
         }
