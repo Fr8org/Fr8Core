f97598b895639ff4ddb7ffc421c24ca099e858fb
Anton Yakovets <jokerosky@gmail.com>
2016-04-13 18:17:34 +0600

    FR-2659 commit after merge conflicts

--- Data/Migrations/201604131156079_Rename_HistoryItemDO_CustomerId_to_Fr8UserId.cs
+++ Data/Migrations/201604131156079_Rename_HistoryItemDO_CustomerId_to_Fr8UserId.cs
@@ -2,19 +2,17 @@ namespace Data.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-
+    
     public partial class Rename_HistoryItemDO_CustomerId_to_Fr8UserId : DbMigration
     {
         public override void Up()
         {
-            RenameColumn(table: "dbo.History", name: "CustomerId", newName: "Fr8UserId");
-
+            RenameColumn("dbo.History", "CustomerId", "Fr8UserId");
         }
-
+        
         public override void Down()
         {
-            RenameColumn(table: "dbo.History", name: "Fr8UserId", newName: "CustomerId");
-
+            RenameColumn("dbo.History", "Fr8UserId", "CustomerId");
         }
     }
 }
