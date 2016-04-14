1e6df2e46f98b008b4a0b4bce2c4e7bf09d40c8e
blazingmind <blaizingmind@gmail.com>
2016-04-15 01:51:38 +0300

    new Run method for PlanController that allows plan execution continuation

--- Tests/DockyardTest/Services/Container/ContainerExecutionTests.cs
+++ Tests/DockyardTest/Services/Container/ContainerExecutionTests.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using StructureMap;
 // This alias is used to avoid ambiguity between StructureMap.IContainer and Core.Interfaces.IContainer
-using InternalInterface = Hub.Interfaces;
 using Data.Interfaces;
-using Hub.Managers;
-using UtilitiesTesting;
 using System.Threading.Tasks;
 using Data.Constants;
 using Data.Crates;
@@ -22,36 +18,15 @@ namespace DockyardTest.Services
 {
     [TestFixture]
     [Category("ContainerExecute")]
-    public class ContainerExecutionTests : BaseTest
+    public class ContainerExecutionTests : ContainerExecutionTestBase
     {
-
-        private ActivityServiceMock ActivityService;
-        private InternalInterface.IContainer _container;
-        private ICrateManager _crateManager;
-        private InternalInterface.IPlan _plan;
-
-        [SetUp]
-        //constructor method as it is run at the test start
-        public override void SetUp()
-        {
-            base.SetUp();
-
-            _crateManager = ObjectFactory.GetInstance<ICrateManager>();
-            ActivityService = new ActivityServiceMock(ObjectFactory.GetInstance<InternalInterface.IActivity>());
-            ObjectFactory.Container.Inject(typeof (InternalInterface.IActivity), ActivityService);
-            _container = ObjectFactory.GetInstance<InternalInterface.IContainer>();
-            _plan = ObjectFactory.GetInstance<InternalInterface.IPlan>();
-
-            FixtureData.AddTestActivityTemplate();
-        }
-
         [Test]
         [ExpectedException(typeof (ArgumentNullException))]
         public async Task Execute_ContainerDoIsNull_ThrowsArgumentNullException()
         {
             using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
             {
-                await _container.Run(uow, null);
+                await Container.Run(uow, null);
             }
         }
 
@@ -107,7 +82,7 @@ namespace DockyardTest.Services
 
                 uow.SaveChanges();
 
-                await _plan.Run(uow, plan);
+                await Plan.Run(uow, plan);
 
                 AssertExecutionSequence(new[]
                 {
@@ -167,7 +142,7 @@ namespace DockyardTest.Services
 
                 uow.SaveChanges();
 
-                await _plan.Run(uow, plan);
+                await Plan.Run(uow, plan);
 
                 AssertExecutionSequence(new[]
                 {
@@ -279,7 +254,7 @@ namespace DockyardTest.Services
 
                 uow.SaveChanges();
 
-                await _plan.Run(uow, plan);
+                await Plan.Run(uow, plan);
 
                 AssertExecutionSequence(new[]
                 {
@@ -401,15 +376,15 @@ namespace DockyardTest.Services
 
                 plan.PlanState = PlanState.Active;
                 plan.StartingSubPlan = (SubPlanDO) plan.ChildNodes[0];
-                ActivityService.CustomActivities[FixtureData.GetTestGuidById(4)] = new SuspenderActivityMock(_crateManager);
+                ActivityService.CustomActivities[FixtureData.GetTestGuidById(4)] = new SuspenderActivityMock(CrateManager);
 
                 uow.SaveChanges();
 
-                await _plan.Run(uow, plan);
+                await Plan.Run(uow, plan);
 
                 var container = uow.ContainerRepository.GetQuery().Single(x => x.PlanId == plan.Id);
 
-                using (var storage = _crateManager.UpdateStorage(() => container.CrateStorage))
+                using (var storage = CrateManager.UpdateStorage(() => container.CrateStorage))
                 {
                     var opState = storage.CrateContentsOfType<OperationalStateCM>().First();
 
@@ -420,7 +395,7 @@ namespace DockyardTest.Services
                 uow.SaveChanges();
 
                 ActivityService.CustomActivities.Remove(FixtureData.GetTestGuidById(4)); // we are not interested in suspender logic testing here. Just allow activity to run by default.
-                await _plan.Continue(container.Id);
+                await Plan.Continue(container.Id);
                 
                 AssertExecutionSequence(new[]
                 {
@@ -490,14 +465,14 @@ namespace DockyardTest.Services
                     }
                 });
 
-                ActivityService.CustomActivities[FixtureData.GetTestGuidById(3)] = new JumperActivityMock(_crateManager, FixtureData.GetTestGuidById(5));
+                ActivityService.CustomActivities[FixtureData.GetTestGuidById(3)] = new JumperActivityMock(CrateManager, FixtureData.GetTestGuidById(5));
 
                 plan.PlanState = PlanState.Active;
                 plan.StartingSubPlan = (SubPlanDO)plan.ChildNodes[0];
 
                 uow.SaveChanges();
 
-                await _plan.Run(uow, plan);
+                await Plan.Run(uow, plan);
 
                 AssertExecutionSequence(new[]
                 {
@@ -568,14 +543,14 @@ namespace DockyardTest.Services
                     }
                 });
 
-                ActivityService.CustomActivities[FixtureData.GetTestGuidById(3)] = new JumperActivityMock(_crateManager, FixtureData.GetTestGuidById(5));
+                ActivityService.CustomActivities[FixtureData.GetTestGuidById(3)] = new JumperActivityMock(CrateManager, FixtureData.GetTestGuidById(5));
 
                 plan.PlanState = PlanState.Active;
                 plan.StartingSubPlan = (SubPlanDO)plan.ChildNodes[0];
 
                 uow.SaveChanges();
 
-                await _plan.Run(uow, plan);
+                await Plan.Run(uow, plan);
 
                 AssertExecutionSequence(new[]
                 {
@@ -621,14 +596,14 @@ namespace DockyardTest.Services
                     }
                 });
 
-                ActivityService.CustomActivities[FixtureData.GetTestGuidById(2)] = new JumpToSelfActivityMock(_crateManager);
+                ActivityService.CustomActivities[FixtureData.GetTestGuidById(2)] = new JumpToSelfActivityMock(CrateManager);
 
                 plan.PlanState = PlanState.Active;
                 plan.StartingSubPlan = (SubPlanDO)plan.ChildNodes[0];
 
                 uow.SaveChanges();
 
-                await _plan.Run(uow, plan);
+                await Plan.Run(uow, plan);
 
                 AssertExecutionSequence(new[]
                 {
@@ -699,9 +674,9 @@ namespace DockyardTest.Services
 
                 uow.SaveChanges();
 
-                ActivityService.CustomActivities[FixtureData.GetTestGuidById(2)] = new SubplanJumperActivityMock(_crateManager, FixtureData.GetTestGuidById(4));
+                ActivityService.CustomActivities[FixtureData.GetTestGuidById(2)] = new SubplanJumperActivityMock(CrateManager, FixtureData.GetTestGuidById(4));
 
-                await _plan.Run(uow, plan);
+                await Plan.Run(uow, plan);
 
                 AssertExecutionSequence(new[]
                 {
@@ -772,9 +747,9 @@ namespace DockyardTest.Services
 
                 uow.SaveChanges();
 
-                ActivityService.CustomActivities[FixtureData.GetTestGuidById(2)] = new CallerActivityMock(_crateManager, FixtureData.GetTestGuidById(4));
+                ActivityService.CustomActivities[FixtureData.GetTestGuidById(2)] = new CallerActivityMock(CrateManager, FixtureData.GetTestGuidById(4));
 
-                await _plan.Run(uow, plan);
+                await Plan.Run(uow, plan);
                 
                 AssertExecutionSequence(new[]
                 {
@@ -834,9 +809,9 @@ namespace DockyardTest.Services
 
                 uow.SaveChanges();
 
-                ActivityService.CustomActivities[FixtureData.GetTestGuidById(3)] = new SuspenderActivityMock(_crateManager);
+                ActivityService.CustomActivities[FixtureData.GetTestGuidById(3)] = new SuspenderActivityMock(CrateManager);
 
-                await _plan.Run(uow, plan);
+                await Plan.Run(uow, plan);
 
                 Assert.AreEqual(State.Suspended, uow.ContainerRepository.GetQuery().Single(x => x.PlanId == plan.Id).State, "Invalid container state");
                 AssertExecutionSequence(new[]
@@ -845,7 +820,7 @@ namespace DockyardTest.Services
                     new ActivityExecutionCall(ActivityExecutionMode.InitialRun, FixtureData.GetTestGuidById(3)),
                 }, ActivityService.ExecutedActivities);
 
-                await _plan.Continue(uow.ContainerRepository.GetQuery().Single(x => x.PlanId == plan.Id).Id);
+                await Plan.Continue(uow.ContainerRepository.GetQuery().Single(x => x.PlanId == plan.Id).Id);
 
                 Assert.AreEqual(1, uow.ContainerRepository.GetQuery().Count(x => x.PlanId == plan.Id), "New container was stared");
 
@@ -932,10 +907,10 @@ namespace DockyardTest.Services
 
                 uow.SaveChanges();
 
-                ActivityService.CustomActivities[FixtureData.GetTestGuidById(3)] = new LooperActivityMock(_crateManager, 2);
-                ActivityService.CustomActivities[FixtureData.GetTestGuidById(4)] = new CallerActivityMock(_crateManager, FixtureData.GetTestGuidById(5));
+                ActivityService.CustomActivities[FixtureData.GetTestGuidById(3)] = new LooperActivityMock(CrateManager, 2);
+                ActivityService.CustomActivities[FixtureData.GetTestGuidById(4)] = new CallerActivityMock(CrateManager, FixtureData.GetTestGuidById(5));
 
-                await _plan.Run(uow, plan);
+                await Plan.Run(uow, plan);
 
                 AssertExecutionSequence(new[]
                 {
@@ -954,18 +929,6 @@ namespace DockyardTest.Services
                    new ActivityExecutionCall(ActivityExecutionMode.ReturnFromChildren, FixtureData.GetTestGuidById(3)),
                    new ActivityExecutionCall(ActivityExecutionMode.InitialRun, FixtureData.GetTestGuidById(3)),
                 }, ActivityService.ExecutedActivities);
-            }
-        }
-
-
-        private void AssertExecutionSequence(ActivityExecutionCall[] expected, List<ActivityExecutionCall> actual)
-        {
-            Assert.AreEqual(expected.Length, actual.Count, "Invalid count of activity executions");
-
-            for (int i = 0; i < expected.Length; i ++)
-            {
-                Assert.AreEqual(expected[i].Id, actual[i].Id, $"Invalid activtiy is executed at step {i}");
-                Assert.AreEqual(expected[i].Mode, actual[i].Mode, $"Invalid activtiy execution mode at step {i}");
             }
         }
     }
