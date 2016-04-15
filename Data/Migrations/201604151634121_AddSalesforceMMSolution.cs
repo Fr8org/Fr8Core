7507034c83055d0b2356a758d97b4657f2c312cc
Andrey Chaplygin <demarsch@yandex.ru>
2016-04-15 18:34:43 +0200

    Recreated manual migration

--- Data/Migrations/201604151634121_AddSalesforceMMSolution.cs
+++ Data/Migrations/201604151634121_AddSalesforceMMSolution.cs
@@ -3,7 +3,7 @@ namespace Data.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class AddOrUpdateSalesforceMMSolution : DbMigration
+    public partial class AddSalesforceMMSolution : DbMigration
     {
         public override void Up()
         {
