933ea7f1ea8f89318520abd61766a81cac896904
Volodymyr Krytskyi <vkrytskyy@gmail.com>
2016-04-18 23:09:37 -0400

    :fire: cleaning commit

--- Data/Interfaces/DataTransferObjects/FieldDTO.cs
+++ Data/Interfaces/DataTransferObjects/FieldDTO.cs
@@ -5,7 +5,6 @@ using Newtonsoft.Json;
 
 namespace Data.Interfaces.DataTransferObjects
 {
-
     [JsonConverter(typeof(FieldConverter))]
     [System.Diagnostics.DebuggerDisplay("Key = '{Key}', Value = '{Value}'")]
     public class FieldDTO : System.ICloneable
