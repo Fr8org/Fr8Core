a1aca6dc5699597f26a0e72743e9390a3bc1d9df
Bahadır Bozdağ <bahadir.bb@gmail.com>
2016-04-22 11:04:49 +0300

    Modify .gitignore

---
+++ Scripts/tests/e2e/login/login.spec.js
@@ -0,0 +1,21 @@
+﻿var LoginPage = require('../pages/login.page.js');
+
+describe('login page tests', function () {
+    var loginPage;
+
+    beforeEach(function () {
+        loginPage = new LoginPage();
+        loginPage.get();
+    });
+
+    it('should login', function () {
+        loginPage.setEmail("integration_test_runner@fr8.company");
+        loginPage.setPassword("fr8#s@lt!");
+        loginPage.login();
+        browser.waitForAngular();
+        expect(browser.getCurrentUrl()).toContain('Welcome');
+    });
+
+});
+ 
+   
