49f1dcdb40ef09ecbe1f1b89d9fc66a6c857f9d3
Bahadır Bozdağ <bahadir.bb@gmail.com>
2016-04-22 11:35:34 +0300

    switched to phantomjs

--- Scripts/tests/e2e/conf.js
+++ Scripts/tests/e2e/conf.js
@@ -1,7 +1,17 @@
 ﻿exports.config = {
-    directConnect: true,
+    //directConnect: true,
     capabilities: {
-        'browserName': 'chrome'
+        'browserName': 'phantomjs',
+        /* 
+        * Can be used to specify the phantomjs binary path.
+        * This can generally be ommitted if you installed phantomjs globally.
+        */
+        'phantomjs.binary.path': require('phantomjs').path,
+        /*
+         * Command line args to pass to ghostdriver, phantomjs's browser driver.
+         * See https://github.com/detro/ghostdriver#faq
+         */
+        'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
     },
     framework: 'jasmine',
     specs: ['**/*.spec.js'],
