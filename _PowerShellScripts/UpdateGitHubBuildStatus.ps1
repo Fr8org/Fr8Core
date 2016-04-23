a2b715e70cab142adb08a6485658cc830bb644b0
Charles Pretzer <charles@fr8.co>
2016-04-23 01:47:18 -0700

    * FR-3150 update script logging

--- _PowerShellScripts/UpdateGitHubBuildStatus.ps1
+++ _PowerShellScripts/UpdateGitHubBuildStatus.ps1
@@ -108,8 +108,8 @@ catch
 }
 
 $tasks = $vsoResponse.records | where {$_.type -eq "Task"}
-$succeededBuildSteps = $tasks | where {$_.name -notlike "Update GitHub status*" -and $_.state -eq "completed" -and $_.result -eq "succeeded" -and $_.state -ne 'Disabled'}
-Write-Host "Comparing succeded steps with count " $succeededBuildSteps.Count " - " $tasks.Count
+$succeededBuildSteps = $tasks | where {$_.name -notlike "Update GitHub status*" -and $_.state -eq "completed" -and $_.result -eq "succeeded"}
+Write-Host "Comparing succeded steps with count " $succeededBuildSteps.Count " - " ($tasks.Count - 1)
 if ($succeededBuildSteps.Count -eq $tasks.Count - 1)
 {
     UpdateGitHubBuildStatus -message $success
