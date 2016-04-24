4a0897461c37389237ea5c1fa5eb9b286bafeffa
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2016-04-24 12:11:32 +0600

    Add TerminalQuickBooksBootstrapper to configure StructureMap

---
+++ terminalQuickBooks/TerminalQuickBooksBootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using Hub.Managers;
+using Hub.StructureMap;
+using StructureMap;
+using terminalQuickBooks.Infrastructure;
+using terminalQuickBooks.Interfaces;
+
+namespace terminalQuickBooks
+{
+    public static class TerminalQuickbooksBootstrapper
+    {
+        public static void ConfigureQuickbooksDependencies(this IContainer container, StructureMapBootStrapper.DependencyType type)
+        {
+            switch (type)
+            {
+                case StructureMapBootStrapper.DependencyType.TEST:
+                    container.Configure(ConfigureLive); // no test mode yet
+                    break;
+
+                case StructureMapBootStrapper.DependencyType.LIVE:
+                    container.Configure(ConfigureLive);
+                    break;
+            }
+        }
+
+        public static void ConfigureLive(ConfigurationExpression configurationExpression)
+        {
+            configurationExpression.For<IAuthenticator>().Use<Authenticator>();
+            configurationExpression.For<ICrateManager>().Use<CrateManager>();
+        }
+    }
+}
