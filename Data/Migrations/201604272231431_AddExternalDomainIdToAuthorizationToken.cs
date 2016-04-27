14e7be9cf0489403687e5510c4347c3d098c658f
Andrey Chaplygin <demarsch@yandex.ru>
2016-04-28 00:36:27 +0200

    Resolving migration conflict

--- Data/Migrations/201604272231431_AddExternalDomainIdToAuthorizationToken.cs
+++ Data/Migrations/201604272231431_AddExternalDomainIdToAuthorizationToken.cs
@@ -3,7 +3,7 @@ namespace Data.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class AddExternalDomainIdToAuthToken : DbMigration
+    public partial class AddExternalDomainIdToAuthorizationToken : DbMigration
     {
         public override void Up()
         {
