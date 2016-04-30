84d4478caf932f2211a0bd74e6720dd8af146559
Bahadır Bozdağ <bahadir.bb@gmail.com>
2016-04-30 13:46:03 +0300

    Modified  powershell script to exit with error code

--- _PowerShellScripts/Run-ProtractorTests.ps1
+++ _PowerShellScripts/Run-ProtractorTests.ps1
@@ -9,7 +9,5 @@ $projectDirectory = (get-item $RootDir ).parent.FullName
 $GulpProtractorCmd = "$projectDirectory\node_modules\.bin\gulp.cmd"
 $arguments = "e2etests --baseUrl $endPoint"
 Write-Host $GulpProtractorCmd $arguments
-
-#Invoke-Expression $GulpProtractorCmd | Out-Null
-
-Start-Process $GulpProtractorCmd $arguments -NoNewWindow -Wait
+$process = Start-Process $GulpProtractorCmd $arguments -NoNewWindow -Wait -PassThru
+Exit $process.ExitCode
