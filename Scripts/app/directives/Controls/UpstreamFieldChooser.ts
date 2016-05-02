39db1ad5cd50c2b211117624f2da082eb3af6726
jarekrzdbk <jarekrzdbk@gmail.com>
2016-05-02 21:02:02 +0300

    modified ui

--- Scripts/app/directives/Controls/UpstreamFieldChooser.ts
+++ Scripts/app/directives/Controls/UpstreamFieldChooser.ts
@@ -14,13 +14,14 @@ module dockyard.directives.upstreamDataChooser {
         openModal: () => void;
         ok: () => void;
         cancel: () => void;
+        expand: () => void;
     }
 
     //More detail on creating directives in TypeScript: 
     //http://blog.aaronholmes.net/writing-angularjs-directives-as-typescript-classes/
     class UpstreamFieldChooser implements ng.IDirective {
         public link: (scope: IUpstreamFieldChooser, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => void;
-        public controller: ($scope: IUpstreamFieldChooser, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, UpstreamExtractor: services.UpstreamExtractor, $modal: any) => void;
+        public controller: ($scope: IUpstreamFieldChooser, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, UpstreamExtractor: services.UpstreamExtractor, $modal: any, $timeout: any) => void;
 
         public template = '<button class="btn btn-primary btn-xs" ng-click="openModal()">Select</button><span>{{field.value}}</span>';
         public restrict = 'E';
@@ -47,8 +48,17 @@ module dockyard.directives.upstreamDataChooser {
                 $element: ng.IAugmentedJQuery,
                 $attrs: ng.IAttributes,
                 UpstreamExtractor: services.UpstreamExtractor,
-                $modal: any            ) => {
+                $modal: any,
+                $timeout: any) => {
                 var modalInstance;
+                $scope.expand = () => {
+                    $timeout(function () {
+                        $(".modal-content").parent().css({
+
+                            'height': () => {return $(".modal-header.ng-scope").height()+40}
+                        })
+                    }, 0);
+                }
                 $scope.openModal = () => {
                     getUpstreamFields();
                     if ($scope.field.listItems.length !== 0) {
@@ -56,6 +66,47 @@ module dockyard.directives.upstreamDataChooser {
                             animation: true,
                             templateUrl: '/AngularTemplate/UpstreamFieldChooser',
                             scope: $scope,
+                            controller: ['$scope','$modalInstance',function ($scope, $modalInstance) {
+                                $timeout(function () {
+                                    (<any>$(".modal-content"))
+                                        .wrap('<div align="center"></div>')
+                                        .css({
+                                            'overflow': 'hidden',
+                                            'max-height': () => { return $(document).height() - 80 }
+                                        })
+                                        .parent()
+                                        .css({
+                                            'min-width': () => {
+                                                var minWidth = parseInt($(".modal-content").attr("min-width"));
+                                                if (angular.isNumber(minWidth) && !isNaN(minWidth) && minWidth !== 0) {
+                                                    return minWidth;
+                                                }
+                                                var curWidth = $(".modal-content").width();
+                                                return curWidth > 0 ? curWidth : 300;
+                                            },
+                                            'width': () => {
+                                                var minWidth = parseInt($(".modal-content").attr("min-width"));
+                                                if (angular.isNumber(minWidth) && !isNaN(minWidth) && minWidth !== 0) {
+                                                    return minWidth;
+                                                }
+                                                var curWidth = $(".modal-content").width();
+                                                return curWidth > 0 ? curWidth : 300;
+                                            },
+                                            'min-height': () => { return $(".modal-header").height()},
+                                            'max-height': () => {
+                                                var curHeight = $(".modal-content").height();
+                                                return curHeight;
+                                            }
+                                        })
+                                        .resizable().draggable()
+                                        .find($(".modal-content"))
+                                        .css({
+                                            overflow: 'auto',
+                                            width: '100%',
+                                            height: '100%'
+                                        });
+                                }, 0);
+                            }],
                             resolve: {
                                 items: function () {
                                     return $scope.field.listItems;
@@ -64,21 +115,21 @@ module dockyard.directives.upstreamDataChooser {
                         });
                         modalInstance.result.then(function (selectedItem) {
                             $scope.field.value = selectedItem;
-                        });
+                            //(<any>$(".modal-content")).parent().css({ 'max-height': 0})
+                        })
                     }
                 }
                 $scope.setItem = (item) => {
                     $scope.selectedFieldValue = item;
-                };
-                $scope.ok = function () {
                     modalInstance.close($scope.selectedFieldValue);
+                    (<any>$(".modal-content")).parent().css({
+                        'min-height': 0 
+                    });
                     if ($scope.change != null && angular.isFunction($scope.change)) {
                         $scope.change()($scope.field);
                     }
                 };
-                $scope.cancel = function () {
-                    modalInstance.dismiss('cancel');
-                };
+
                 var getUpstreamFields = () => {
                     UpstreamExtractor
                         .getAvailableData($scope.currentAction.id, 'Field Description')
@@ -86,7 +137,6 @@ module dockyard.directives.upstreamDataChooser {
                             var listItems: Array<model.DropDownListItem> = [];
 
                             angular.forEach(<Array<any>>data.availableFields, it => {
-                                //var fields = <Array<model.FieldDTO>>cm;
                                     var i, j;
                                     var found = false;
                                     for (i = 0; i < listItems.length; ++i) {
@@ -99,7 +149,6 @@ module dockyard.directives.upstreamDataChooser {
                                         listItems.push(<model.DropDownListItem>it);
                                     }
                             });
-
                             listItems.sort((x, y) => {
                                 if (x.key < y.key) {
                                     return -1;
@@ -121,7 +170,7 @@ module dockyard.directives.upstreamDataChooser {
                         });
                 };
             }
-            UpstreamFieldChooser.prototype.controller['$inject'] = ['$scope', '$element', '$attrs', 'UpstreamExtractor', '$modal'];
+            UpstreamFieldChooser.prototype.controller['$inject'] = ['$scope', '$element', '$attrs', 'UpstreamExtractor', '$modal','$timeout'];
         };
 
 
