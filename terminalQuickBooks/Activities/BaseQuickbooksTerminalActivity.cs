ceda244fa07e44fda4bebd3e8736956867d772bb
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2016-05-03 10:56:27 +0600

    Add BaseQuickbooksTerminalActivity

---
+++ terminalQuickBooks/Activities/BaseQuickbooksTerminalActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Data.Interfaces.Manifests;
+using terminalQuickBooks.Infrastructure;
+using TerminalBase.BaseClasses;
+
+namespace terminalQuickBooks.Actions
+{
+    public abstract class BaseQuickbooksTerminalActivity<T> : EnhancedTerminalActivity<T>
+        where T : StandardConfigurationControlsCM
+    {
+        public BaseQuickbooksTerminalActivity() : base(true)
+        {
+        }
+
+        protected override bool IsTokenInvalidation(Exception ex)
+        {
+            return ex is TerminalQuickbooksTokenExpiredException;
+        }
+    }
+}
