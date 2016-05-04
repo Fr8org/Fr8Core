530b155e301acebfc96c72a57ac0d6b4a7a1e02b
Volodymyr Krytskyi <vkrytskyy@gmail.com>
2016-05-04 13:16:21 -0300

    Renamed script

--- _PowerShellScripts/Set-GitHubBuildStatus.ps1
+++ _PowerShellScripts/Set-GitHubBuildStatus.ps1
@@ -1,7 +1,6 @@
 <#
     .SYNOPSIS
-    The script updates the status of build on GitHub to disable Merge button if something wrong happened
-    This script is intended to avoid situation when new pull requests breaks dev branch after merge. 
+    The script updates the status of build on GitHub     
 #>
 
 param(
@@ -17,14 +16,14 @@ param(
 	[Parameter(Mandatory = $true)]
 	[string]$github_password,
 
-	[string]$mainBranchName = "dev",
+	[string]$buildDefinitionId = $env:SYSTEM_DEFINITIONID,
     [string]$buildId = $env:BUILD_BUILDID,
-	[string]$branchName = $env:BUILD_SOURCEBRANCHNAME,
-	[string]$tempDirectory = $env:BUILD_STAGINGDIRECTORY
+	[string]$branchName = $env:BUILD_SOURCEBRANCHNAME
 )
 
-$tempFileName = $tempDirectory + "\gitCommandsOutput+$branchName.txt"
 $target_url = "https://fr8.visualstudio.com/DefaultCollection/fr8/_build?_a=summary&buildId=" + $buildId
+[uri]$vsoTimelineUri = "https://fr8.visualstudio.com/defaultcollection/fr8/_apis/build/builds/" + $buildId + "/timeline?api-version=2.0"
+[uri]$vsoBuildDefinitionUri = "https://fr8.visualstudio.com/defaultcollection/fr8/_apis/build/definitions/" + $buildDefinitionId + "?api-version=2.0"
 
 $failure = @{
 				state = "failure"
@@ -40,17 +39,17 @@ $success = @{
 				context = "feature-branch-ci/vso"
 			} | ConvertTo-Json
 
-Function DeleteBranchIfExists($buildBranchName)
-{
-	$command = "git branch --list $buildBranchName"
-	$result = Invoke-Expression $command
 
-	if (![System.String]::IsNullOrEmpty($result))
-	{
-		$command = "git branch -D $buildBranchName"
-		Invoke-Expression $command    
-	}
-}
+$basicAuthVSO = ("{0}:{1}" -f $vso_username,$vso_password)
+$basicAuthVSO = [System.Text.Encoding]::UTF8.GetBytes($basicAuthVSO)
+$basicAuthVSO = [System.Convert]::ToBase64String($basicAuthVSO)
+$headersVSO = @{Authorization=("Basic {0}" -f $basicAuthVSO)}
+
+$basicAuthGitHub = ("{0}:{1}" -f $github_username,$github_password)
+$basicAuthGitHub = [System.Text.Encoding]::UTF8.GetBytes($basicAuthGitHub)
+$basicAuthGitHub = [System.Convert]::ToBase64String($basicAuthGitHub)
+$headersGitHub = @{Authorization=("Basic {0}" -f $basicAuthGitHub)}
+
 
 Function UpdateGitHubBuildStatus($message)
 {
@@ -84,33 +83,39 @@ Function UpdateGitHubBuildStatus($message)
 	}	
 }
 
-$basicAuthVSO = ("{0}:{1}" -f $vso_username,$vso_password)
-$basicAuthVSO = [System.Text.Encoding]::UTF8.GetBytes($basicAuthVSO)
-$basicAuthVSO = [System.Convert]::ToBase64String($basicAuthVSO)
-$headersVSO = @{Authorization=("Basic {0}" -f $basicAuthVSO)}
+$vsoBDResponse = try
+{
+	Invoke-RestMethod -Uri $vsoBuildDefinitionUri -headers $headersVSO -Method Get -ContentType 'application/json'
+}
+catch
+{
+	Write-Host "Failed to get current build definition info."
+    Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
+    Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
+	exit 1
+}
 
-$basicAuthGitHub = ("{0}:{1}" -f $github_username,$github_password)
-$basicAuthGitHub = [System.Text.Encoding]::UTF8.GetBytes($basicAuthGitHub)
-$basicAuthGitHub = [System.Convert]::ToBase64String($basicAuthGitHub)
-$headersGitHub = @{Authorization=("Basic {0}" -f $basicAuthGitHub)}
+# get all enabled build steps. 
+# Note: "Get Souces" step is not included in the response
+$activeBuildSteps = $vsoBDResponse.build | where {$_.enabled -eq "true"}
 
-[uri]$vsoTimelineUri = "https://fr8.visualstudio.com/defaultcollection/fr8/_apis/build/builds/" + $buildId + "/timeline?api-version=2.0"
 $vsoResponse = try
 {
 	Invoke-RestMethod -Uri $vsoTimelineUri -headers $headersVSO -Method Get -ContentType 'application/json'
 }
 catch
 {
-	Write-Host "Failed to get current build status"
+	Write-Host "Failed to get current build timeline."
     Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
     Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
 	exit 1
 }
+$activeTasks = $vsoResponse.records | where {$_.type -eq "Task" -and $_.name -notlike "Update GitHub status*"}
+$succeededBuildSteps = $activeTasks | where {$_.state -eq "completed" -and $_.result -eq "succeeded"}
 
-$tasks = $vsoResponse.records | where {$_.type -eq "Task"}
-$succeededBuildSteps = $tasks | where {$_.name -notlike "Update GitHub status*" -and $_.state -eq "completed" -and $_.result -eq "succeeded" -and $_.state -ne 'Disabled'}
-Write-Host "Comparing succeded steps with count " $succeededBuildSteps.Count " - " ($tasks.Count - 1)
-if ($succeededBuildSteps.Count -eq $tasks.Count - 1)
+Write-Host "Comparing succeded steps with active build steps count " $succeededBuildSteps.Count " - " $activeBuildSteps.Count
+
+if ($succeededBuildSteps.Count -eq $activeBuildSteps.Count)
 {
     UpdateGitHubBuildStatus -message $success
 }
@@ -119,20 +124,4 @@ else
     UpdateGitHubBuildStatus -message $failure
 }
 
-Invoke-Expression "git checkout . 2> $tempFileName"
-Invoke-Expression "git checkout $branchName 2> $tempFileName"
-if ($LastExitCode -ne 0)
-{
-	Write-Host "Failed checkout to $branchName branch."
-	exit 1
-}
-else
-{
-	$buildBranchName = $mainBranchName + "+" + $branchName
-	DeleteBranchIfExists $buildBranchName
-}
-
-if (Test-Path $tempFileName) {
-  Remove-Item $tempFileName
-}
 
