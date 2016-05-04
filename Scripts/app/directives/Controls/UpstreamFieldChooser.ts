db0349f23a74c8319610fb4039e502e57ebc7952
jarekrzdbk <jarekrzdbk@gmail.com>
2016-05-04 17:24:14 +0300

    fixed tests

--- Scripts/app/directives/Controls/UpstreamFieldChooser.ts
+++ Scripts/app/directives/Controls/UpstreamFieldChooser.ts
@@ -17,11 +17,141 @@ module dockyard.directives.upstreamDataChooser {
         expand: () => void;
     }
 
+    export class UpstreamFieldChooserController {
+
+        static $inject = ['$scope', '$element', '$attrs', 'UpstreamExtractor', '$modal', '$timeout','NgTableParams'];
+        constructor($scope: IUpstreamFieldChooser,
+            $element: ng.IAugmentedJQuery,
+            $attrs: ng.IAttributes,
+            UpstreamExtractor: services.UpstreamExtractor,
+            $modal: any,
+            $timeout: any,
+            NgTableParams) {
+            var modalInstance;
+            $scope.expand = () => {
+                $timeout(function () {
+                    $(".modal-content").parent().css({
+
+                        'height': () => { return $(".modal-header.ng-scope").height() + 40 }
+                    })
+                }, 0);
+            }
+            $scope.openModal = () => {
+                getUpstreamFields();
+                if ($scope.field.listItems.length !== 0) {
+                    modalInstance = $modal.open({
+                        animation: true,
+                        templateUrl: '/AngularTemplate/UpstreamFieldChooser',
+                        scope: $scope,
+                        controller: ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+                            $timeout(function () {
+                                (<any>$(".modal-content"))
+                                    .wrap('<div align="center"></div>')
+                                    .css({
+                                        'overflow': 'hidden',
+                                        'max-height': () => { return $(document).height() - 80 }
+                                    })
+                                    .parent()
+                                    .css({
+                                        'min-width': () => {
+                                            var minWidth = parseInt($(".modal-content").attr("min-width"));
+                                            if (angular.isNumber(minWidth) && !isNaN(minWidth) && minWidth !== 0) {
+                                                return minWidth;
+                                            }
+                                            var curWidth = $(".modal-content").width();
+                                            return curWidth > 0 ? curWidth : 300;
+                                        },
+                                        'width': () => {
+                                            var minWidth = parseInt($(".modal-content").attr("min-width"));
+                                            if (angular.isNumber(minWidth) && !isNaN(minWidth) && minWidth !== 0) {
+                                                return minWidth;
+                                            }
+                                            var curWidth = $(".modal-content").width();
+                                            return curWidth > 0 ? curWidth : 300;
+                                        },
+                                        'min-height': () => { return $(".modal-header").height() },
+                                        'max-height': () => {
+                                            var curHeight = $(".modal-content").height();
+                                            if (curHeight > 85) {
+                                                return curHeight;
+                                            }
+                                        }
+                                    })
+                                    .resizable().draggable()
+                                    .find($(".modal-content"))
+                                    .css({
+                                        overflow: 'auto',
+                                        width: '100%',
+                                        height: '100%'
+                                    });
+                            }, 0);
+                        }],
+                        resolve: {
+                            items: function () {
+                                return $scope.field.listItems;
+                            }
+                        }
+                    });
+                }
+            }
+            $scope.setItem = (item) => {
+                $scope.field.value = item;
+                modalInstance.close($scope.field.value);
+                (<any>$(".modal-content")).parent().css({
+                    'min-height': 0
+                });
+                if ($scope.change != null && angular.isFunction($scope.change)) {
+                    $scope.change()($scope.field);
+                }
+            };
+
+            var getUpstreamFields = () => {
+                UpstreamExtractor
+                    .getAvailableData($scope.currentAction.id, 'NotSet')
+                    .then((data: any) => {
+                        var listItems: Array<model.DropDownListItem> = [];
+
+                        angular.forEach(<Array<any>>data.incomingFields, it => {
+                            var i, j;
+                            var found = false;
+                            for (i = 0; i < listItems.length; ++i) {
+                                if (listItems[i].key === it.key) {
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            if (!found) {
+                                listItems.push(<model.DropDownListItem>it);
+                            }
+                        });
+                        listItems.sort((x, y) => {
+                            if (x.key < y.key) {
+                                return -1;
+                            }
+                            else if (x.key > y.key) {
+                                return 1;
+                            }
+                            else {
+                                return 0;
+                            }
+                        });
+                        if (listItems.length === 0) {
+                            $scope.$emit(<any>upstreamFieldChooserEvents.NO_UPSTREAM_FIELDS, new AlertEventArgs());
+                        }
+                        else {
+                            $scope.field.listItems = listItems;
+                            $scope.tableParams = new NgTableParams({ count: 50 }, { data: $scope.field.listItems, counts: [], groupBy: 'label', groupOptions: { isExpanded: false } });
+                        }
+                    });
+            };
+        }
+    }
+
     //More detail on creating directives in TypeScript: 
     //http://blog.aaronholmes.net/writing-angularjs-directives-as-typescript-classes/
     class UpstreamFieldChooser implements ng.IDirective {
         public link: (scope: IUpstreamFieldChooser, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => void;
-        public controller: ($scope: IUpstreamFieldChooser, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, UpstreamExtractor: services.UpstreamExtractor, $modal: any, $timeout: any) => void;
+        public controller: any;
 
         public template = '<button class="btn btn-primary btn-xs" ng-click="openModal()">Select</button><span>{{field.value}}</span>';
         public restrict = 'E';
@@ -33,7 +163,7 @@ module dockyard.directives.upstreamDataChooser {
 
         private CrateHelper: services.CrateHelper;
 
-        constructor(CrateHelper: services.CrateHelper, NgTableParams) {
+        constructor(CrateHelper: services.CrateHelper) {
             this.CrateHelper = CrateHelper;
 
             UpstreamFieldChooser.prototype.link = (
@@ -41,150 +171,22 @@ module dockyard.directives.upstreamDataChooser {
                 element: ng.IAugmentedJQuery,
                 attrs: ng.IAttributes) => {
 
-            }
-
-            UpstreamFieldChooser.prototype.controller = (
-                $scope: IUpstreamFieldChooser,
-                $element: ng.IAugmentedJQuery,
-                $attrs: ng.IAttributes,
-                UpstreamExtractor: services.UpstreamExtractor,
-                $modal: any,
-                $timeout: any) => {
-                var modalInstance;
-                $scope.expand = () => {
-                    $timeout(function () {
-                        $(".modal-content").parent().css({
-
-                            'height': () => {return $(".modal-header.ng-scope").height()+40}
-                        })
-                    }, 0);
-                }
-                $scope.openModal = () => {
-                    getUpstreamFields();
-                    if ($scope.field.listItems.length !== 0) {
-                        modalInstance = $modal.open({
-                            animation: true,
-                            templateUrl: '/AngularTemplate/UpstreamFieldChooser',
-                            scope: $scope,
-                            controller: ['$scope','$modalInstance',function ($scope, $modalInstance) {
-                                $timeout(function () {
-                                    (<any>$(".modal-content"))
-                                        .wrap('<div align="center"></div>')
-                                        .css({
-                                            'overflow': 'hidden',
-                                            'max-height': () => { return $(document).height() - 80 }
-                                        })
-                                        .parent()
-                                        .css({
-                                            'min-width': () => {
-                                                var minWidth = parseInt($(".modal-content").attr("min-width"));
-                                                if (angular.isNumber(minWidth) && !isNaN(minWidth) && minWidth !== 0) {
-                                                    return minWidth;
-                                                }
-                                                var curWidth = $(".modal-content").width();
-                                                return curWidth > 0 ? curWidth : 300;
-                                            },
-                                            'width': () => {
-                                                var minWidth = parseInt($(".modal-content").attr("min-width"));
-                                                if (angular.isNumber(minWidth) && !isNaN(minWidth) && minWidth !== 0) {
-                                                    return minWidth;
-                                                }
-                                                var curWidth = $(".modal-content").width();
-                                                return curWidth > 0 ? curWidth : 300;
-                                            },
-                                            'min-height': () => { return $(".modal-header").height()},
-                                            'max-height': () => {
-                                                var curHeight = $(".modal-content").height();
-                                                if (curHeight > 85) {
-                                                    return curHeight;   
-                                                }                                                
-                                            }
-                                        })
-                                        .resizable().draggable()
-                                        .find($(".modal-content"))
-                                        .css({
-                                            overflow: 'auto',
-                                            width: '100%',
-                                            height: '100%'
-                                        });
-                                }, 0);
-                            }],
-                            resolve: {
-                                items: function () {
-                                    return $scope.field.listItems;
-                                }
-                            }
-                        });
-                        modalInstance.result.then(function (selectedItem) {
-                            $scope.field.value = selectedItem;
-                        })
-                    }
-                }
-                $scope.setItem = (item) => {
-                    $scope.selectedFieldValue = item;
-                    modalInstance.close($scope.selectedFieldValue);
-                    (<any>$(".modal-content")).parent().css({
-                        'min-height': 0 
-                    });
-                    if ($scope.change != null && angular.isFunction($scope.change)) {
-                        $scope.change()($scope.field);
-                    }
-                };
-
-                var getUpstreamFields = () => {
-                    UpstreamExtractor
-                        .getAvailableData($scope.currentAction.id, 'NotSet')
-                        .then((data: any) => {
-                            var listItems: Array<model.DropDownListItem> = [];
-
-                            angular.forEach(<Array<any>>data.incomingFields, it => {
-                                    var i, j;
-                                    var found = false;
-                                    for (i = 0; i < listItems.length; ++i) {
-                                        if (listItems[i].key === it.key) {
-                                            found = true;
-                                            break;
-                                        }
-                                    }
-                                    if (!found) {
-                                        listItems.push(<model.DropDownListItem>it);
-                                    }
-                            });
-                            listItems.sort((x, y) => {
-                                if (x.key < y.key) {
-                                    return -1;
-                                }
-                                else if (x.key > y.key) {
-                                    return 1;
-                                }
-                                else {
-                                    return 0;
-                                }
-                            });
-                            if (listItems.length === 0) {
-                                $scope.$emit(<any>upstreamFieldChooserEvents.NO_UPSTREAM_FIELDS, new AlertEventArgs());
-                            }
-                            else {
-                                $scope.field.listItems = listItems;
-                                $scope.tableParams = new NgTableParams({ count: 50 }, { data: $scope.field.listItems, counts: [], groupBy: 'label', groupOptions: { isExpanded: false } });   
-                            }
-                        });
-                };
-            }
-            UpstreamFieldChooser.prototype.controller['$inject'] = ['$scope', '$element', '$attrs', 'UpstreamExtractor', '$modal','$timeout'];
+            },
+            UpstreamFieldChooser.prototype.controller = UpstreamFieldChooserController;
         };
 
 
         //The factory function returns Directive object as per Angular requirements
         public static Factory() {
-            var directive = (CrateHelper: services.CrateHelper, NgTableParams) => {
-                return new UpstreamFieldChooser(CrateHelper, NgTableParams);
+            var directive = (CrateHelper: services.CrateHelper) => {
+                return new UpstreamFieldChooser(CrateHelper);
             };
 
-            directive['$inject'] = ['CrateHelper', 'NgTableParams'];
+            directive['$inject'] = ['CrateHelper'];
             return directive;
         }
     }
 
     app.directive('upstreamFieldChooser', UpstreamFieldChooser.Factory());
+    app.controller('UpstreamFieldChooserController', UpstreamFieldChooserController);
 } 
