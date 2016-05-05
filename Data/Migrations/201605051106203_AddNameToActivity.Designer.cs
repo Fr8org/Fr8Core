9c3f6e071ee6bf685f984b5c10a666b6dafe48cd
jarekrzdbk <jarekrzdbk@gmail.com>
2016-05-05 14:41:55 +0300

    Merged from 'dev'

--- Data/Migrations/201605051106203_AddNameToActivity.Designer.cs
+++ Data/Migrations/201605051106203_AddNameToActivity.Designer.cs
@@ -13,7 +13,7 @@ namespace Data.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201604212027550_AddNameToActivity"; }
+            get { return "201605051106203_AddNameToActivity"; }
         }
         
         string IMigrationMetadata.Source
