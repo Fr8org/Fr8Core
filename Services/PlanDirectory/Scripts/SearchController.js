73dea50e89f1b86351fec80a9be548b155f9b7f8
Yakov Gnusin <y.gnusin@gmail.com>
2016-05-12 18:50:05 +0600

    UI improvements.

--- Services/PlanDirectory/Scripts/SearchController.js
+++ Services/PlanDirectory/Scripts/SearchController.js
@@ -9,9 +9,11 @@
                 searchText: ''
             };
 
-            $scope.pageSize = 4;
+            $scope.pageSize = 5;
             $scope.totalCount = 0;
             $scope.planTemplates = [];
+            $scope.currentPage = 1;
+            $scope.pages = [];
 
             var doSearch = function (pageStart) {
                 var url = urlPrefix + '/api/plantemplates/search'
@@ -19,19 +21,29 @@
                     + '&pageStart=' + pageStart
                     + '&pageSize=' + $scope.pageSize;
 
+                Metronic.blockUI({ animate: true });
+
                 var promise = $q(function (resolve, reject) {
                     $http.post(url)
                         .then(function (res) {
                             $scope.totalCount = res.data.TotalCount;
                             $scope.planTemplates = res.data.PlanTemplates;
+
+                            $scope.currentPage = pageStart;
+                            $scope.pages = [];
+                            for (var i = 0; i < Math.ceil(res.data.TotalCount / $scope.pageSize) ; ++i) {
+                                $scope.pages.push(i + 1);
+                            }
+
+                            Metronic.unblockUI();
                         });
                 });
 
                 return promise;
             };
 
-            $scope.submitSearch = function () {
-                doSearch(1);
+            $scope.submitSearch = function (pageStart) {
+                doSearch(pageStart);
             };
 
             doSearch(1);
