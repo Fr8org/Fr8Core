d3804fcd51188e1fc929d50089919826da04f2ec
Andrey Chaplygin <demarsch@yandex.ru>
2016-05-16 01:20:55 +0200

    Added Query_DocuSign_v2 activity

---
+++ terminalDocuSign/Activities/EnhancedDocuSignActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Fr8Data.Manifests;
+using StructureMap;
+using terminalDocuSign.Services.New_Api;
+using TerminalBase.BaseClasses;
+
+namespace terminalDocuSign.Activities
+{
+    public abstract class EnhancedDocuSignActivity<TActivityUi> : EnhancedTerminalActivity<TActivityUi> where TActivityUi : StandardConfigurationControlsCM
+    {
+        protected readonly IDocuSignManager DocuSignManager;
+        //TODO: remove this constructor after introducing constructor injection
+        protected EnhancedDocuSignActivity() : this(ObjectFactory.GetInstance<IDocuSignManager>())
+        {
+        }
+
+        protected EnhancedDocuSignActivity(IDocuSignManager docuSignManager) : base(true)
+        {
+            if (docuSignManager == null)
+            {
+                throw new ArgumentNullException(nameof(docuSignManager));
+            }
+            DocuSignManager = docuSignManager;
+        } 
+
+        protected override bool IsTokenInvalidation(Exception ex)
+        {
+            return ex.Message.Contains("AUTHORIZATION_INVALID_TOKEN");
+        }
+    }
+}
--- terminalDocuSign/Controllers/TerminalController.cs
+++ terminalDocuSign/Controllers/TerminalController.cs
@@ -18,7 +18,7 @@ namespace terminalDocuSign.Controllers
         [ResponseType(typeof(StandardFr8TerminalCM))]
         public IHttpActionResult Get()
         {
-            var terminal = new TerminalDTO()
+            var terminal = new TerminalDTO
             {
                 Name = "terminalDocuSign",
                 Label = "DocuSign",
@@ -48,144 +48,145 @@ namespace terminalDocuSign.Controllers
             //};
 
             var waitForDocusignEventActionTemplate = new ActivityTemplateDTO()
-            {
-                Version = "1",
-                Name = "Monitor_DocuSign_Envelope_Activity",
-                Label = "Monitor DocuSign Envelope Activity",
-                Category = ActivityCategory.Monitors,
-                Terminal = terminal,
-                NeedsAuthentication = true,
-                MinPaneWidth = 380,
-                WebService = webService
+                {
+                    Version = "1",
+                    Name = "Monitor_DocuSign_Envelope_Activity",
+                    Label = "Monitor DocuSign Envelope Activity",
+                    Category = ActivityCategory.Monitors,
+                    Terminal = terminal,
+                    NeedsAuthentication = true,
+                    MinPaneWidth = 380,
+                    WebService = webService
+                },
+                new ActivityTemplateDTO
+                {
+                    Version = "1",
+                    Name = "Send_DocuSign_Envelope",
+                    Label = "Send DocuSign Envelope",
+                    Category = ActivityCategory.Forwarders,
+                    Tags = string.Join(",", Tags.EmailDeliverer),
+                    Terminal = terminal,
+                    NeedsAuthentication = true,
+                    WebService = webService,
+                    MinPaneWidth = 330,
+                },
+                new ActivityTemplateDTO
+                {
+                    Version = "1",
+                    Name = "Use_DocuSign_Template_With_New_Document",
+                    Label = "Use DocuSign Template With New Document",
+                    Category = ActivityCategory.Forwarders,
+                    Tags = Tags.EmailDeliverer,
+                    Terminal = terminal,
+                    NeedsAuthentication = true,
+                    WebService = webService,
+                    MinPaneWidth = 380
+                },
+                new ActivityTemplateDTO
+                {
+                    Version = "1",
+                    Name = "Get_DocuSign_Envelope",
+                    Label = "Get DocuSign Envelope",
+                    Category = ActivityCategory.Receivers,
+                    Terminal = terminal,
+                    NeedsAuthentication = true,
+                    WebService = webService,
+                    MinPaneWidth = 330
+                },
+                new ActivityTemplateDTO
+                {
+                    Version = "1",
+                    Name = "Get_DocuSign_Template",
+                    Label = "Get DocuSign Template",
+                    Category = ActivityCategory.Receivers,
+                    Terminal = terminal,
+                    NeedsAuthentication = true,
+                    WebService = webService,
+                    MinPaneWidth = 330
+                },
+                new ActivityTemplateDTO
+                {
+                    Name = "Prepare_DocuSign_Events_For_Storage",
+                    Label = "Prepare DocuSign Events For Storage",
+                    Version = "1",
+                    Category = ActivityCategory.Monitors,
+                    Terminal = terminal,
+                    NeedsAuthentication = true,
+                    WebService = webService,
+                    MinPaneWidth = 330,
+                    Tags = Tags.Internal
+                },
+                new ActivityTemplateDTO
+                {
+                    Name = "Mail_Merge_Into_DocuSign",
+                    Label = "Mail Merge Into DocuSign",
+                    Version = "1",
+                    NeedsAuthentication = true,
+                    Category = ActivityCategory.Solution,
+                    Terminal = terminal,
+                    WebService = webService,
+                    MinPaneWidth = 500,
+                    Tags = Tags.UsesReconfigureList
+                },
+                new ActivityTemplateDTO
+                {
+                    Name = "Extract_Data_From_Envelopes",
+                    Label = "Extract Data From Envelopes",
+                    Version = "1",
+                    Category = ActivityCategory.Solution,
+                    Terminal = terminal,
+                    WebService = webService,
+                    MinPaneWidth = 380,
+                    NeedsAuthentication = true
+                },
+                new ActivityTemplateDTO
+                {
+                    Name = "Track_DocuSign_Recipients",
+                    Label = "Track DocuSign Recipients",
+                    Version = "1",
+                    Category = ActivityCategory.Solution,
+                    NeedsAuthentication = true,
+                    Terminal = terminal,
+                    WebService = webService,
+                    MinPaneWidth = 380
+                },
+                new ActivityTemplateDTO
+                {
+                    Name = "Query_DocuSign",
+                    Label = "Query DocuSign",
+                    Version = "1",
+                    Category = ActivityCategory.Receivers,
+                    NeedsAuthentication = true,
+                    Terminal = terminal,
+                    WebService = webService,
+                    MinPaneWidth = 380
+                },
+                new ActivityTemplateDTO
+                {
+                    Name = "Query_DocuSign",
+                    Label = "Query DocuSign",
+                    Version = "2",
+                    Category = ActivityCategory.Receivers,
+                    NeedsAuthentication = true,
+                    Terminal = terminal,
+                    WebService = webService,
+                    MinPaneWidth = 380
+                },
+                new ActivityTemplateDTO
+                {
+                    Name = "Search_DocuSign_History",
+                    Label = "Search DocuSign History",
+                    Version = "1",
+                    Category = ActivityCategory.Receivers,
+                    NeedsAuthentication = true,
+                    Terminal = terminal,
+                    WebService = webService,
+                    MinPaneWidth = 380,
+                    Tags = Tags.Internal
+                }
             };
 
-            var sendDocuSignEnvelopeActionTemplate = new ActivityTemplateDTO()
-            {
-                Version = "1",
-                Name = "Send_DocuSign_Envelope",
-                Label = "Send DocuSign Envelope",
-                Category = ActivityCategory.Forwarders,
-                Tags = string.Join(",", Tags.EmailDeliverer),
-                Terminal = terminal,
-                NeedsAuthentication = true,
-                WebService = webService,
-                MinPaneWidth = 330,
-            };
-
-            var useDocuSignTemplateWithNewDocument = new ActivityTemplateDTO()
-            {
-                Version = "1",
-                Name = "Use_DocuSign_Template_With_New_Document",
-                Label = "Use DocuSign Template With New Document",
-                Category = ActivityCategory.Forwarders,
-                Tags = Tags.EmailDeliverer,
-                Terminal = terminal,
-                NeedsAuthentication = true,
-                WebService = webService,
-                MinPaneWidth = 380
-            };
-
-            var extractDataFromEnvelopeActionTemplate = new ActivityTemplateDTO()
-            {
-                Version = "1",
-                Name = "Get_DocuSign_Envelope",
-                Label = "Get DocuSign Envelope",
-                Category = ActivityCategory.Receivers,
-                Terminal = terminal,
-                NeedsAuthentication = true,
-                WebService = webService,
-                MinPaneWidth = 330
-            };
-
-            var getDocuSignTemplateActionTemplate = new ActivityTemplateDTO()
-            {
-                Version = "1",
-                Name = "Get_DocuSign_Template",
-                Label = "Get DocuSign Template",
-                Category = ActivityCategory.Receivers,
-                Terminal = terminal,
-                NeedsAuthentication = true,
-                WebService = webService,
-                MinPaneWidth = 330
-            };
-
-            var recordDocuSignEvents = new ActivityTemplateDTO
-            {
-                Name = "Prepare_DocuSign_Events_For_Storage",
-                Label = "Prepare DocuSign Events For Storage",
-                Version = "1",
-                Category = ActivityCategory.Monitors,
-                Terminal = terminal,
-                NeedsAuthentication = true,
-                WebService = webService,
-                MinPaneWidth = 330,
-                Tags = Tags.Internal
-            };
-
-            var mailMergeActionTemplate = new ActivityTemplateDTO
-            {
-                Name = "Mail_Merge_Into_DocuSign",
-                Label = "Mail Merge Into DocuSign",
-                Version = "1",
-                NeedsAuthentication = true,
-                Category = ActivityCategory.Solution,
-                Terminal = terminal,
-                WebService = webService,
-                MinPaneWidth = 500,
-                Tags = Tags.UsesReconfigureList
-            };
-
-            var collectFormDataSolution = new ActivityTemplateDTO
-            {
-                Name = "Extract_Data_From_Envelopes",
-                Label = "Extract Data From Envelopes",
-                Version = "1",
-                Category = ActivityCategory.Solution,
-                Terminal = terminal,
-                WebService = webService,
-                MinPaneWidth = 380,
-                NeedsAuthentication = true
-            };
-
-            var trackDocuSignRecipientsSolution = new ActivityTemplateDTO
-            {
-                Name = "Track_DocuSign_Recipients",
-                Label = "Track DocuSign Recipients",
-                Version = "1",
-                Category = ActivityCategory.Solution,
-                NeedsAuthentication = true,
-                Terminal = terminal,
-                WebService = webService,
-                MinPaneWidth = 380
-            };
-
-
-            var queryDocusign = new ActivityTemplateDTO
-            {
-                Name = "Query_DocuSign",
-                Label = "Query DocuSign",
-                Version = "1",
-                Category = ActivityCategory.Receivers,
-                NeedsAuthentication = true,
-                Terminal = terminal,
-                WebService = webService,
-                MinPaneWidth = 380
-            };
-
-            var searchDocusignHistory = new ActivityTemplateDTO
-            {
-                Name = "Search_DocuSign_History",
-                Label = "Search DocuSign History",
-                Version = "1",
-                Category = ActivityCategory.Receivers,
-                NeedsAuthentication = true,
-                Terminal = terminal,
-                WebService = webService,
-                MinPaneWidth = 380,
-                Tags = Tags.Internal
-            };
-
-            var actionList = new List<ActivityTemplateDTO>()
+            var curStandardFr8TerminalCM = new StandardFr8TerminalCM
             {
                 waitForDocusignEventActionTemplate,
                 extractDataFromEnvelopeActionTemplate,
