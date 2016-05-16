cd157df9a3bba25c82a1ec5528a990eb1be0a5bf
Andrey Chaplygin <demarsch@yandex.ru>
2016-05-16 22:42:39 +0200

    More fixes and tests for Query_DocuSign_v2

--- Tests/terminalDocuSignTests/Activities/BaseTest.cs
+++ Tests/terminalDocuSignTests/Activities/BaseTest.cs
@@ -8,6 +8,8 @@ using Hub.Managers;
 using Moq;
 using NUnit.Framework;
 using StructureMap;
+using terminalDocuSign.Interfaces;
+using terminalDocuSign.Services.New_Api;
 using TerminalBase.Infrastructure;
 using UtilitiesTesting.Fixtures;
 using TerminalBase.BaseClasses;
@@ -16,16 +18,25 @@ namespace terminalDocuSignTests.Activities
 {
     public class BaseTest : UtilitiesTesting.BaseTest
     {
+        protected readonly AuthorizationTokenDO FakeToken = new AuthorizationTokenDO { Token = "1" };
         [SetUp]
         public override void SetUp()
         {
             base.SetUp();
             var hubCommunicatorMock = new Mock<IHubCommunicator>();
-
             hubCommunicatorMock.Setup(x => x.GetPayload(It.IsAny<ActivityDO>(), It.IsAny<Guid>(), It.IsAny<string>()))
                                .Returns(Task.FromResult(FixtureData.PayloadDTO2()));
+            ObjectFactory.Configure(x => x.For<Mock<IHubCommunicator>>().Use(hubCommunicatorMock));
             ObjectFactory.Configure(x => x.For<IHubCommunicator>().Use(hubCommunicatorMock.Object));
-
+            var docuSignPlanMock = new Mock<IDocuSignPlan>();
+            ObjectFactory.Configure(x => x.For<Mock<IDocuSignPlan>>().Use(docuSignPlanMock));
+            ObjectFactory.Configure(x => x.For<IDocuSignPlan>().Use(docuSignPlanMock.Object));
+            var docuSignManagerMock = new Mock<IDocuSignManager>();
+            ObjectFactory.Configure(x => x.For<Mock<IDocuSignManager>>().Use(docuSignManagerMock));
+            ObjectFactory.Configure(x => x.For<IDocuSignManager>().Use(docuSignManagerMock.Object));
+            var docuSignFoldersMock = new Mock<IDocuSignFolders>();
+            ObjectFactory.Configure(x => x.For<Mock<IDocuSignFolders>>().Use(docuSignFoldersMock));
+            ObjectFactory.Configure(x => x.For<IDocuSignFolders>().Use(docuSignFoldersMock.Object));
         }
 
         protected async Task<ValidationResultsCM> Validate(BaseTerminalActivity activity, ActivityDO activityDo)
@@ -54,7 +65,7 @@ namespace terminalDocuSignTests.Activities
         {
             var errors = validationResults.GetErrorsForControl(controlName);
 
-            Assert.IsTrue(errors.Any(x=>x== errorMessage), "Error message is missing for control: " + controlName);
+            Assert.IsTrue(errors.Any(x => x == errorMessage), "Error message is missing for control: " + controlName);
         }
 
         protected void AssertErrorMessage(ValidationResultsCM validationResults, string errorMessage)
