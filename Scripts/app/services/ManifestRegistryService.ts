dcef719430fef41cc3d5d2878d8ccd0d0dade05e
Anton Yakovets <jokerosky@gmail.com>
2016-05-18 01:16:03 +0600

    FR-3400

--- Scripts/app/services/ManifestRegistryService.ts
+++ Scripts/app/services/ManifestRegistryService.ts
@@ -1,30 +1,30 @@
 ﻿
 module dockyard.services {
     export interface IManifestRegistryService extends ng.resource.IResourceClass<interfaces.IManifestRegistryVM> {
-        checkVersionAndName: (version: { version: string }, name: { name: string }) => any;
-        getDescriptionWithLastVersion: (name: { name: string }) => any;
+        checkVersionAndName: (data:{version: string , name: string} ) => any;
+        getDescriptionWithLastVersion: (data:{ name: string }) => any;
     }
 
     app.factory("ManifestRegistryService", ["$resource", ($resource: ng.resource.IResourceService): IManifestRegistryService =>
         <IManifestRegistryService>$resource("/api/manifestregistry?id=:id", { id: "@id" }, {
             checkVersionAndName: {
                 method: 'POST',
-                params: {
-                    version: "@version",
-                    name: "@name"
-                },
+                //params: {
+                //    version: "@version",
+                //    name: "@name"
+                //},
                 isArray: false,
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                headers: { 'Content-Type': 'application/json' },
                 url: "/api/manifest_registries/query"
                 //url: "/api/manifestregistry/checkVersionAndName"
             },
             getDescriptionWithLastVersion: {
                 method: 'POST',
-                params: {
-                    name: "@name"
-                },
+                //params: {
+                //    name: "@name"
+                //},
                 isArray: false,
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                headers: { 'Content-Type': 'application/json' },
                 url: "/api/manifest_registries/query"
                 //url: "/api/manifestregistry/getDescriptionWithLastVersion"
             }
---
+++ Scripts/tests/unit/services/ManifestRegistryServiceTests.ts
@@ -0,0 +1,40 @@
+﻿/// <reference path="../../../app/_all.ts" />
+/// <reference path="../../../typings/angularjs/angular-mocks.d.ts" />
+// <reference path="../../utils/endpoints.ts" />
+
+module dockyard.tests.controller {
+
+    import CrateHelper = dockyard.services.CrateHelper;
+    import fx = utils.fixtures;
+    import filterByTagFactory = dockyard.filters.filterByTag.factory;
+
+
+    describe('ManifestRegistryService', () => {
+
+        let mrSvc: dockyard.services.IManifestRegistryService;
+        let httpBackend: ng.IHttpBackendService;
+        let http: ng.IHttpService;
+        //let endpoints: Endpoints = new Endpoints();
+
+        beforeEach(module('app'));
+
+
+        beforeEach(() => {
+            inject((_ManifestRegistryService_, _$httpBackend_, _$http_) => {
+                mrSvc = _ManifestRegistryService_;
+                httpBackend = _$httpBackend_;
+                http = _$http_;
+            });
+        });
+
+        it('should call query endpoint',() => {
+            debugger;
+            let testVandName = mrSvc.checkVersionAndName({ version: 'v2.0', name: 'activity' });
+            let testVersion = mrSvc.getDescriptionWithLastVersion( {name:'activity' });
+
+            httpBackend.flush();
+
+        });
+
+    });
+}
