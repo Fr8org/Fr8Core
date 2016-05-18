6ad134a47533b99d8d3524ad5efbd8a975baf32e
Bahadır Bozdağ <bahadir.bb@gmail.com>
2016-05-18 11:13:16 +0300

    Completed refactoring terminalAtlassian

--- terminalAtlassian/Interfaces/IAtlassianService.cs
+++ terminalAtlassian/Interfaces/IAtlassianService.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
 using Fr8Data.DataTransferObjects;
+using TerminalBase.Models;
 
 namespace terminalAtlassian.Interfaces
 {
     public interface IAtlassianService
     {
         bool IsValidUser(CredentialsDTO curCredential);
-        List<FieldDTO> GetJiraIssue(string jiraKey, AuthorizationTokenDTO authorizationTokenDO);
+        List<FieldDTO> GetJiraIssue(string jiraKey, AuthorizationToken authorizationToken);
     }
 }
