ebcaa2a8b22a121dea5b6d9bbef089907fcd687f
makigjuro <maki.gjuroski@gmail.com>
2016-05-20 14:18:39 +0200

    fix

--- Tests/terminalDocuSignTests/Integration/MonitorAllDocuSignEventsLocal_Tests.cs
+++ Tests/terminalDocuSignTests/Integration/MonitorAllDocuSignEventsLocal_Tests.cs
@@ -69,7 +69,7 @@ namespace terminalDocuSignTests.Integration
 </DocuSignEnvelopeInformation>";
 
 
-        private const int MaxAwaitPeriod = 300000;
+        private const int MaxAwaitPeriod = 400000;
         private const int SingleAwaitPeriod = 10000;
         private const string DocuSignEmail = "fr8.madse.testing@gmail.com"; // "freight.testing@gmail.com";
         private const string DocuSignApiPassword = "I6HmXEbCxN";
