04a06e18427734ae761cb202b741597f254a5350
makigjuro <maki.gjuroski@gmail.com>
2016-05-23 14:41:23 +0200

    fix with migration

--- Data/Migrations/201605231150066_AddOrganizationAndAccountToObjectRolePermissions.cs
+++ Data/Migrations/201605231150066_AddOrganizationAndAccountToObjectRolePermissions.cs
@@ -9,6 +9,36 @@ namespace Data.Migrations
         {
             AddColumn("dbo.ObjectRolePermissions", "Fr8AccountId", c => c.String(nullable: false, maxLength: 128));
             AddColumn("dbo.ObjectRolePermissions", "OrganizationId", c => c.Int(nullable: true));
+
+            var sqlMigration = @"
+                declare @permissionSetId uniqueidentifier;
+                set @permissionSetId = newid();
+                
+                declare @counter int;
+                set @counter = (select count (*) from dbo._PermissionTypeTemplate)
+                if(@counter = 0) 
+                begin 
+	                insert into dbo._PermissionTypeTemplate(id) values(1)
+	                insert into dbo._PermissionTypeTemplate(id) values(2)
+	                insert into dbo._PermissionTypeTemplate(id) values(3)
+	                insert into dbo._PermissionTypeTemplate(id) values(4)
+	                insert into dbo._PermissionTypeTemplate(id) values(5)
+                end
+                insert into dbo.PermissionSets(id, Name, ObjectType, CreateDate, LastUpdated, HasFullAccess) values(@permissionSetId, 'PermissionForOwners', 'PlanNodeDO', '2016-05-12 09:16:34.7926131 +00:00','2016-05-12 09:16:34.7926131 +00:00', 0)
+                insert into dbo.PermissionSetPermissions(permissionSetId, PermissionTypeTemplateId) values(@permissionSetId, 1)
+                insert into dbo.PermissionSetPermissions(permissionSetId, PermissionTypeTemplateId) values(@permissionSetId, 2)
+                insert into dbo.PermissionSetPermissions(permissionSetId, PermissionTypeTemplateId) values(@permissionSetId, 3)
+                insert into dbo.PermissionSetPermissions(permissionSetId, PermissionTypeTemplateId) values(@permissionSetId, 4)
+                insert into dbo.PermissionSetPermissions(permissionSetId, PermissionTypeTemplateId) values(@permissionSetId, 5)
+                declare @rolePermissionSetId uniqueidentifier;
+                set @rolePermissionSetId = newid();
+                declare @roleId nvarchar(128)
+                set @roleId = (select top 1 id from dbo.AspNetRoles where Name = 'OwnerOfCurrentObject')
+                insert into dbo.RolePermissions(id, permissionSetId, roleId, CreateDate, LastUpdated) values (@rolePermissionSetId, @permissionSetId, @roleId, '2016-05-12 09:16:34.7926131 +00:00','2016-05-12 09:16:34.7926131 +00:00')
+                insert into dbo.ObjectRolePermissions(id, objectId, RolePermissionId, Type, Fr8AccountId, CreateDate, LastUpdated) 
+                select newid(),id,@rolePermissionSetId, 'PlanNodeDO',Fr8AccountId, '2016-05-12 09:16:34.7926131 +00:00','2016-05-12 09:16:34.7926131 +00:00'  from dbo.PlanNodes";
+
+            Sql(sqlMigration);
         }
 
         public override void Down()
