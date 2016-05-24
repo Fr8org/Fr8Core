2816b72315719dc42f76aaefb42bd6b54cd30e6c
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2016-05-24 19:37:03 +0600

    Refactor WebServicesController. Move activities get into Get method

--- Controllers/Api/WebServicesController.cs
+++ Controllers/Api/WebServicesController.cs
@@ -25,8 +25,64 @@ namespace HubWeb.Controllers
         }
 
         [HttpGet]
-        public IHttpActionResult Get()
+        public IHttpActionResult Get(int id = 0)
         {
+            if (id > 0)
+            {
+                var category = (ActivityCategory) id;
+                List<WebServiceActivitySetDTO> webServiceList;
+
+                using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
+                {
+                    // Getting web services and their actions as one set, then filtering that set
+                    // to get only those actions whose category matches any of categories provided
+                    // resulting set is grouped into batches 1 x web service - n x actions
+
+                    var unknwonService = uow.WebServiceRepository.GetQuery().FirstOrDefault(x => x.Name == UknownWebServiceName);
+                    Fr8Account fr8Account = new Fr8Account();
+
+                    var activityTemplate = _activityTemplate.GetQuery()
+                        .Where(x => x.ActivityTemplateState == ActivityTemplateState.Active)
+                        .Where(x => category == x.Category)
+                        .Where(x => x.Tags == null || !x.Tags.Contains(Tags.Internal));
+
+                    if (!fr8Account.IsCurrentUserInAdminRole())
+                        activityTemplate = activityTemplate.Where(x => x.ClientVisibility != false);
+
+                    webServiceList = activityTemplate
+                    .GroupBy(x => x.WebService, x => x, (key, group) => new
+                    {
+                        WebService = key,
+                        SortOrder = key == null ? 1 : 0,
+                        Actions = group
+                    }).OrderBy(x => x.SortOrder)
+                    .Select(x => new WebServiceActivitySetDTO
+                    {
+                        WebServiceIconPath = x.WebService != null ? x.WebService.IconPath : (unknwonService != null ? unknwonService.IconPath : null),
+                        WebServiceName = x.WebService != null ? x.WebService.Name : string.Empty,
+                        Activities = x.Actions
+                         .GroupBy(y => y.Name)
+                         .Select(y => y.OrderByDescending(z => int.Parse(z.Version)).First())
+                         .Select(p => new ActivityTemplateDTO
+                         {
+                             Id = p.Id,
+                             Name = p.Name,
+                             Category = p.Category,
+                             Label = p.Label,
+                             MinPaneWidth = p.MinPaneWidth,
+                             Version = p.Version,
+                             Type = p.Type,
+                             Tags = p.Tags,
+                             WebService = Mapper.Map<WebServiceDTO>(p.WebService),
+                             Terminal = Mapper.Map<TerminalDTO>(p.Terminal)
+                         }).ToList()
+                    }).ToList();
+                }
+
+                return Ok(webServiceList);
+            }
+
+            // If there is no category
             using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
             {
                 var models = uow.WebServiceRepository
@@ -52,62 +108,6 @@ namespace HubWeb.Controllers
             var model = Mapper.Map<WebServiceDTO>(entity);
 
             return Ok(model);
-        }
-
-        [HttpPost]
-        [ActionName("activities")]
-        public IHttpActionResult GetActivities(ActivityCategory[] categories)
-        {
-            List<WebServiceActivitySetDTO> webServiceList;
-
-            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
-            {
-                // Getting web services and their actions as one set, then filtering that set
-                // to get only those actions whose category matches any of categories provided
-                // resulting set is grouped into batches 1 x web service - n x actions
-
-                var unknwonService = uow.WebServiceRepository.GetQuery().FirstOrDefault(x => x.Name == UknownWebServiceName);
-                Fr8Account fr8Account = new Fr8Account();
-
-                var activityTemplate = _activityTemplate.GetQuery()
-                    .Where(x => x.ActivityTemplateState == ActivityTemplateState.Active)
-                    .Where(x => categories == null || categories.Contains(x.Category))
-                    .Where(x => x.Tags == null || !x.Tags.Contains(Tags.Internal));
-
-                if (!fr8Account.IsCurrentUserInAdminRole())
-                    activityTemplate = activityTemplate.Where(x => x.ClientVisibility != false);
-
-                webServiceList = activityTemplate
-                .GroupBy(x => x.WebService, x => x, (key, group) => new
-                {
-                    WebService = key,
-                    SortOrder = key == null ? 1 : 0,
-                    Actions = group
-                }).OrderBy(x => x.SortOrder)
-                .Select(x => new WebServiceActivitySetDTO
-                {
-                    WebServiceIconPath = x.WebService != null ? x.WebService.IconPath : (unknwonService != null ? unknwonService.IconPath : null),
-                    WebServiceName = x.WebService != null ? x.WebService.Name : string.Empty,
-                    Activities = x.Actions
-                     .GroupBy(y => y.Name)
-                     .Select(y => y.OrderByDescending(z => int.Parse(z.Version)).First())
-                     .Select(p => new ActivityTemplateDTO
-                     {
-                         Id = p.Id,
-                         Name = p.Name,
-                         Category = p.Category,
-                         Label = p.Label,
-                         MinPaneWidth = p.MinPaneWidth,
-                         Version = p.Version,
-                         Type = p.Type,
-                         Tags = p.Tags,
-                         WebService = Mapper.Map<WebServiceDTO>(p.WebService),
-                         Terminal = Mapper.Map<TerminalDTO>(p.Terminal)
-                     }).ToList()
-                }).ToList();
-            }
-
-            return Ok(webServiceList);
         }
     }
 }
