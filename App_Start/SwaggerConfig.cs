3ceb20c3d4a14fa62b0646195364ad682317fa67
jarekrzdbk <jarekrzdbk@gmail.com>
2016-05-25 21:14:48 +0300

    removed duplicated POST with query params

--- App_Start/SwaggerConfig.cs
+++ App_Start/SwaggerConfig.cs
@@ -237,7 +237,7 @@ namespace HubWeb
     {
         public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
         {
-            var duplicates1 = apiExplorer.ApiDescriptions.Where(x => x.RelativePath.ToLower().Contains("/" + x.HttpMethod.Method.ToLower() + "/")).ToList();
+            var duplicates1 = apiExplorer.ApiDescriptions.Where(x => x.RelativePath.ToLower().Contains("/" + x.HttpMethod.Method.ToLower() + "/") || x.RelativePath.ToLower().Contains("/" + x.HttpMethod.Method.ToLower() + "?")).ToList();
             var duplicates2 = apiExplorer.ApiDescriptions.Where(x => x.RelativePath.EndsWith(x.HttpMethod.Method, System.StringComparison.OrdinalIgnoreCase)).ToList();
             duplicates1.AddRange(duplicates2);
             foreach (var item in duplicates1)
