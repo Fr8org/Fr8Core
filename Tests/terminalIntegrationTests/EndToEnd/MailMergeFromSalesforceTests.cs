89a244ebb6e00469d92ebc540317d3efa1aee0d6
Bahadır Bozdağ <bahadir.bb@gmail.com>
2016-05-26 12:19:11 +0300

    Fixes

--- Tests/terminalIntegrationTests/EndToEnd/MailMergeFromSalesforceTests.cs
+++ Tests/terminalIntegrationTests/EndToEnd/MailMergeFromSalesforceTests.cs
@@ -3,12 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using terminalIntegrationTests.Fixtures;
 using terminalSalesforce.Services;
 using Data.Entities;
 using terminalSalesforce.Infrastructure;
 using HealthMonitor.Utility;
-using Hub.Managers;
 using terminalSalesforce.Actions;
 using terminaBaselTests.Tools.Terminals;
 using Data.States;
@@ -19,10 +19,10 @@ using Fr8Data.Constants;
 using Fr8Data.Control;
 using Fr8Data.Crates;
 using Fr8Data.DataTransferObjects;
-using Fr8Data.Manifests;
-using TerminalBase.Models;
 using Fr8Data.Managers;
-using AutoMapper;
+using Fr8Data.Manifests;
+using TerminalBase.Helpers;
+using TerminalBase.Models;
 
 namespace terminalIntegrationTests.EndToEnd
 {
@@ -30,14 +30,20 @@ namespace terminalIntegrationTests.EndToEnd
     public class MailMergeFromSalesforceTests : BaseHubIntegrationTest
     {
         private readonly IntegrationTestTools_terminalDocuSign _docuSignTestTools;
-        private CrateManager _crateManager = new CrateManager();
 
         public MailMergeFromSalesforceTests()
         {
             _docuSignTestTools = new IntegrationTestTools_terminalDocuSign(this);
+            Mapper.CreateMap<AuthorizationTokenDO, AuthorizationToken>();
         }
 
-        public override string TerminalName => "terminalSalesforce";
+        public override string TerminalName
+        {
+            get
+            {
+                return "terminalSalesforce";
+            }
+        }
 
         [Test, Category("Integration.terminalSalesforce")]
         public async Task MailMergeFromSalesforceEndToEnd()
@@ -45,8 +51,7 @@ namespace terminalIntegrationTests.EndToEnd
             await RevokeTokens("terminalDocuSign");
             var salesforceAuthToken = await HealthMonitor_FixtureData.CreateSalesforceAuthToken();
             //Create Case object in Salesforce
-            var authorizationToken = Mapper.Map<AuthorizationToken>(salesforceAuthToken);
-            var caseIdAndName = await CreateCase(authorizationToken);
+            var caseIdAndName = await CreateCase(salesforceAuthToken);
             PlanDTO plan = null;
             try
             {
@@ -54,43 +59,31 @@ namespace terminalIntegrationTests.EndToEnd
                 var solution = plan.Plan.SubPlans.First().Activities.Single();
                 await ApplyAuthTokenToSolution(solution, salesforceAuthToken);
                 //Initial configuration
-                solution = await ConfigureSolution(solution);
+                solution = await Configure(solution);
                 //Folowup configuration
-
-                using(var storage = _crateManager.GetUpdatableStorage(solution))
+                solution = solution.UpdateControls<Mail_Merge_From_Salesforce_v1.ActivityUi>(x =>
                 {
-                    storage.UpdateControls<Mail_Merge_From_Salesforce_v1.ActivityUi>(x =>
-                    {
-                        x.SalesforceObjectSelector.selectedKey = "Case";
-                        x.SalesforceObjectSelector.Value = "Case";
-                    });
-                }
+                    x.SalesforceObjectSelector.selectedKey = "Case";
+                    x.SalesforceObjectSelector.Value = "Case";
+                });
                 //This call will make solution to load specified Salesforce object properties and clear filter
-                solution = await ConfigureSolution(solution);
+                solution = await Configure(solution);
                 //This call will run generation of child activities
-                using (var storage = _crateManager.GetUpdatableStorage(solution))
+                solution = solution.UpdateControls<Mail_Merge_From_Salesforce_v1.ActivityUi>(x =>
                 {
-                    storage.UpdateControls<Mail_Merge_From_Salesforce_v1.ActivityUi>(x =>
-                    {
-                        x.SalesforceObjectFilter.Value = $"[{{\"field\":\"SuppliedName\",\"operator\":\"eq\",\"value\":\"{caseIdAndName.Item2}\"}}]";
-                        var sendDocuSignItem = x.MailSenderActivitySelector.ListItems.FirstOrDefault(y => y.Key == "Send DocuSign Envelope");
-                        Assert.IsNotNull(sendDocuSignItem, $"Send DocuSign Envelope activity is not marked with '{Tags.EmailDeliverer}' tag");
-                        x.MailSenderActivitySelector.selectedKey = sendDocuSignItem.Key;
-                        x.MailSenderActivitySelector.Value = sendDocuSignItem.Value;
-                        x.RunMailMergeButton.Clicked = true;
-                    });
-                }
-                solution = await ConfigureSolution(solution);
+                    x.SalesforceObjectFilter.Value = $"[{{\"field\":\"SuppliedName\",\"operator\":\"eq\",\"value\":\"{caseIdAndName.Item2}\"}}]";
+                    var sendDocuSignItem = x.MailSenderActivitySelector.ListItems.FirstOrDefault(y => y.Key == "Send DocuSign Envelope");
+                    Assert.IsNotNull(sendDocuSignItem, $"Send DocuSign Envelope activity is not marked with '{Tags.EmailDeliverer}' tag");
+                    x.MailSenderActivitySelector.selectedKey = sendDocuSignItem.Key;
+                    x.MailSenderActivitySelector.Value = sendDocuSignItem.Value;
+                    x.RunMailMergeButton.Clicked = true;
+                });
+                solution = await Configure(solution);
                 Assert.AreEqual(2, solution.ChildrenActivities.Length, "Child activities were not generated after mail merge was requested");
                 //Configure Send DocuSign Envelope activity to use proper upstream values
                 var docuSignActivity = solution.ChildrenActivities[1].ChildrenActivities[0];
-                var activityPayload = Mapper.Map<ActivityPayload>(docuSignActivity);
-                var activityContext = new ActivityContext
-                {
-                    ActivityPayload = activityPayload
-                };
-                var docusSignAuthAndConfig = await AuthorizeAndConfigureDocuSignActivity(activityContext);
-                var docuSignActivityPayload = docusSignAuthAndConfig.Item1;
+                var docusSignAuthAndConfig = await AuthorizeAndConfigureDocuSignActivity(docuSignActivity);
+                docuSignActivity = docusSignAuthAndConfig.Item1;
                 //Run plan
                 var container = await Run(plan);
                 Assert.AreEqual(State.Completed, container.State, "Container state is not equal to Completed");
@@ -103,7 +96,7 @@ namespace terminalIntegrationTests.EndToEnd
             }
             finally
             {
-                var caseWasDeleted = await DeleteCase(caseIdAndName.Item1, authorizationToken);
+                var caseWasDeleted = await DeleteCase(caseIdAndName.Item1, salesforceAuthToken);
                 Assert.IsTrue(caseWasDeleted, "Case created for test purposes failed to be deleted");
                 //if (plan != null)
                 //{
@@ -114,11 +107,8 @@ namespace terminalIntegrationTests.EndToEnd
 
         private void AssertEnvelopeContents(Guid docuSignTokenId, string expectedName)
         {
-            var authorizationToken = new AuthorizationToken
-            {
-                Token = _docuSignTestTools.GetDocuSignAuthToken(docuSignTokenId).Token
-            };
-            var configuration = new DocuSignManager().SetUp(authorizationToken);
+            var token = Mapper.Map<AuthorizationToken>(_docuSignTestTools.GetDocuSignAuthToken(docuSignTokenId));
+            var configuration = new DocuSignManager().SetUp(token);
             //find the envelope on the Docusign Account
             var folderItems = DocuSignFolders.GetFolderItems(configuration, new DocuSignQuery()
             {
@@ -136,52 +126,41 @@ namespace terminalIntegrationTests.EndToEnd
             Assert.IsNotNull(tabs, "Envelope does not contain any tabs. Check for problems in DocuSignManager and HandleTemplateData");
         }
 
-        private async Task<Tuple<ActivityPayload, Guid>> AuthorizeAndConfigureDocuSignActivity(ActivityContext docuSignActivity)
+        private async Task<Tuple<ActivityDTO, Guid>> AuthorizeAndConfigureDocuSignActivity(ActivityDTO docuSignActivity)
         {
-            var crateStorage = docuSignActivity.ActivityPayload.CrateStorage;
+            var crateStorage = Crate.GetStorage(docuSignActivity);
             var authenticationRequired = crateStorage.CratesOfType<StandardAuthenticationCM>().Any();
             var tokenId = Guid.Empty;
             if (authenticationRequired)
             {
                 // Authenticate with DocuSign
-                tokenId = await _docuSignTestTools.AuthenticateDocuSignAndAssociateTokenWithAction(docuSignActivity.ActivityPayload.Id, GetDocuSignCredentials(), docuSignActivity.ActivityPayload.ActivityTemplate.Terminal);
-                await Configure(docuSignActivity.ActivityPayload);
+                tokenId = await _docuSignTestTools.AuthenticateDocuSignAndAssociateTokenWithAction(docuSignActivity.Id, GetDocuSignCredentials(), docuSignActivity.ActivityTemplate.Terminal);
+                docuSignActivity = await Configure(docuSignActivity);
             }
-            var controlsDocusign = docuSignActivity.ActivityPayload.CrateStorage.FirstCrate<StandardConfigurationControlsCM>();
-            var templateSelector = controlsDocusign.Content.FindByName<DropDownList>("target_docusign_template");
-            templateSelector.selectedKey = "SendEnvelopeTestTemplate";
-            templateSelector.Value = "392f63c3-cabb-4b21-b331-52dabf1c2993";
-            
+            docuSignActivity.UpdateControls<Send_DocuSign_Envelope_v2.ActivityUi>(x => x.TemplateSelector.SelectByKey("SendEnvelopeTestTemplate"));
             //This configuration call will generate text source fields for selected template properties
-            await Configure(docuSignActivity.ActivityPayload);
-            var controls = docuSignActivity.ActivityPayload.CrateStorage.FirstCrate<StandardConfigurationControlsCM>();
-            var textSource = controls.Content.FindByName<TextSource>("RolesMappingTestSigner role email");
-            textSource.ValueSource = "upstream";
-            textSource.selectedKey = "SuppliedEmail";
-            textSource.Value = "SuppliedEmail";
+            docuSignActivity = await Configure(docuSignActivity);
+            docuSignActivity.UpdateControls<Send_DocuSign_Envelope_v2.ActivityUi>(x =>
+            {
+                var roleEmailControl = x.RolesFields.First(y => y.Name == "TestSigner role email");
+                roleEmailControl.ValueSource = TextSource.UpstreamValueSrouce;
+                roleEmailControl.selectedKey = "SuppliedEmail";
+                roleEmailControl.Value = "SuppliedEmail";
 
-            textSource = controls.Content.FindByName<TextSource>("RolesMappingTestSigner role name");
-            textSource.ValueSource = "upstream";
-            textSource.selectedKey = "SuppliedName";
-            textSource.Value = "SuppliedName";
-            return new Tuple<ActivityPayload, Guid>(await Save(docuSignActivity.ActivityPayload), tokenId);
+                var roleNameControl = x.RolesFields.First(y => y.Name == "TestSigner role name");
+                roleNameControl.ValueSource = TextSource.UpstreamValueSrouce;
+                roleNameControl.selectedKey = "SuppliedName";
+                roleNameControl.Value = "SuppliedName";
+            });
+            return new Tuple<ActivityDTO, Guid>(await Save(docuSignActivity), tokenId);
         }
 
-        private async Task<ActivityPayload> Save(ActivityPayload activityPayload)
+        private async Task<ActivityDTO> Save(ActivityDTO activity)
         {
-            var activityDTO = Mapper.Map<ActivityDTO>(activityPayload);
-            var result = await HttpPostAsync<ActivityDTO, ActivityDTO>($"{_baseUrl}activities/save", activityDTO);
-            return Mapper.Map<ActivityPayload>(result);
+            return await HttpPostAsync<ActivityDTO, ActivityDTO>($"{_baseUrl}activities/save", activity);
         }
 
-        private async Task<ActivityPayload> Configure(ActivityPayload activityPayload)
-        {
-            var activityDTO = Mapper.Map<ActivityDTO>(activityPayload);
-            var result = await HttpPostAsync<ActivityDTO, ActivityDTO>($"{_baseUrl}activities/configure?id={activityPayload.Id}", activityDTO);
-            return Mapper.Map<ActivityPayload>(result);
-        }
-
-        private async Task<ActivityDTO> ConfigureSolution(ActivityDTO activity)
+        private async Task<ActivityDTO> Configure(ActivityDTO activity)
         {
             return await HttpPostAsync<ActivityDTO, ActivityDTO>($"{_baseUrl}activities/configure?id={activity.Id}", activity);
         }
@@ -213,17 +192,19 @@ namespace terminalIntegrationTests.EndToEnd
             return await HttpPostAsync<string, PlanDTO>(solutionCreateUrl, null);
         }
 
-        private async Task<bool> DeleteCase(string caseId, AuthorizationToken authToken)
+        private async Task<bool> DeleteCase(string caseId, AuthorizationTokenDO authToken)
         {
-            return await new SalesforceManager().Delete(SalesforceObjectType.Case, caseId, authToken);
+            var token = Mapper.Map<AuthorizationToken>(authToken);
+            return await new SalesforceManager().Delete(SalesforceObjectType.Case, caseId, token);
         }
 
-        private async Task<Tuple<string, string>> CreateCase(AuthorizationToken authToken)
+        private async Task<Tuple<string, string>> CreateCase(AuthorizationTokenDO authToken)
         {
+            var token = Mapper.Map<AuthorizationToken>(authToken);
             var manager = new SalesforceManager();
             var name = Guid.NewGuid().ToString();
             var data = new Dictionary<string, object> { { "SuppliedEmail", TestEmail }, { "SuppliedName", name } };
-            return new Tuple<string, string>(await manager.Create(SalesforceObjectType.Case, data, authToken), name);
+            return new Tuple<string, string>(await manager.Create(SalesforceObjectType.Case, data, token), name);
         }
     }
 }
---
+++ terminalBase/Helpers/ActivityHelper.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Fr8Data.Crates;
+using Fr8Data.DataTransferObjects;
+using Fr8Data.Helpers;
+using Fr8Data.Managers;
+using Fr8Data.Manifests;
+using TerminalBase.BaseClasses;
+
+namespace TerminalBase.Helpers
+{
+    public static class ActivityHelper
+    {
+        /// <summary>
+        /// Lets you update activity UI control values without need to unpack and repack control crates
+        /// </summary>
+        public static ActivityDTO UpdateControls<TActivityUi>(this ActivityDTO activity, Action<TActivityUi> action) where TActivityUi : StandardConfigurationControlsCM, new()
+        {
+            return UpdateControls((object)activity, action) as ActivityDTO;
+        }
+
+        private static object UpdateControls<TActivityUi>(object activity, Action<TActivityUi> action) where TActivityUi : StandardConfigurationControlsCM, new()
+        {
+            if (activity == null)
+            {
+                throw new ArgumentNullException(nameof(activity));
+            }
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+            var activityDTO = activity as ActivityDTO;
+            var crateManager = new CrateManager();
+            using (var storage = crateManager.GetUpdatableStorage(activityDTO))
+            {
+                var controlsCrate = storage.FirstCrate<StandardConfigurationControlsCM>();
+                var activityUi = new TActivityUi().ClonePropertiesFrom(controlsCrate.Content) as TActivityUi;
+                activityUi.RestoreDynamicControlsFrom(controlsCrate.Content);
+                action(activityUi);
+                var newControls = new StandardConfigurationControlsCM(activityUi.Controls.ToArray());
+                storage.ReplaceByLabel(Crate.FromContent(controlsCrate.Label, newControls, controlsCrate.Availability));
+                activityUi.SaveDynamicControlsTo(newControls);
+
+            }
+            return activityDTO;
+        }
+
+        internal static string GetDynamicControlName(string controlName, string controlOwnerName)
+        {
+            return $"{controlOwnerName}_{controlName}";
+        }
+
+        private static readonly Tuple<string, string> EmptyDynamicControlNameAndOwner = new Tuple<string, string>(string.Empty, string.Empty);
+
+        internal static Tuple<string, string> GetDynamicControlNameAndOwner(string dynamicControlName)
+        {
+            if (string.IsNullOrWhiteSpace(dynamicControlName))
+            {
+                return EmptyDynamicControlNameAndOwner;
+            }
+            var delim = dynamicControlName.IndexOf('_');
+            if (delim <= 0)
+            {
+                return EmptyDynamicControlNameAndOwner;
+            }
+            return new Tuple<string, string>(dynamicControlName.Substring(delim + 1), dynamicControlName.Substring(0, delim));
+        }
+
+        internal static void RestoreDynamicControlsFrom<TActivityUi>(this TActivityUi source, StandardConfigurationControlsCM destination)
+        {
+            if (destination == null)
+            {
+                throw new ArgumentNullException(nameof(destination));
+            }
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            var dynamicControlsCollection = Fr8ReflectionHelper.GetMembers(source.GetType()).Where(x => x.CanRead
+                                                                                                        && x.GetCustomAttribute<DynamicControlsAttribute>() != null
+                                                                                                        && Fr8ReflectionHelper.CheckIfMemberIsCollectionOf<IControlDefinition>(x))
+                                                               .ToDictionary(x => x.Name, x => x);
+
+            if (dynamicControlsCollection.Count > 0)
+            {
+                foreach (var control in destination.Controls)
+                {
+                    var nameAndOwner = GetDynamicControlNameAndOwner(control.Name);
+                    if (string.IsNullOrEmpty(nameAndOwner.Item2))
+                    {
+                        continue;
+                    }
+                    IMemberAccessor member;
+
+                    if (!dynamicControlsCollection.TryGetValue(nameAndOwner.Item2, out member))
+                    {
+                        continue;
+                    }
+
+                    var controlsCollection = (IList)member.GetValue(source);
+
+                    if (controlsCollection == null && (!member.CanWrite || member.MemberType.IsAbstract || member.MemberType.IsInterface))
+                    {
+                        continue;
+                    }
+
+                    if (controlsCollection == null)
+                    {
+                        controlsCollection = (IList)Activator.CreateInstance(member.MemberType);
+                        member.SetValue(source, controlsCollection);
+                    }
+
+                    control.Name = nameAndOwner.Item1;
+                    controlsCollection.Add(control);
+                }
+            }
+        }
+
+        internal static void SaveDynamicControlsTo<TActivityUi>(this TActivityUi source, StandardConfigurationControlsCM destination)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            var insertIndex = 0;
+
+            foreach (var member in Fr8ReflectionHelper.GetMembers(source.GetType()).Where(x => x.CanRead))
+            {
+                if (member.GetCustomAttribute<DynamicControlsAttribute>() != null && Fr8ReflectionHelper.CheckIfMemberIsCollectionOf<IControlDefinition>(member))
+                {
+                    var collection = member.GetValue(source) as IList;
+
+                    if (collection != null)
+                    {
+                        foreach (var control in collection.Cast<object>().OfType<ControlDefinitionDTO>())
+                        {
+                            control.Name = GetDynamicControlName(control.Name, member.Name);
+                            destination.Controls.Insert(insertIndex, control);
+                            insertIndex++;
+                        }
+                    }
+                }
+
+                var controlDef = member.GetValue(source) as IControlDefinition;
+                if (!string.IsNullOrWhiteSpace(controlDef?.Name))
+                {
+                    for (int i = 0; i < destination.Controls.Count; i++)
+                    {
+                        if (destination.Controls[i].Name == controlDef.Name)
+                        {
+                            insertIndex = i + 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
