6e8f6c1ac224efe98660a748a2a897d1458f9954
Yakov Gnusin <y.gnusin@gmail.com>
2016-05-31 15:53:38 +0600

    Code cleanup.

--- Hub/Interfaces/IPlanTemplates.cs
+++ Hub/Interfaces/IPlanTemplates.cs
@@ -1,11 +1,6 @@
-﻿using Data.Entities;
-using Data.Interfaces.DataTransferObjects.PlanTemplates;
+﻿using System;
+using Data.Entities;
 using Fr8Data.DataTransferObjects.PlanTemplates;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hub.Interfaces
 {
@@ -13,14 +8,5 @@ namespace Hub.Interfaces
     {
         PlanTemplateDTO GetPlanTemplate(Guid planId, string fr8UserId);
         PlanDO LoadPlan(PlanTemplateDTO planTemplate, string fr8UserId);
-
-        // Commented out by yakov.gnusin,
-        // since we're using new approach described in https://maginot.atlassian.net/wiki/display/SH/Plan+Directory+Architecture
-
-        // PlanTemplateDTO SavePlan(Guid planId, string curFr8UserId);
-        // List<PlanTemplateDTO> GetTemplates(string userId);
-        // string LoadPlan(int planDescriptionId, string userId);
-        // PlanTemplateDTO GetTemplate(int planDescriptionId, string userId);
-        // void DeleteTemplate(int id, string userId);
     }
 }
