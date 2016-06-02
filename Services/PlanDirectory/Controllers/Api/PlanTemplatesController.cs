e771d954824b730abee39cf85c455395e2bee399
Yakov Gnusin <y.gnusin@gmail.com>
2016-06-02 18:48:02 +0600

    More exception logging.

--- Services/PlanDirectory/Controllers/Api/PlanTemplatesController.cs
+++ Services/PlanDirectory/Controllers/Api/PlanTemplatesController.cs
@@ -87,26 +87,43 @@ namespace PlanDirectory.Controllers
         [PlanDirectoryHMACAuthenticate]
         public async Task<IHttpActionResult> CreatePlan(Guid id)
         {
-            var fr8AccountId = User.Identity.GetUserId();
+            try
+            {
+                var fr8AccountId = User.Identity.GetUserId();
 
-            var hmacService = ObjectFactory.GetInstance<IHMACService>();
-            var client = ObjectFactory.GetInstance<IRestfulServiceClient>();
+                var hmacService = ObjectFactory.GetInstance<IHMACService>();
+                var client = ObjectFactory.GetInstance<IRestfulServiceClient>();
 
-            var planTemplateDTO = await _planTemplate.Get(fr8AccountId, id);
+                var planTemplateDTO = await _planTemplate.Get(fr8AccountId, id);
 
-            var uri = new Uri(CloudConfigurationManager.GetSetting("HubUrl") + "/api/v1/plans/load");
-            var headers = await hmacService.GenerateHMACHeader(
-                uri,
-                "PlanDirectory",
-                CloudConfigurationManager.GetSetting("PlanDirectorySecret"),
-                User.Identity.GetUserId(),
-                planTemplateDTO.PlanContents
-            );
+                var uri = new Uri(CloudConfigurationManager.GetSetting("HubUrl") + "/api/v1/plans/load");
+                var headers = await hmacService.GenerateHMACHeader(
+                    uri,
+                    "PlanDirectory",
+                    CloudConfigurationManager.GetSetting("PlanDirectorySecret"),
+                    User.Identity.GetUserId(),
+                    planTemplateDTO.PlanContents
+                );
 
-            var plan = await client.PostAsync<JToken, Fr8Data.DataTransferObjects.PlanEmptyDTO>(
-                uri, planTemplateDTO.PlanContents, headers: headers);
+                var plan = await client.PostAsync<JToken, Fr8Data.DataTransferObjects.PlanEmptyDTO>(
+                    uri, planTemplateDTO.PlanContents, headers: headers);
 
-            return Ok(new { RedirectUrl = CloudConfigurationManager.GetSetting("HubUrl") + "/dashboard/plans/" + plan.Id.ToString() + "/builder?viewMode=plan" });
+                return Ok(new { RedirectUrl = CloudConfigurationManager.GetSetting("HubUrl") + "/dashboard/plans/" + plan.Id.ToString() + "/builder?viewMode=plan" });
             }
+            catch (Exception ex)
+            {
+                var sb = new System.Text.StringBuilder();
+
+                while (ex != null)
+                {
+                    sb.AppendLine(ex.Message);
+                    sb.AppendLine(ex.StackTrace);
+
+                    ex = ex.InnerException;
+                }
+
+                return Ok(new { exception = sb.ToString() });
+            }
+        }
     }
 }
