9a97b0324cc50b58b0f0bae2adff447b40c21415
Andrey Chaplygin <demarsch@yandex.ru>
2016-06-02 13:09:17 +0200

    More work on Google form workflow

--- Scripts/app/controllers/ManifestRegistryListController.ts
+++ Scripts/app/controllers/ManifestRegistryListController.ts
@@ -7,6 +7,7 @@ module dockyard.controllers {
         manifestRegistry: Array<interfaces.IManifestRegistryVM>;
         showAddManifestDescriptionModal: () => void;
         showModalWithPopulatedValues: (manifestDescription: interfaces.IManifestRegistryVM) => void;
+        goToPrefilledManifestSubmissionForm: (manifestDescription: interfaces.IManifestRegistryVM) => void;
     }
 
     class ManifestRegistryListController {
@@ -26,8 +27,7 @@ module dockyard.controllers {
             private ManifestRegistryService: services.IManifestRegistryService,
             private $modal: any) {
 
-            $scope.showAddManifestDescriptionModal = <() => void> angular.bind(this, this.showAddManifestDescriptionModal);
-            $scope.showModalWithPopulatedValues = <(manifestDescription: interfaces.IManifestRegistryVM) => void> angular.bind(this, this.showModalWithPopulatedValues);
+            $scope.goToPrefilledManifestSubmissionForm = <(manifestDescription: interfaces.IManifestRegistryVM) => void>angular.bind(this, this.goToPrefilledManifestSubmissionForm);
 
             ManifestRegistryService.query().$promise.then(data => {
                     $scope.manifestRegistry = data;
@@ -35,44 +35,16 @@ module dockyard.controllers {
 
         }
 
-        private showAddManifestDescriptionModal() {
-            
-            this.$modal.open({
-                animation: true,
-                templateUrl: 'manifestDescriptionFormModal',
-                controller: 'ManifestRegistryFormController',
-                resolve: {
-                    descriptionName: function () {
-                        return undefined;
-                    }
-                }
-            })
-                .result.then(manifestDescription => {
-                    this.$scope.manifestRegistry.push(manifestDescription);
-                });
-        }
+        private goToPrefilledManifestSubmissionForm(manifestDescription: interfaces.IManifestRegistryVM) {
 
-        private showModalWithPopulatedValues(manifestDescription: interfaces.IManifestRegistryVM) {
-            var descriptionName = { value: manifestDescription.name };
-
-            this.$modal.open({
-                animation: true,
-                templateUrl: 'manifestDescriptionFormModal',
-                controller: 'ManifestRegistryFormController',
-                resolve: {
-                    descriptionName: function () {
-                        return descriptionName;
-                    }
-                }
-            })
-                .result.then(manifestDescription => {
-                    this.$scope.manifestRegistry.push(manifestDescription);
-                    this.ManifestRegistryService.query().$promise.then(data => {
-                        this.$scope.manifestRegistry = data;
-                    });
-                });
-        }
-
+            var url = "https://docs.google.com/forms/d/1Uc8-_qLmYl3dMs8v8fDlEjPbsO7HfZz6ugReNKq-XmY/viewform?entry.580225613=#ManifestType&entry.786138775=#Version&entry.560315139=#SampleJson&entry.255557102=#Description&entry.24056202=#RegBy"
+                .replace("#ManifestType", manifestDescription.name)
+                .replace("#Version", manifestDescription.version)
+                .replace("#SampleJson", manifestDescription.sampleJSON)
+                .replace("#Description", manifestDescription.description)
+                .replace("#RegBy", manifestDescription.registeredBy);
+            window.open(url);
+        };
     }
 
     app.controller('ManifestRegistryListController', ManifestRegistryListController);
