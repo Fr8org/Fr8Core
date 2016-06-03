e9961bf82d1184cd46fc2976baeb5a1861cd9957
Alp Gürtan <alpgurtan@gmail.com>
2016-06-03 10:19:28 +0300

    Unnecessary VMs are deleted

--- Tests/UtilitiesTesting/BaseTest.cs
+++ Tests/UtilitiesTesting/BaseTest.cs
@@ -150,34 +150,17 @@ namespace UtilitiesTesting
                 var identity = new ClaimsIdentity(claims);
                 controller.User = new GenericPrincipal(identity, userRoles);
             }
-
             return controller;
         }
 
         public static void ConfigureAutoMapper()
         {
-
-            Mapper.CreateMap<Fr8AccountDO, ManageUserVM>()
-                .ForMember(mu => mu.HasLocalPassword, opts => opts.ResolveUsing(account => !string.IsNullOrEmpty(account.PasswordHash)))
-                .ForMember(mu => mu.HasDocusignToken, opts => opts.Ignore())
-                .ForMember(mu => mu.HasGoogleToken, opts => opts.Ignore())
-                .ForMember(mu => mu.GoogleSpreadsheets, opts => opts.Ignore());
-
             Mapper.CreateMap<ActivityNameDTO, ActivityTemplateDO>()
                   .ForMember(activityTemplateDO => activityTemplateDO.Name, opts => opts.ResolveUsing(e => e.Name))
                   .ForMember(activityTemplateDO => activityTemplateDO.Version, opts => opts.ResolveUsing(e => e.Version));
 
             Mapper.CreateMap<PlanEmptyDTO, PlanDO>();
             Mapper.CreateMap<PlanDO, PlanEmptyDTO>();
-            Mapper.CreateMap<UserVM, EmailAddressDO>()
-                .ForMember(userDO => userDO.Address, opts => opts.ResolveUsing(e => e.EmailAddress));
-            Mapper.CreateMap<UserVM, Fr8AccountDO>()
-                .ForMember(userDO => userDO.Id, opts => opts.ResolveUsing(e => e.Id))
-                .ForMember(userDO => userDO.FirstName, opts => opts.ResolveUsing(e => e.FirstName))
-                .ForMember(userDO => userDO.LastName, opts => opts.ResolveUsing(e => e.LastName))
-                .ForMember(userDO => userDO.UserName, opts => opts.ResolveUsing(e => e.UserName))
-                .ForMember(userDO => userDO.EmailAddress, opts => opts.ResolveUsing(e => new EmailAddressDO { Address = e.EmailAddress }))
-                .ForMember(userDO => userDO.Roles, opts => opts.Ignore());
 
             Mapper.CreateMap<ActivityDO, ActivityDTO>();
 
