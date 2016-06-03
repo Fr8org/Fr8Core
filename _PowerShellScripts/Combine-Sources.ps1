96673adaaf6ad9ff962666830ee6fd4bda04ad2b
Alexei Avrutin <alexavrutin@gmail.com>
2016-06-04 01:23:16 +0200

    Fix

--- _PowerShellScripts/Combine-Sources.ps1
+++ _PowerShellScripts/Combine-Sources.ps1
@@ -9,10 +9,22 @@ param(
 	[Parameter(Mandatory = $true)]
 	[string]$github_password,
 
+    [string]$branchName = "dev",
+
 	[string]$sourcesDirectory = $env:BUILD_SOURCESDIRECTORY,
 
     [string]$tempDirectory = $env:BUILD_STAGINGDIRECTORY
 )
+
+function Checkout-Branch($branchName) {
+    # Checkout the specified branch 
+    Invoke-Expression "git checkout $branchName 2> $tempFileName"
+    if ($LastExitCode -ne 0)
+    {
+	    return $false;
+    }
+    return $true;
+}
 
 # $ErrorActionPreference = 'Stop'
 
@@ -37,15 +49,34 @@ ForEach ($currentRepo in $openSourceRepos) {
     	cd $curTemp
     }
 
-	# Fetch open source repo
-	$command = "git clone $currentRepoUrl $curTemp 2> $tempFileName"
-    Invoke-Expression  $command
+	# Prevent git operation from failing (see http://stackoverflow.com/questions/11693074/git-credential-cache-is-not-a-git-command)
+    Invoke-Expression  "git config --global credential.helper wincred"
+	if ($LastExitCode -ne 0)
+	{
+		Write-Host ("Unable to configure git. Let's try to proceed..." -f $currentRepo)
+	}
+
+
+	# Clone open source repo
+    Invoke-Expression  "git clone  $currentRepoUrl $curTemp 2> $tempFileName"
 
 	if ($LastExitCode -ne 0)
 	{
 		Write-Host ("Failed to clone the repository {0}." -f $currentRepo)
 		exit 1;
 	}
+
+    # Checkout the actual branch: try the specified branch and fallback to master 
+    if (-not(Checkout-Branch ($branchName))){
+        if (-not(Checkout-Branch ("master"))){
+	        Write-Host "Failed to checkout the branch $branchName or master, existing."
+            exit 1;
+        }  
+        else
+        {
+            Write-Host ("The branch {0} cannot be checked out, checked out master instead." -f $branchName)
+        }
+    }
 
     $curTerminal = [System.IO.Path]::Combine($sourcesDirectory, $currentRepo)
 
