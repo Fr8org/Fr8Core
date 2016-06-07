3521489b18ae3e8581d06f64479a1f9c2132b0aa
Anton Yakovets <jokerosky@gmail.com>
2016-06-07 18:39:37 +0600

    FR-3661 Migration for clean base and fix tests

---
+++ Data/Migrations/201606071223281_Delete FindObjects solution.cs
@@ -0,0 +1,57 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeleteFindObjectssolution : DbMigration
+    {
+        public override void Up()
+        {
+            var query = "DELETE FROM ActivityTemplate WHERE name LIKE 'FindObjects_Solution';";
+
+            Sql(query);
+        }
+        
+        public override void Down()
+        {
+            var query = @"INSERT INTO ActivityTemplate
+           ([Id]
+           ,[Name]
+           ,[Label]
+           ,[Tags]
+           ,[Version]
+           ,[Description]
+           ,[NeedsAuthentication]
+           ,[ComponentActivities]
+           ,[ActivityTemplateState]
+           ,[TerminalId]
+           ,[Category]
+           ,[Type]
+           ,[MinPaneWidth]
+           ,[WebServiceId]
+           ,[ClientVisibility]
+           ,[LastUpdated]
+           ,[CreateDate])
+     VALUES
+           ('2F4F50FF-13A0-492C-B0B3-4D0C8B4119F4'
+           ,'FindObjects_Solution'
+           ,'Find Objects Solution'
+           ,NULL
+           ,1
+           ,NULL
+           ,0
+           ,NULL
+           ,0
+           ,3
+           ,5
+           ,3
+           ,330
+           ,6
+           ,1
+           ,'0001-01-01 00:00:00.0000000 +00:00'
+           ,'0001-01-01 00:00:00.0000000 +00:00');";
+
+            Sql(query);
+        }
+    }
+}
