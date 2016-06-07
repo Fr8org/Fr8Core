8a625fcbdfcb915db06e2bccc8d76b5f1404c1d4
Alp Gürtan <alpgurtan@gmail.com>
2016-06-07 12:28:32 +0300

    Database name removed from migration

--- Data/Migrations/201606070712251_RemoveFr8CoreActivities.cs
+++ Data/Migrations/201606070712251_RemoveFr8CoreActivities.cs
@@ -8,12 +8,12 @@ namespace Data.Migrations
         public override void Up()
         {
             // Deleting Fr8 Core from Actions
-            const string SubSqlScript = "SELECT at.Id FROM [DockyardDB2].[dbo].[ActivityTemplate] AS at JOIN [DockyardDB2].[dbo].[WebServices] as ws ON at.WebServiceId = ws.Id WHERE ws.Name LIKE 'fr8 Core'";
-            Sql("DELETE FROM [DockyardDB2].[dbo].[Actions] WHERE ActivityTemplateId IN ( " + SubSqlScript + ")");
+            const string SubSqlScript = "SELECT at.Id FROM [dbo].[ActivityTemplate] AS at JOIN [dbo].[WebServices] as ws ON at.WebServiceId = ws.Id WHERE ws.Name LIKE 'fr8 Core'";
+            Sql("DELETE FROM [dbo].[Actions] WHERE ActivityTemplateId IN ( " + SubSqlScript + ")");
             // Deleting Fr8 Core from ActivityTemplate
-            Sql("DELETE FROM [DockyardDB2].[dbo].[ActivityTemplate] WHERE Id IN ( " + SubSqlScript + ")");
+            Sql("DELETE FROM [dbo].[ActivityTemplate] WHERE Id IN ( " + SubSqlScript + ")");
             // Deleting Fr8 Core from WebServices
-            Sql("DELETE FROM [DockyardDB2].[dbo].[WebServices] WHERE Name LIKE 'fr8 Core'");
+            Sql("DELETE FROM [dbo].[WebServices] WHERE Name LIKE 'fr8 Core'");
         }
         
         public override void Down()
