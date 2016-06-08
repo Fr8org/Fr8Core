2205034ffe832adbc34017d368aa74ee429dc4be
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2016-06-08 10:50:46 +0700

    Add PageDefinition service. #FR-3447

--- Hub/Interfaces/IPageDefinition.cs
+++ Hub/Interfaces/IPageDefinition.cs
@@ -1,9 +1,16 @@
-﻿using Data.Entities;
+﻿using System.Collections.Generic;
+using Data.Entities;
 
 namespace Hub.Interfaces
 {
     public interface IPageDefinition
     {
+        IEnumerable<PageDefinitionDO> GetAll();
+
+        PageDefinitionDO Get(int id);
+
         void CreateOrUpdate(PageDefinitionDO pageDefinitionDO);
+
+        void Delete(int id);
     }
 }
--- Hub/Services/PageDefinition.cs
+++ Hub/Services/PageDefinition.cs
@@ -3,10 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.Validation.Providers;
+using Data.Entities;
+using Data.Infrastructure;
+using Data.Interfaces;
+using Data.Repositories;
+using Hub.Interfaces;
+using StructureMap;
 
 namespace Hub.Services
 {
-    class PageDefinition
+    public class PageDefinition : IPageDefinition
     {
+        private const string TagsSeparator = "-";
+        private const string PageExtension = ".html";
+
+        public IEnumerable<PageDefinitionDO> GetAll()
+        {
+            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
+            {
+                return uow.PageDefinitionRepository.GetAll();
+            }
+        }
+
+        public PageDefinitionDO Get(int id)
+        {
+            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
+            {
+                return uow.PageDefinitionRepository.GetByKey(id);
+            }
+        }
+
+        public void CreateOrUpdate(PageDefinitionDO pageDefinitionDO)
+        {
+            pageDefinitionDO.PageName = GeneratePageNameFromTags(pageDefinitionDO);
+            if (pageDefinitionDO.Id > 0)
+            {
+
+                using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
+                {
+                    var pageDefinitionToUpdate = uow.PageDefinitionRepository.GetByKey(pageDefinitionDO.Id);
+                    pageDefinitionToUpdate = pageDefinitionDO;
+                    uow.SaveChanges();
+                }
+            }
+            else
+            {
+                using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
+                {
+                    uow.PageDefinitionRepository.Add(pageDefinitionDO);
+                    uow.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
+            {
+                var pageDefinitionToRemove = uow.PageDefinitionRepository.GetByKey(id);
+                uow.PageDefinitionRepository.Remove(pageDefinitionToRemove);
+            }
+        }
+
+        /// <summary>
+        /// Generates pageName from tags
+        /// </summary>
+        /// <param name="pageDefinitionDO"></param>
+        /// <returns></returns>
+        private static string GeneratePageNameFromTags(PageDefinitionDO pageDefinitionDO)
+        {
+            return string.Join(
+                TagsSeparator,
+                pageDefinitionDO.Tags.Select(x => x.ToLower()).OrderBy(x => x)) + PageExtension;
+        }
     }
 }
