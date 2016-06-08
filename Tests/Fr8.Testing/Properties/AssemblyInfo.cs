80705f46a676c639a8d347e03e837c8e6b20d16f
Alexei Avrutin <alexavrutin@gmail.com>
2016-06-09 00:07:31 +0200

    AssemblyInfo fix

--- Tests/Fr8.Testing/Properties/AssemblyInfo.cs
+++ Tests/Fr8.Testing/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Fr8.Testing")]
 [assembly: AssemblyCopyright("Copyright © The Fr8 Company 2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("en-US")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
