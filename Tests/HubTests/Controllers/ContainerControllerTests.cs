738686814811bb93716bf4bd930921840093f722
Yakov Gnusin <y.gnusin@gmail.com>
2016-06-09 15:47:58 +0600

    Test fix.

--- Tests/HubTests/Controllers/ContainerControllerTests.cs
+++ Tests/HubTests/Controllers/ContainerControllerTests.cs
@@ -85,7 +85,7 @@ namespace HubTests.Controllers
             //Act
             var containerController = CreateContainerController();
             Addcontainer();
-            Guid? id = FixtureData.TestContainer_Id_55();
+            Guid id = FixtureData.TestContainer_Id_55();
             var actionResult = containerController.Get(id);
             //Assert
             Assert.IsNull(actionResult as OkNegotiatedContentResult<ContainerDO>);
@@ -99,8 +99,7 @@ namespace HubTests.Controllers
             Addcontainer();
 
             //Act
-            Guid? id = null;
-            var actionResult = containerController.Get(id) as OkNegotiatedContentResult<IEnumerable<ContainerDTO>>;
+            var actionResult = containerController.Get() as OkNegotiatedContentResult<IEnumerable<ContainerDTO>>;
 
             ////Assert
             Assert.NotNull(actionResult);
@@ -115,7 +114,7 @@ namespace HubTests.Controllers
             Addcontainer();
 
             //Act
-            Guid? id = FixtureData.TestContainer_Id_1();
+            Guid id = FixtureData.TestContainer_Id_1();
             var actionResult = containerController.Get(id) as OkNegotiatedContentResult<ContainerDTO>;
 
             ////Assert
