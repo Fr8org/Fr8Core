10372666b4f9a8dafde01d1601bdabb9617480cb
Douglas Silvio Correa <me@douglascorrea.io>
2016-06-09 20:56:48 -0300

    FR-3722 testing if there is actions in the plan before reloading the first one

--- Scripts/app/controllers/PlanBuilderController.ts
+++ Scripts/app/controllers/PlanBuilderController.ts
@@ -181,11 +181,9 @@ module dockyard.controllers {
                 $mdOpenMenu(ev);
             };
             $scope.reConfigureAction = (action: model.ActivityDTO) => {
-                if (action) {
-                    var actionsArray = new Array<model.ActivityDTO>();
-                    actionsArray.push(action);
-                    this.reConfigure(actionsArray);
-                }
+                var actionsArray = new Array<model.ActivityDTO>();
+                actionsArray.push(action);
+                this.reConfigure(actionsArray);
             };
 
             $scope.openAddLabelModal = (action: model.ActivityDTO) => { 
@@ -458,7 +456,12 @@ module dockyard.controllers {
 
         private reloadFirstActions() {
             this.$timeout(() => {
-                this.$scope.current.plan.subPlans.forEach(plan => this.$scope.reConfigureAction(plan.activities[0]));
+                this.$scope.current.plan.subPlans.forEach(
+                    plan => {
+                        if (plan.activities.length > 0) {
+                            this.$scope.reConfigureAction(plan.activities[0])
+                        }
+                    });
             }, 1500);
         }
 
