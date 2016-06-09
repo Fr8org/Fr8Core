3eb0ffd979dc673941d3875a751a1118ad7978cf
Yakov Gnusin <y.gnusin@gmail.com>
2016-06-09 15:30:10 +0600

    Test endpoints.

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -18,11 +18,6 @@ namespace HubWeb
             RegisterAuthenticationEndPoints(config);
 
             config.Routes.MapHttpRoute(
-                name: "DefaultApiWithAction",
-                routeTemplate: "api/v1/{controller}/{action}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-                );
-            config.Routes.MapHttpRoute(
                 name: "DefaultApiGet",
                 routeTemplate: "api/v1/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional, action = "Get" },
@@ -46,6 +41,12 @@ namespace HubWeb
                 defaults: new { id = RouteParameter.Optional, action = "Delete" },
                 constraints: new { httpMethod = new HttpMethodConstraint(HttpMethod.Delete) }
                 );
+            config.Routes.MapHttpRoute(
+                name: "DefaultApiWithAction",
+                routeTemplate: "api/v1/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+
             //config.Routes.MapHttpRoute(
             //    name: "DefaultApi",
             //    routeTemplate: "api/v1/{controller}/{id}",
