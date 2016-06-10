2f4600d51354da8fd1bb9f6e24571f4b02281874
cpretzer <charles@fr8.co>
2016-06-10 11:22:01 -0700

    * FR-3747: fix for local VS Application Insights logging

--- Global.asax.cs
+++ Global.asax.cs
@@ -25,6 +25,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Segment;
 using StructureMap;
+using Microsoft.ApplicationInsights.Extensibility;
 using Logger = Fr8.Infrastructure.Utilities.Logging.Logger;
 
 namespace HubWeb
@@ -137,6 +138,7 @@ namespace HubWeb
 
 #if DEBUG
             SetServerUrl(HttpContext.Current);
+            TelemetryConfiguration.Active.DisableTelemetry = true;
 #endif
             NormalizeUrl();
             RewriteAngularRequests();
