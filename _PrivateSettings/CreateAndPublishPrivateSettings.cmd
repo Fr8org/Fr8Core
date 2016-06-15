2d4410552ddc602b6f05323351edd359ad7abc57
Alexei Avrutin <alexavrutin@gmail.com>
2016-06-15 05:36:19 +0200

    _PrivateSettings moved to the fr8config repo

--- _PrivateSettings/CreateAndPublishPrivateSettings.cmd
+++
@@ -1,6 +0,0 @@
-set /P version=Enter package version:
-
-%~dp0..\.nuget\NuGet.exe pack %~dp0Fr8.PrivateSettings.nuspec -OutputDirectory %~dp0 -Version %version%
-%~dp0Tools\NuGet.exe push %~dp0Fr8.PrivateSettings.%version%.nupkg -Source https://fr8.pkgs.visualstudio.com/DefaultCollection/_packaging/fr8-private/nuget/v3/index.json -ApiKey VST
-
-set /P version=Press Enter to close
