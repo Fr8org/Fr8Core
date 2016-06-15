117f3679ae724089d1dca23140f9419659b65981
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2016-06-15 17:23:57 +0700

    Fix after merge

--- Data/Migrations/201606151014116_AddPageDefinition.Designer.cs
+++ Data/Migrations/201606151014116_AddPageDefinition.Designer.cs
@@ -13,7 +13,7 @@ namespace Data.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201606101439338_AddPageDefinition"; }
+            get { return "201606151014116_AddPageDefinition"; }
         }
         
         string IMigrationMetadata.Source
