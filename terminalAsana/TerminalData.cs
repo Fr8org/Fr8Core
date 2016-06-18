6889d9b1dda0f1b4d1c8ff53cb65a9272a2ed5b8
tony.yakovets <jokerosky@gmail.com>
2016-06-18 17:25:12 +0600

    FR-3807 Add terminal project

---
+++ terminalAsana/TerminalData.cs
@@ -0,0 +1,24 @@
+﻿using Fr8.Infrastructure.Data.DataTransferObjects;
+using Fr8.Infrastructure.Data.States;
+using Fr8.Infrastructure.Utilities.Configuration;
+
+namespace terminalDemo
+{
+    public static class TerminalData
+    {
+        public static WebServiceDTO WebServiceDTO = new WebServiceDTO
+        {
+            Name = "Asana Activities",
+            IconPath = "/Content/icons/web_services/fr8-demo-icon-64x64.png"
+        };
+
+        public static TerminalDTO TerminalDTO = new TerminalDTO
+        {
+            Endpoint = CloudConfigurationManager.GetSetting("terminalAsana.TerminalEndpoint"),
+            TerminalStatus = TerminalStatus.Active,
+            Name = "terminalAsana",
+            Label = "Asana terminal",
+            Version = "1"
+        };
+    }
+}
