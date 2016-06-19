2fbfee5e6f42d70e043f0ef1b83e4517ea3fc831
blazingmind <blaizingmind@gmail.com>
2016-06-19 05:49:06 +0300

    fixed run db migration script

--- _PowerShellScripts/Run-DBMigration.ps1
+++ _PowerShellScripts/Run-DBMigration.ps1
@@ -2,14 +2,14 @@ param(
 	[Parameter(Mandatory = $true)]
 	[string]$migrationToolPath,
 	
-	[Parameter(Mandatory = $true)]
-	[string]$buildResultDirectory,
+	[Parameter(Mandatory = $false)]
+	[string]$buildResultDirectory
 )
 	
 
 $RootDir = Split-Path -parent $PSCommandPath
 
-$runMigrationCmd = "`"$RootDir\..\$migrationToolPath`" Data.dll /startupDirectory=`"$RootDir\..\$buildResultDirectory\bin`"  /startupConfigurationFile=`"$RootDir\..\$buildResultDirectory\web.config`"";
+$runMigrationCmd = "$RootDir\..\$migrationToolPath Data.dll /startupDirectory=`"$RootDir\..\$buildResultDirectory\bin`"  /startupConfigurationFile=`"$RootDir\..\$buildResultDirectory\web.config`"";
 
 Write-Host $runMigrationCmd
 
