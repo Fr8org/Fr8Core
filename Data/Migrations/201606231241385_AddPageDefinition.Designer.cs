3d6df1f96b028a9302f79bfd4335f38e73dba220
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2016-06-23 19:50:03 +0700

    #FR-3447. Fix build

--- Data/Migrations/201606231241385_AddPageDefinition.Designer.cs
+++ Data/Migrations/201606231241385_AddPageDefinition.Designer.cs
@@ -13,7 +13,7 @@ namespace Data.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201606151014116_AddPageDefinition"; }
+            get { return "201606231241385_AddPageDefinition"; }
         }
         
         string IMigrationMetadata.Source
