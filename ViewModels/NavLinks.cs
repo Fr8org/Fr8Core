e0c621df568bdfd36eec7ad8e5678001babd056a
Alp Gürtan <alpgurtan@gmail.com>
2016-06-24 18:45:50 +0300

    Navigation links ar combined and imrpoved with starting to use Web configs

--- ViewModels/NavLinks.cs
+++ ViewModels/NavLinks.cs
@@ -1,12 +1,12 @@
-using Fr8.Infrastructure.Utilities.Configuration;
+using System.Configuration;
 
 namespace HubWeb.ViewModels
 {
-    public class NavLinks
+    public static class NavLinks
     {
-        public string BaseUrl = CloudConfigurationManager.GetSetting("BaseUrl");
-        public string Blog = "http://blog.fr8.co";
-        public string Developers = "https://github.com/Fr8org/Fr8Core/blob/master/Docs/Home.md";
-        public string PlanDiscovery = "http://discovery.fr8.co/";
+        public static string BaseUrl = ConfigurationManager.AppSettings["ServerUrl"];
+        public static string Blog = "http://blog.fr8.co";
+        public static string Developers = "https://github.com/Fr8org/Fr8Core/blob/master/Docs/Home.md";
+        public static string PlanDiscovery = ConfigurationManager.AppSettings["PlanDirectoryUrl"];
     }
 }
