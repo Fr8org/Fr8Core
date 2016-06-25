280c1539849f1907b8e6926b07a3212454df0b73
teh_netaholic <teh.netaholic@gmail.com>
2016-06-25 16:06:43 +0300

    fixed an activity discovery test

--- Tests/terminalGoogleTests/Integration/Terminal_Discover_v1_Tests.cs
+++ Tests/terminalGoogleTests/Integration/Terminal_Discover_v1_Tests.cs
@@ -30,16 +30,18 @@ namespace terminalGoogleTests.Integration
 
             Assert.IsNotNull(googleTerminalDiscoveryResponse, "Terminal Google discovery did not happen.");
             Assert.IsNotNull(googleTerminalDiscoveryResponse.Activities, "Google terminal does not have actions.");
-            Assert.AreEqual(3, googleTerminalDiscoveryResponse.Activities.Count, "Google terminal expected 3 actions.");
+            Assert.AreEqual(5, googleTerminalDiscoveryResponse.Activities.Count, "Google terminal expected 4 actions.");
             Assert.AreEqual("terminalGoogle", googleTerminalDiscoveryResponse.Definition.Name);
             Assert.AreEqual("Google", googleTerminalDiscoveryResponse.Definition.Label);
             Assert.AreEqual(googleTerminalDiscoveryResponse.Activities.Any(a => a.Name == "Get_Google_Sheet_Data"), true, "Action Get_Google_Sheet_Data was not loaded");
             Assert.AreEqual(googleTerminalDiscoveryResponse.Activities.Any(a => a.Name == "Monitor_Form_Responses"), true, "Action Monitor_Form_Responses was not loaded");
             Assert.AreEqual(googleTerminalDiscoveryResponse.Activities.Any(a => a.Name == "Save_To_Google_Sheet"), true, "Action Save_To_Google_Sheet was not loaded");
+            Assert.AreEqual(googleTerminalDiscoveryResponse.Activities.Any(a => a.Name == "Monitor_Gmail_Inbox"), true, "Action Monitor_Gmail_Inbox was not loaded");
             //Check Activities Categories
             Assert.AreEqual(googleTerminalDiscoveryResponse.Activities.Single(a => a.Name == "Get_Google_Sheet_Data").Category, ActivityCategory.Receivers, "Activity Get_Google_Sheet_Data is not of Category Receivers");
             Assert.AreEqual(googleTerminalDiscoveryResponse.Activities.Single(a => a.Name == "Monitor_Form_Responses").Category, ActivityCategory.Monitors, "Activity Monitor_Form_Responses is not of Category Monitors");
             Assert.AreEqual(googleTerminalDiscoveryResponse.Activities.Single(a => a.Name == "Save_To_Google_Sheet").Category, ActivityCategory.Forwarders, "Activity Save_To_Google_Sheet is not of Category Forwarders");
+            Assert.AreEqual(googleTerminalDiscoveryResponse.Activities.Single(a => a.Name == "Monitor_Gmail_Inbox").Category, ActivityCategory.Monitors, "Activity Monitor_Gmail_Inbox is not of Category Monitors");
         }
     }
 }
