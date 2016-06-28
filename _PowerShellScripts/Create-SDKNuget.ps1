425a9b83e65e469884523824a8a89310251fb13a
Alexei Avrutin <alexavrutin@gmail.com>
2016-06-28 13:34:24 +0200

    Merge dev

--- _PowerShellScripts/Create-SDKNuget.ps1
+++ _PowerShellScripts/Create-SDKNuget.ps1
@@ -10,4 +10,4 @@ $packageVersion = "$env:BUILD_BUILDNUMBER" -replace "Build ", ""
 Write-Host "Package version: $packageVersion"
 $nuget = "{0}\agent\worker\tools\NuGet.exe pack {1}\{2} -OutputDirectory {1}\{3} -Properties Configuration=dev -IncludeReferencedProjects  -Version {4}" -f $env:AGENT_HOMEDIRECTORY, $env:BUILD_REPOSITORY_LOCALPATH, "Fr8TerminalBase.NET\Fr8TerminalSDK.Dev.nuspec", "Fr8TerminalBase.NET", $packageVersion
 Write-Host "Running Nuget packager: $nuget"
-Invoke-Expression $nuget 
+Invoke-Expression $nuget
