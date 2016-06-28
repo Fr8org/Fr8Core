b4777d13ef8f9a87bdf0dafcbe9a276f5717f1c2
Alexei Avrutin <alexavrutin@gmail.com>
2016-06-28 14:47:02 +0200

    SDK Nuget name and versioning update

--- _PowerShellScripts/Create-SDKNuget.ps1
+++ _PowerShellScripts/Create-SDKNuget.ps1
@@ -6,7 +6,7 @@
 #>
 $ErrorActionPreference = 'Stop'
 
-$packageVersion = "$env:BUILD_BUILDNUMBER" -replace "Build ", ""
+$packageVersion = "1.0.0.$env:BUILD_BUILDID"
 Write-Host "Package version: $packageVersion"
 $nuget = "{0}\agent\worker\tools\NuGet.exe pack {1}\{2} -OutputDirectory {1}\{3} -Properties Configuration=dev -IncludeReferencedProjects  -Version {4}" -f $env:AGENT_HOMEDIRECTORY, $env:BUILD_REPOSITORY_LOCALPATH, "Fr8TerminalBase.NET\Fr8TerminalSDK.Dev.nuspec", "Fr8TerminalBase.NET", $packageVersion
 Write-Host "Running Nuget packager: $nuget"
