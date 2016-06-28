1973ccad8e2f6a192f1d28d9dc0b31503986f602
jarekrzdbk <jarekrzdbk@gmail.com>
2016-06-28 17:14:00 +0300

    added userId

--- terminalAtlassian/Activities/Save_Jira_Issue_v1.cs
+++ terminalAtlassian/Activities/Save_Jira_Issue_v1.cs
@@ -391,7 +391,7 @@ namespace terminalAtlassian.Actions
 
             Payload.Add(Crate.FromContent("jira issue", new StandardPayloadDataCM(new KeyValueDTO() { Key = "jira issue key", Value = issueInfo.Key })));
             Payload.Add(Crate.FromContent("jira issue", new StandardPayloadDataCM(new KeyValueDTO() { Key = "jira domain", Value = credentialsDTO.Domain })));
-            await _pushNotificationService.PushUserNotification(MyTemplate, "Success", "Jira issue created", $"Created new jira issue: {jiraUrl}");
+            await _pushNotificationService.PushUserNotification(MyTemplate, "Success", "Jira issue created", $"Created new jira issue: {jiraUrl}",CurrentUserId);
             Payload.Add(Crate<KeyValueListCM>.FromContent(RuntimeCrateLabel, new KeyValueListCM(
                                                                                       new KeyValueDTO(JiraIdField, issueInfo.Key),
                                                                                       new KeyValueDTO(JiraUrlField, jiraUrl))));
