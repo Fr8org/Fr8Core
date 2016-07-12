932ed5f18f9dab4728a438f1e8795b1a1086e0fc
jarekrzdbk <jarekrzdbk@gmail.com>
2016-07-12 20:59:51 +0300

    Deleting old logs

---
+++ Data/Migrations/201607121755519_DeleteOldLogs.cs
@@ -0,0 +1,17 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeleteOldLogs : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("DELETE FROM [DockyardDB2].[dbo].[History]  WHERE [CreateDate] < DATEADD(day, -14, GETDATE())");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
