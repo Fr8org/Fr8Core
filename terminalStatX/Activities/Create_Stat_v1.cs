3f454b878f3b41cdde2620587421d1e73f8432d5
makigjuro <maki.gjuroski@gmail.com>
2016-07-14 00:17:18 +0200

    added create stat activity

---
+++ terminalStatX/Activities/Create_Stat_v1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Fr8.Infrastructure.Data.Control;
+using Fr8.Infrastructure.Data.DataTransferObjects;
+using Fr8.Infrastructure.Data.Managers;
+using Fr8.Infrastructure.Data.Manifests;
+using Fr8.Infrastructure.Data.States;
+using Fr8.TerminalBase.BaseClasses;
+using Fr8.TerminalBase.Infrastructure;
+
+namespace terminalStatX.Activities
+{
+    public class Create_Stat_v1 : TerminalActivity<Create_Stat_v1.ActivityUi>
+    {
+        public static ActivityTemplateDTO ActivityTemplateDTO = new ActivityTemplateDTO
+        {
+            Name = "Update_Stat",
+            Label = "Update Stat",
+            Version = "1",
+            Category = ActivityCategory.Forwarders,
+            Terminal = TerminalData.TerminalDTO,
+            NeedsAuthentication = true,
+            MinPaneWidth = 300,
+            WebService = TerminalData.WebServiceDTO,
+            Categories = new[]
+            {
+                ActivityCategories.Forward,
+                new ActivityCategoryDTO(TerminalData.WebServiceDTO.Name, TerminalData.WebServiceDTO.IconPath)
+            }
+        };
+
+        protected override ActivityTemplateDTO MyTemplate => ActivityTemplateDTO;
+
+        public override Task Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task FollowUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class ActivityUi : StandardConfigurationControlsCM
+        {
+            public ActivityUi()
+            {
+            }
+
+            public void ClearDynamicFields()
+            {
+            }
+        }
+
+        public Create_Stat_v1(ICrateManager crateManager) : base(crateManager)
+        {
+        }
+    }
+}
