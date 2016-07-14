7fb7056ed3d3490926801b687190e3d60740a83b
Alexei Avrutin <alexavrutin@gmail.com>
2016-07-14 17:35:41 +0200

    The IsFr8OwnTerminal property added (and DB migration)

---
+++ Data/Migrations/201607141445182_IsFr8OwnTerminal_Property.cs
@@ -0,0 +1,18 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IsFr8OwnTerminal_Property : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.TerminalRegistration", "IsFr8OwnTerminal", c => c.Boolean(nullable: false, defaultValue: true));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.TerminalRegistration", "IsFr8OwnTerminal");
+        }
+    }
+}
