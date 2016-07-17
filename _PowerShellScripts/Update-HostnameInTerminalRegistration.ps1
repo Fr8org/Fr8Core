dc973273b9d6e9ac92f4d9afb15b19d8702fc730
Alexei Avrutin <alexavrutin@gmail.com>
2016-07-17 06:42:45 +0200

    Added for Localhost argument to Update-HostnameInTerminalRegistration.ps1

--- _PowerShellScripts/Update-HostnameInTerminalRegistration.ps1
+++ _PowerShellScripts/Update-HostnameInTerminalRegistration.ps1
@@ -6,7 +6,10 @@
 	[string]$newHostname,
 
 	[Parameter(Mandatory = $false)]
-	[string]$overrideDbName
+	[string]$overrideDbName,
+
+	[Parameter(Mandatory = $false)]
+	[string]$localhostOnly = $false
 )
 
 Write-Host "Update terminal URLs to $newHostname"
@@ -15,8 +18,12 @@ $commandText = "
 	-- Update hostname only if port value is present in endpoint URL and terminal belongs to Fr8
     UPDATE TerminalRegistration SET [Endpoint] = 
 			('$newHostname' + RIGHT ([Endpoint], CHARINDEX (':', REVERSE ([Endpoint]))))
-	WHERE CHARINDEX (':', REVERSE ([Endpoint])) <= 6 AND IsFr8OwnTerminal = 1
-";
+	WHERE CHARINDEX (':', REVERSE ([Endpoint])) <= 6 AND IsFr8OwnTerminal = 1"
+
+if ($localhostOnly)
+{
+	$commandText += " AND [Endpoint] LIKE 'http://localhost:%'" 
+}
 
 Write-Host $commandText
 
