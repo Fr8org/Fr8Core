a844669d2dfbcddde3e7ab30d0adae3a5b7b1370
makigjuro <maki.gjuroski@gmail.com>
2016-07-19 13:50:56 +0200

    move count of objects to existing crate manifest

--- terminalSalesforce/Services/SalesforceManager.cs
+++ terminalSalesforce/Services/SalesforceManager.cs
@@ -308,19 +308,33 @@ namespace terminalSalesforce.Services
             {
                 parsedObjects = queryResult.Records.Select(record => ((JObject)record)).ToList();
             }
+        
+            var countOfObjectTableCell = new TableCellDTO()
+            {
+                Cell = new KeyValueDTO()
+                {
+                    Key = "Count of Objects",
+                    Value = queryResult.Records.Count.ToString()
+                }
+            };
+
+            List<TableRowDTO> list = new List<TableRowDTO>();
+            foreach (var row in parsedObjects.Select(parsedObject => parsedObject.Properties()
+                .Where(y => y.Value.Type == JTokenType.String && !string.IsNullOrEmpty(y.Value.Value<string>()))
+                .Select(y => new TableCellDTO
+            {
+                Cell = new KeyValueDTO
+                {
+                    Key = y.Name, Value = y.Value.Value<string>()
+                }
+            }).ToList()))
+            {
+                row.Add(countOfObjectTableCell);
+                list.Add(new TableRowDTO() { Row = row });
+            }
             return new StandardTableDataCM
             {
-                Table = parsedObjects.Select(x => x.Properties()
-                                            .Where(y => y.Value.Type == JTokenType.String && !string.IsNullOrEmpty(y.Value.Value<string>()))
-                                            .Select(y => new TableCellDTO
-                                            { Cell = new KeyValueDTO
-                                                {
-                                                    Key = y.Name,
-                                                    Value = y.Value.Value<string>()
-                                                }
-                                            }))
-                                     .Select(x => new TableRowDTO { Row = x.ToList() })
-                                     .ToList()
+                Table = list
             };
         }
 
