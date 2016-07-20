b22235210821c025b951fbc01210e715563a5ec4
Alp Gürtan <alpgurtan@gmail.com>
2016-07-20 10:54:47 +0300

    Enums are added both front-end and back-end

--- Scripts/app/enums/NotificationType.ts
+++ Scripts/app/enums/NotificationType.ts
@@ -1,12 +1,10 @@
-﻿namespace Fr8.Infrastructure.Data.Constants
-{
-    // Don't forget to add corresponding type to NotificationType.ts
-    public enum NotificationType
-    {
+﻿module dockyard.enums {
+    // Don't forget to add corresponding type to NotificationType.cs
+    export enum NotificationType {
         GenericSuccess = 1,     // fr8pusher_generic_success
         GenericFailure = 2,     // fr8pusher_generic_failure
         GenericInfo = 3,        // fr8pusher_activity_execution_info
         TerminalEvent = 4,      // fr8pusher_terminal_event
         ExecutionStopped = 5
-    }
+    };
 }
