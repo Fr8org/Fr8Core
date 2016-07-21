d2a9d716655ceb9e992460f4483fa8966a6e27e5
Alp Gürtan <alpgurtan@gmail.com>
2016-07-21 17:03:16 +0300

    Toast messages are added fully and binded

--- Scripts/app/services/UINotificationService.ts
+++ Scripts/app/services/UINotificationService.ts
@@ -7,37 +7,50 @@
 module dockyard.services {
 
     export interface IUINotificationService {
-        notify: (title: string, message: string, status: dockyard.enums.UINotificationStatus, options: any) => void
+        notify: (message: string, status: dockyard.enums.UINotificationStatus, options: any) => void
     }
 
     declare var noty: any;
 
     class UINotificationService implements IUINotificationService {
-        constructor(private noty: any) {
-        }
+        public notify(message: string, status: dockyard.enums.UINotificationStatus, options: any) {
+            // For more options please look at noty library
+            if (!options) {
+                options = {
+                    layout: 'topCenter',    // 'bottom', 'bottomCenter', 'bottomLeft', 'bottomRight', 'center', 'centerLeft', centerRight'
+                                            // 'inline', 'top', 'topCenter', 'topLeft', 'topRight'
+                    theme: 'relax',         // 'default' or 'relax'
+                    dismissQueue: true,     // If you want to use queue feature set this true
+                    timeout: 5000,          // delay for closing event. Set false for sticky notifications
+                    force: false,           // adds notification to the beginning of queue when set to true
+                    maxVisible: 5,          // you can set max visible notification for dismissQueue true option,
+                    killer: false,          // for close all notifications before show
+                    closeWith: ['click'],   // ['click', 'button', 'hover', 'backdrop'] // backdrop click will close all notifications
+                }
+            }
 
-        public notify(title: string, message: string, status: dockyard.enums.UINotificationStatus, options: any) {
             // Determines notification type and add necessary attributes
+            options['text'] = message;
             switch (status) {
                 case dockyard.enums.UINotificationStatus.Success:
-                    var n = noty({
-                        text: message
-                        });
+                    options['type'] = 'success';
                     break;
                 case dockyard.enums.UINotificationStatus.Info:
-
+                    options['type'] = 'information';
                     break;
                 case dockyard.enums.UINotificationStatus.Warning:
-
+                    options['type'] = 'warning';
                     break;
                 case dockyard.enums.UINotificationStatus.Error:
-
+                    options['type'] = 'error';
                     break;
                 case dockyard.enums.UINotificationStatus.Alert:
+                    options['type'] = 'alert';
                     break
             }
+            noty(options);
         }
     }
 
-    //app.factory('UINotificationService', [IUINotificationService => new UINotificationService()]);
+    app.factory('UINotificationService', [IUINotificationService => new UINotificationService()]);
 }
--- gulpfile.js
+++ gulpfile.js
@@ -406,7 +406,7 @@ gulp.task('cdnizer-js', ['bower'], function () {
                 cdn: '//ajax.googleapis.com/ajax/libs/angular_material/${ version }/angular-material.min.js'
             },
             {
-                file: '~/bower_components/noty/js/noty/packaged/jquery.noty.packaged.min',
+                file: '~/bower_components/noty/js/noty/packaged/jquery.noty.packaged.min.js',
                 package: 'noty',
                 cdn: '//cdnjs.cloudflare.com/ajax/libs/jquery-noty/2.3.8/jquery.noty.min.js'
             }
