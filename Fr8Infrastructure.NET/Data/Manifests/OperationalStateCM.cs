117540501cef95e39e156b3fb6aa0d9606d3ff17
blazingmind <blaizingmind@gmail.com>
2016-07-22 01:01:05 +0300

    removed redundant properties from OperationalStateCM

--- Fr8Infrastructure.NET/Data/Manifests/OperationalStateCM.cs
+++ Fr8Infrastructure.NET/Data/Manifests/OperationalStateCM.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections.Generic;
 using Fr8.Infrastructure.Data.Constants;
 using Fr8.Infrastructure.Data.DataTransferObjects;
+using Newtonsoft.Json;
 
 namespace Fr8.Infrastructure.Data.Manifests
 {
     public partial class OperationalStateCM : Manifest
     {
         public ActivityCallStack CallStack { get; set; } = new ActivityCallStack();
-
         public List<HistoryElement> History { get; set; }
-        public ActivityErrorCode? CurrentActivityErrorCode { get; set; }
-        public string CurrentActivityErrorMessage { get; set; }
-        public string CurrentClientActivityName { get; set; }
+
+        //This property should not be persisted and should not be exposed outside the Hub
+        [JsonIgnore]
+        [ManifestField(IsHidden = true)]
         public StackLocalData BypassData { get; set; }
 
         public ActivityResponseDTO CurrentActivityResponse { get; set; }
