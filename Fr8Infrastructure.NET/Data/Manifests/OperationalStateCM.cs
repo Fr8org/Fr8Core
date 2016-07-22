66331e50a6b91d9df91419ba5a5791c64506a627
blazingmind <blaizingmind@gmail.com>
2016-07-22 23:25:26 +0300

    Removed illogical BypassData from OperationalStateCM

--- Fr8Infrastructure.NET/Data/Manifests/OperationalStateCM.cs
+++ Fr8Infrastructure.NET/Data/Manifests/OperationalStateCM.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using Fr8.Infrastructure.Data.Constants;
 using Fr8.Infrastructure.Data.DataTransferObjects;
-using Newtonsoft.Json;
 
 namespace Fr8.Infrastructure.Data.Manifests
 {
@@ -9,12 +8,6 @@ namespace Fr8.Infrastructure.Data.Manifests
     {
         public ActivityCallStack CallStack { get; set; } = new ActivityCallStack();
         public List<HistoryElement> History { get; set; }
-
-        //This property should not be persisted and should not be exposed outside the Hub
-        [JsonIgnore]
-        [ManifestField(IsHidden = true)]
-        public StackLocalData BypassData { get; set; }
-
         public ActivityResponseDTO CurrentActivityResponse { get; set; }
 
         public OperationalStateCM()
