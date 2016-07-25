8bbdc3e0d4219cde7a8168df412c329ce425dc38
Anton Yakovets <jokerosky@gmail.com>
2016-07-25 18:11:00 +0600

    FR-4763 Asana SDK unit tests

--- Tests/terminalAsanaTests/Fixtures/FixtureData.cs
+++ Tests/terminalAsanaTests/Fixtures/FixtureData.cs
@@ -3,12 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fr8.Infrastructure.Data.DataTransferObjects;
 using Fr8.TerminalBase.Models;
 
 namespace terminalAsanaTests.Fixtures
 {
     public static partial class FixtureData
     {
+        public static string SampleTasksDataResponse()
+        {
+            return @"{
+                      ""data"": [{
+                        ""id"": 145598938536201,
+                        ""name"": ""Asana Fr8 Dev"",
+                      }]
+                    }";
+        }
+
+        public static string SampleUsersDataResponse()
+        {
+            return @"{
+                      ""data"": [{
+                        ""id"": 145598938536201,
+                        ""name"": ""Asana Fr8 Dev"",
+                        ""email"": ""asana-dev@fr8.co""
+                      }]
+                    }";
+        }
+
         public static string SampleAsanaOauthTokenResponse()
         {
             return @"{
@@ -30,8 +52,37 @@ namespace terminalAsanaTests.Fixtures
             {
                 Token = SampleAsanaOauthTokenResponse(),
                 ExternalAccountId = "145598938536201",
-                AdditionalAttributes = DateTime.UtcNow.AddSeconds(3600).ToString("O")
+                AdditionalAttributes = DateTime.UtcNow.AddSeconds(3600).ToString("O"),
+                ExpiresAt = DateTime.UtcNow.AddSeconds(3600)
             };
+        }
+
+        public static Fr8DataDTO Get_Tasks_v1_InitialConfiguration_Fr8DataDTO()
+        {
+            var token = SampleAuthorizationToken();
+
+            var activityTemplate = new ActivityTemplateDTO()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Get_Tasks_TEST",
+                Version = "1"
+            };
+
+            var activityDTO = new ActivityDTO()
+            {
+                Id = Guid.NewGuid(),
+                Label = "Post To Timeline",
+                ActivityTemplate = activityTemplate,
+                AuthToken = new AuthorizationTokenDTO()
+                {
+                    Token = token.Token,
+                    ExpiresAt = token.ExpiresAt,
+                    ExternalAccountId = token.ExternalAccountId,
+                    ExternalAccountName = "Asana Fr8 Dev"
+                }
+            };
+
+            return new Fr8DataDTO { ActivityDTO = activityDTO };
         }
     }
 }
---
+++ Tests/terminalAsanaTests/Integration/Get_Taks_v1_Tests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fr8.Infrastructure.Data.Constants;
+using Fr8.Infrastructure.Data.Control;
+using Fr8.Infrastructure.Data.Crates;
+using Fr8.Infrastructure.Data.DataTransferObjects;
+using Fr8.Infrastructure.Data.Manifests;
+using Fr8.Testing.Integration;
+using NUnit.Framework;
+
+namespace terminalAsanaTests.Integration
+{
+    class Get_Taks_v1_Tests: BaseTerminalIntegrationTest
+    {
+        public override string TerminalName => "terminalAsana";
+
+        private void AssertConfigurationControls(StandardConfigurationControlsCM control)
+        {
+            Assert.AreEqual(4, control.Controls.Count, "Control count is not 4");
+            Assert.IsTrue(control.Controls.Select(x=> x is DropDownList).Count() == 3);
+            Assert.IsTrue(control.Controls.Select(x => x is TextBlock).Count() == 1);
+        }
+
+
+        /// <summary>
+        /// Validate correct crate-storage structure in initial configuration response.
+        /// </summary>
+        [Test]
+        public async Task Get_Taks_v1_initial_configuratino_check()
+        {
+            var configureUrl = GetTerminalConfigureUrl();
+            var requestDataDTO = Fixtures.FixtureData.Get_Tasks_v1_InitialConfiguration_Fr8DataDTO();
+            var responseDTO = await HttpPostAsync<Fr8DataDTO, ActivityDTO>(configureUrl, requestDataDTO);
+            var crateStorage = Crate.FromDto(responseDTO.CrateStorage);
+
+            AssertConfigurationControls(crateStorage.CrateContentsOfType<StandardConfigurationControlsCM>().Single());
+
+            var fieldsToPayload = crateStorage.CratesOfType<CrateDescriptionCM>().ToList();
+
+            Assert.AreEqual(2,fieldsToPayload.Count);
+
+            Assert.IsTrue(fieldsToPayload.Where(x=> x.ManifestType == CrateManifestType.FromEnum(MT.AsanaTaskList)).Count() == 1);
+            Assert.IsTrue(fieldsToPayload.Where(x => x.ManifestType == CrateManifestType.FromEnum(MT.StandardTableData)).Count() == 1);
+        }
+
+
+        /// <summary>
+        /// Validate correct crate-storage structure in followup configuration response.
+        /// </summary>
+        [Test]
+        public async Task Get_Taks_v1_FollowUp_Configuration_Check_Crate_Structure()
+        {
+            //var configureUrl = GetTerminalConfigureUrl();
+            //var responseDTO = await CompleteInitialConfiguration();
+            //responseDTO.AuthToken = FixtureData.Facebook_AuthToken();
+            //var dataDTO = new Fr8DataDTO
+            //{
+            //    ActivityDTO = responseDTO
+            //};
+            ////nothing should change on followup
+            //responseDTO = await HttpPostAsync<Fr8DataDTO, ActivityDTO>(configureUrl, dataDTO);
+            //AssertInitialConfigurationResponse(responseDTO);
+        }
+    }
+}
