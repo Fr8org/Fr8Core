d47158008c1048b66443f00aa2584a33edd3470e
Bahadır Bozdağ <bahadir.bb@gmail.com>
2016-07-28 13:10:28 +0300

    Removed terminalDemo related stuff

--- terminalBasecamp2/Properties/AssemblyInfo.cs
+++ terminalBasecamp2/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("terminalDemo")]
+[assembly: AssemblyTitle("terminalBasecamp2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("terminalDemo")]
+[assembly: AssemblyProduct("terminalBasecamp2")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
