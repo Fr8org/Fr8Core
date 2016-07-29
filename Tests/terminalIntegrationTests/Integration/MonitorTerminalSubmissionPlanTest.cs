bc663826f9613e73baaa27285753b37a40395e8e
jarekrzdbk <jarekrzdbk@gmail.com>
2016-07-29 19:07:08 +0300

    temporary remove Ignore attribute from test

--- Tests/terminalIntegrationTests/Integration/MonitorTerminalSubmissionPlanTest.cs
+++ Tests/terminalIntegrationTests/Integration/MonitorTerminalSubmissionPlanTest.cs
@@ -78,7 +78,7 @@ namespace terminalIntegrationTests.Integration
 
 
 
-        [Test, Ignore("Is being resolved with 4692")]
+        [Test]
         public async Task MonitorTerminalSubmissionPlan()
         {
             AutoMapperBootstrapper.ConfigureAutoMapper();
