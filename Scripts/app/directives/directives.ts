e13e7603cd355fca73d7f37f3c71c8b525f13624
David Vanderlip <david.vand1115@yahoo.com>
2016-07-31 02:51:00 +0300

    Fixed FR-3531

--- Scripts/app/directives/directives.ts
+++ Scripts/app/directives/directives.ts
@@ -367,7 +367,7 @@ app.directive('eventPlandashboard', ['$timeout', '$window', function ($timeout, 
     };
 }]);
 
-//== scroll grey area of PB vertically and horizontally
+//== scroll grey area of PB vertically and horizontally 
 app.directive('pbScrollPane', ['$timeout', '$window', function ($timeout, $window) {
     return {
         restrict: 'A',
