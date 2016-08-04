b2fa4204dfe9218f170109573ee4819f0d09a5e8
emrevarisli <emrevarisli@gmail.com>
2016-08-04 16:37:03 +0300

    salesforce.spec.js added
    salesforce.spec.js added

---
+++ Scripts/tests/e2e/authorizationPathways/salesforce.spec.js
@@ -0,0 +1,40 @@
+﻿var PlansPage = require('../pages/plans.page.js');
+var ManageAuthTokens = require('../shared/manageAuthTokens.js');
+var AccountHelper = require('../shared/accountHelper.js');
+var MyAccountPage = require('../pages/myAccount.page.js');
+var UIHelpers = require('../shared/uiHelpers.js');
+
+escribe('SalesForce Authorization pathway test', function () {
+    var plansPage = new PlansPage();
+    var manageAuthTokens = new ManageAuthTokens();
+    var accountHelper = new AccountHelper();
+    var myAccountPage = new MyAccountPage();
+    var uiHelpers = new UIHelpers();
+
+    it('should login', function () {
+        return accountHelper.login().then(function () {
+            plansPage.setEmail(browser.params.username);
+            plansPage.setPassword(browser.params.password);
+            return plansPage.login().click().then(function () {
+            });
+        });
+    });
+
+    it('should go to myAccount page', function () {
+        return myAccountPage.get();
+        expect(browser.getCurrentUrl()).toEqual('http://dev.fr8.co/dashboard/myaccount');
+    });
+
+    it('should add plan', function () {
+        return plansPage.addPlan().then(function () {
+            return plansPage.addActivity();
+        });
+    });
+
+    it('should add salesForce activity', function () {
+        return plansPage.addSalesForceActivity().then(function () {
+            return plansPage.getSalesForceGetDataActivity();
+        });
+    });
+
+});
--- Scripts/tests/e2e/pages/plans.page.js
+++ Scripts/tests/e2e/pages/plans.page.js
@@ -2,18 +2,24 @@
 
 var PlansPage = function () {
 
-    //Login Properties
+    /* Login Properties */
     var emailInput = element(by.id('Email'));
     var passwordInput = element(by.id('Password'));
     var loginButton = element(by.xpath('//*[@id="loginform"]/form/div[2]/div/div/button'));
 
-    //Add Google Activity Properties
+    /* General Properties */
+    var uiHelpers = new UIHelpers();
     var addPlanButton = element(by.xpath('//*[@id="Myfr8lines"]/h3/a'));
     var addActivityButton = element(by.className('action-add-button-link'));
+
+    /* Add Google Activity Properties */
     var googleActivityButton = element(by.xpath('/html/body/div[2]/div[2]/div/div/div/div/div[1]/div[1]/div[1]/action-picker-panel/div/div[2]/div/div[13]/div[1]/img'));
     var googleSheetActivityButton = element(by.xpath('/html/body/div[2]/div[2]/div/div/div/div/div[1]/div[1]/div[1]/action-picker-panel/div/div[2]/div/ul/li[1]/a/span'));
 
-    var uiHelpers = new UIHelpers();
+    /* SalesForce Activity Properties */
+    var salesForceActivityButton = element(by.xpath('/html/body/div[2]/div[2]/div/div/div/div/div[1]/div[1]/div[1]/action-picker-panel/div/div[2]/div/div[22]/div[1]/img'));
+    var salesForceGetDataActivityButton = element(by.xpath('/html/body/div[2]/div[2]/div/div/div/div/div[1]/div[1]/div[1]/action-picker-panel/div/div[2]/div/ul/li[1]/a/span'));
+
 
     this.get = function () {
         browser.ignoreSynchronization = true;
@@ -53,6 +59,18 @@ var PlansPage = function () {
     this.getGoogleSheetActivity = function () {
         return uiHelpers.waitReady(googleSheetActivityButton).then(function () {
             return googleSheetActivityButton.click();
+        });
+    };
+
+    this.addSalesForceActivity = function () {
+        return uiHelpers.waitReady(salesForceActivityButton).then(function () {
+            return salesForceActivityButton.click();
+        });
+    };
+
+    this.getSalesForceGetDataActivity = function () {
+        return uiHelpers.waitReady(salesForceGetDataActivityButton).then(function () {
+            return salesForceGetDataActivityButton.click();
         });
     };
 
