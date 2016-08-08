01f48e40ce9f5efbf9d05d3b35c9ca99b05279d2
Andrei Chaplygin <demarsch@yandex.ru>
2016-08-09 01:31:57 +0200

    Added missing sample data factories and created int test for Swagger

---
+++ Documentation/Swagger/SampleData/ActivityTemplateSummarySampleFactory.cs
@@ -0,0 +1,23 @@
+﻿using Fr8.Infrastructure.Data.DataTransferObjects;
+
+namespace HubWeb.Documentation.Swagger.SampleData
+{
+    public class ActivityTemplateSummarySampleFactory : ISwaggerSampleFactory<ActivityTemplateSummaryDTO>
+    {
+        public ActivityTemplateSummaryDTO GetSampleData()
+        {
+            return new ActivityTemplateSummaryDTO
+            {
+                Version = "1",
+                Name = "Build_Message",
+                TerminalName = "terminalFr8Core",
+                TerminalVersion = "1"
+            };
+        }
+
+        object ISwaggerSampleFactory.GetSampleData()
+        {
+            return GetSampleData();
+        }
+    }
+}
---
+++ Documentation/Swagger/SampleData/TerminalSummarySampleFactory.cs
@@ -0,0 +1,21 @@
+﻿using Fr8.Infrastructure.Data.DataTransferObjects;
+
+namespace HubWeb.Documentation.Swagger.SampleData
+{
+    public class TerminalSummarySampleFactory : ISwaggerSampleFactory<TerminalSummaryDTO>
+    {
+        public TerminalSummaryDTO GetSampleData()
+        {
+            return new TerminalSummaryDTO
+            {
+                Version = "1",
+                Name = "terminalFr8Core"
+            };
+        }
+
+        object ISwaggerSampleFactory.GetSampleData()
+        {
+            return GetSampleData();
+        }
+    }
+}
