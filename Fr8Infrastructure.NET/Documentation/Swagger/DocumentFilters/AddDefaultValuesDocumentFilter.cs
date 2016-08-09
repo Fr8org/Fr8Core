6b43c7c3529392f8584a0b3ce4ee00fb1907a962
Andrei Chaplygin <demarsch@yandex.ru>
2016-08-09 17:29:38 +0200

    Moved swagger sample factories into Fr8Infrastructure.NET in order to make them available for terminals too

---
+++ Fr8Infrastructure.NET/Documentation/Swagger/DocumentFilters/AddDefaultValuesDocumentFilter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Description;
+using StructureMap;
+using StructureMap.Pipeline;
+using Swashbuckle.Swagger;
+
+namespace Fr8.Infrastructure.Documentation.Swagger
+{
+    //This class produces sample data for complex objects to be displayed in Swagger UI
+    //In order to provide sample value for your type just add a class that implements ISwaggerSampleFactory<YourType> 
+    //You can rely on constructor injection of other implentations in order to reuse default values for those type
+    //But make sure first that such class doesn't already exist. See examples in Documentation/Swagger/SampleData folder
+    public class AddDefaultValuesDocumentFilter : IDocumentFilter
+    {
+        private static readonly IContainer _container;
+
+        private static readonly Dictionary<string, Type> _defaultFactoriesByTypeName;
+
+        static AddDefaultValuesDocumentFilter()
+        {
+            _container = new Container();
+            _defaultFactoriesByTypeName = new Dictionary<string, Type>();
+        }
+        public AddDefaultValuesDocumentFilter()
+        {
+            var thisAssembly = GetType().Assembly;
+            var defaultValueFactories = thisAssembly.GetTypes()
+                .Where(x => x.IsClass)
+                .Select(x => new {Type = x, Interface = x.GetInterface("ISwaggerSampleFactory`1")})
+                .Where(x => x.Interface != null)
+                .ToArray();
+            _container.Configure(x =>
+            {
+                foreach (var defaultValueFactory in defaultValueFactories)
+                {
+                    x.For(defaultValueFactory.Interface, new SingletonLifecycle()).Use(defaultValueFactory.Type);
+                }
+            });
+            foreach (var defaultValueFactory in defaultValueFactories)
+            {
+                _defaultFactoriesByTypeName.Add(GetSwaggerTypeName(defaultValueFactory.Interface), defaultValueFactory.Interface);
+            }
+        }
+
+        private string GetSwaggerTypeName(Type @interface)
+        {
+            var genericType = @interface.GetGenericArguments()[0];
+            if (genericType.IsGenericType)
+            {
+                var result = $"{genericType.Name.Remove(genericType.Name.IndexOf('`'))}[{string.Join(",", genericType.GenericTypeArguments.Select(x => x.Name))}]";
+                return result;
+            }
+            return genericType.Name;
+        }
+
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            foreach (var schema in schemaRegistry.Definitions)
+            {
+                Type defaultFactoryType;
+                if (_defaultFactoriesByTypeName.TryGetValue(schema.Key, out defaultFactoryType))
+                {
+                    schema.Value.example = (_container.GetInstance(defaultFactoryType) as ISwaggerSampleFactory).GetSampleData();
+                }
+            }
+        }
+    }
+}
---
+++ Fr8Infrastructure.NET/Documentation/Swagger/SampleData/ActivityTemplateSampleFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Fr8.Infrastructure.Data.DataTransferObjects;
+
+namespace Fr8.Infrastructure.Documentation.Swagger
+{
+    public class ActivityTemplateSampleFactory : ISwaggerSampleFactory<ActivityTemplateDTO>
+    {
+        private readonly ISwaggerSampleFactory<TerminalDTO> _terminalSampleFactory;
+
+        public ActivityTemplateSampleFactory(ISwaggerSampleFactory<TerminalDTO> terminalSampleFactory)
+        {
+            _terminalSampleFactory = terminalSampleFactory;
+        }
+
+        public ActivityTemplateDTO GetSampleData()
+        {
+            return new ActivityTemplateDTO
+            {
+                Id = Guid.Parse("D64F823D-E127-41E5-A3E5-0D48BA2750DB"),
+                Name = "Build_Message",
+                Label = "Build a Message",
+                Version = "1",
+                MinPaneWidth = 330,
+                Terminal = _terminalSampleFactory.GetSampleData(),
+                Tags = string.Empty
+            };
+        }
+
+        object ISwaggerSampleFactory.GetSampleData()
+        {
+            return GetSampleData();
+        }
+    }
+}
