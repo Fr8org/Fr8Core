97b75e07b2d83a703796b1e7cce02304d2ca1392
Andrei Chaplygin <demarsch@yandex.ru>
2016-08-09 17:29:38 +0200

    Moved swagger sample factories into Fr8Infrastructure.NET in order to make them available for terminals too

---
+++ Fr8Infrastructure.NET/Documentation/Swagger/SampleData/AuthenticationTokenGrantSampleFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Fr8.Infrastructure.Data.DataTransferObjects;
+
+namespace Fr8.Infrastructure.Documentation.Swagger
+{
+    public class AuthenticationTokenGrantSampleFactory : ISwaggerSampleFactory<AuthenticationTokenGrantDTO>
+    {
+        public AuthenticationTokenGrantDTO GetSampleData()
+        {
+            return new AuthenticationTokenGrantDTO
+            {
+                IsMain = true,
+                AuthTokenId = Guid.Parse("2FF82BC0-105F-419C-A3EE-D05C37B5D8C1"),
+                ActivityId = Guid.Parse("157A2147-5F29-46A1-BAAD-BA1032655ECF")
+            };
+        }
+
+        object ISwaggerSampleFactory.GetSampleData()
+        {
+            return GetSampleData();
+        }
+    }
+}
