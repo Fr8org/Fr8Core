f9c95c0afe9d76b0d1c03511203d0cf78b417e6e
Alp Gürtan <alpgurtan@gmail.com>
2016-08-09 10:21:39 +0300

    Fixing dev build

--- _PowerShellScripts/CleanUpAfterTests.ps1
+++ _PowerShellScripts/CleanUpAfterTests.ps1
@@ -69,67 +69,7 @@ $command.CommandTimeout = 300 #5 minutes
 $command.ExecuteNonQuery()
 
 Write-Host "Removing all data created during registration testing."
-$commandText = "
-IF object_id('tempdb..#TempUsers') IS NOT NULL
-	DROP TABLE #TempUsers;
-
-IF object_id('tempdb..#Nodes2') IS NOT NULL
-	DROP TABLE #Nodes2;
-
-WITH TestUser (UserId) AS (
-	SELECT Users.Id 
-	FROM Users
-	WHERE Users.EmailAddressID IN (	
-		SELECT EmailAddresses.Id
-		FROM EmailAddresses
-		WHERE EmailAddresses.Address LIKE 'testuser_registration@fr8.co')
-)
-SELECT DISTINCT (TestUser.UserId) INTO #TempUsers FROM TestUser;
-
-WITH NodeTree AS (
-	SELECT seedNodes.Id
-	FROM PlanNodes seedNodes INNER JOIN #TempUsers tu ON tu.UserId = seedNodes.Fr8AccountId
-)
-SELECT DISTINCT (NodeTree.Id) INTO #Nodes2 FROM NodeTree;
-
-UPDATE derived SET derived.ParentPlanNodeId = null, derived.RootPlanNodeId = null FROM PlanNodes derived INNER JOIN #Nodes2 n ON n.Id = derived.Id;
-
-DELETE derived FROM ObjectRolePermissions derived
-INNER JOIN #Nodes2 n ON CONVERT(nvarchar(50), n.Id) = derived.ObjectId
-WHERE derived.[Type] <> 'Plan Template';
-
-DELETE h FROM History h 
-INNER JOIN Containers AS derived ON CONVERT(nvarchar(50), derived.Id) = h.ObjectId
-INNER JOIN #Nodes2 n ON n.Id = derived.PlanId;
-
-DELETE derived FROM Containers derived INNER JOIN #Nodes2 n ON n.Id = derived.PlanId;
-
-DELETE derived FROM Plans derived INNER JOIN #Nodes2 n ON n.Id = derived.Id;
-
-DELETE derived FROM SubPlans derived INNER JOIN #Nodes2 n ON n.Id = derived.Id;
-
-DELETE derived FROM Actions derived INNER JOIN #Nodes2 n ON n.Id = derived.Id;
-
-DELETE derived FROM PlanNodes derived INNER JOIN #Nodes2 n ON n.Id = derived.Id;
-
-DELETE derived FROM ObjectRolePermissions derived INNER JOIN #TempUsers tu ON tu.UserId = derived.Fr8AccountId;
-
-DELETE derived FROM MTData derived INNER JOIN #TempUsers tu ON tu.UserId = derived.fr8AccountId;
-
-DELETE derived FROM History derived INNER JOIN #TempUsers tu ON tu.UserId = derived.Fr8UserId OR tu.UserId = derived.CreatedByID;
-
-DELETE derived FROM AuthorizationTokens derived INNER JOIN #TempUsers tu ON tu.UserId = derived.UserID;
-
-DELETE derived FROM AspNetUserRoles derived INNER JOIN #TempUsers tu ON tu.UserId = derived.UserId;
-
-DELETE derived FROM Users derived INNER JOIN #TempUsers tu ON tu.UserId = derived.Id;
-
-DELETE FROM EmailAddresses WHERE EmailAddresses.Address LIKE 'testuser_%@fr8.co';
-
-DELETE derived FROM AspNetUsers derived INNER JOIN #TempUsers tu ON tu.UserId = derived.Id;
-
-DROP TABLE #Nodes2;
-DROP TABLE #TempUsers;"
+$commandText = ""
 
 $command = new-object system.data.sqlclient.sqlcommand($commandText, $connection)
 $command.CommandTimeout = 300 #5 minutes
