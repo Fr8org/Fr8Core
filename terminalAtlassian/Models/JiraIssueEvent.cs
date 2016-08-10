148a6a0207ac90175c2a052f2717d7047928db24
Ömer Hancı <hanciomer@gmail.com>
2016-08-10 13:37:26 +0300

    WebHook is created

---
+++ terminalAtlassian/Models/JiraIssueEvent.cs
@@ -0,0 +1,41 @@
+﻿using Atlassian.Jira;
+using terminalAtlassian.Interfaces;
+
+namespace terminalAtlassian.Models
+{
+    public class JiraIssueEvent
+    {
+        public string Timestamp { get; set; }
+
+        public string WebhookEvent { get; set; }
+
+        public string IssueEventTypeName { get; set; }
+
+        public JiraUser User { get; set; }
+
+        public JiraIssue Issue { get; set; }
+    }
+    public class JiraIssue
+    {
+
+        public string Id { get; set; }
+
+        public string Self { get; set; }
+
+        public string Key { get; set; }
+
+        public IssueFields Fields { get; set; }
+    }
+    public class IssueFields
+    {
+        public IssueType IssueType { get; set; }
+
+        public Project Project { get; set; }
+
+        public IssueResolution Resolution { get; set; }
+
+        public IssuePriority Priority { get; set; }
+
+        public IssueLabels Labels { get; set; }
+    }
+}
