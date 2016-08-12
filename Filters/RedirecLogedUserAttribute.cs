e9d3a958e177c11a54f1564dd2390b862226313a
Yakov Gnusin <y.gnusin@gmail.com>
2016-08-12 10:01:40 +0700

    Fixed Register button.

--- Filters/RedirecLogedUserAttribute.cs
+++ Filters/RedirecLogedUserAttribute.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
+using System.Security.Claims;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -13,6 +11,9 @@ namespace HubWeb.Filters
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             base.OnActionExecuting(filterContext);
+
+            var claimsIdentity = (ClaimsIdentity)filterContext.HttpContext.User.Identity;
+            var isGuest = claimsIdentity.Claims.Any(x => x.Type == ClaimTypes.Role && x.Value == "Guest");
 
             if (filterContext.HttpContext.User.Identity.IsAuthenticated)
             {
