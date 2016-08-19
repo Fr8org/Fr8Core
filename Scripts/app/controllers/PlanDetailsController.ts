91d0aa8e132db1cd0bfb8d33fbf4dbea65c31253
Alp Gürtan <alpgurtan@gmail.com>
2016-08-19 11:41:38 +0300

    Misspelled inject is fixed

--- Scripts/app/controllers/PlanDetailsController.ts
+++ Scripts/app/controllers/PlanDetailsController.ts
@@ -31,7 +31,7 @@ module dockyard.controllers {
             'PlanService',
             '$stateParams',
             '$filter',
-            'UINotificationSservice'
+            'UINotificationService'
         ];
 
         constructor(
