72aec439a06c86f3fa8b7f0b1d9f29a5574e7788
Alexei Avrutin <alexavrutin@gmail.com>
2016-10-20 12:29:24 +0200

    Int tests rescheduled to run once in 3 hours, reduced retention period for logs in the DB.

--- SQL scripts/DeleteOldLogs.sql
+++ SQL scripts/DeleteOldLogs.sql
@@ -1,12 +1,10 @@
 
 --Deleting logs older then two weeks
-DELETE FROM History  WHERE CreateDate < DATEADD(day, -14, GETDATE())
+DELETE FROM History  WHERE CreateDate < DATEADD(day, -3, GETDATE())
 GO
 
-DELETE FROM Containers  WHERE CreateDate < DATEADD(day, -14, GETDATE())
+DELETE FROM Containers  WHERE CreateDate < DATEADD(day, -3, GETDATE())
 GO
 
-  DELETE [MtData] FROM [MtData]  INNER JOIN [MtTypes] on [MtData].[Type] = [MtTypes].[id] Where [MtTypes].[ManifestId] = 37 and [MtData].[CreatedAt] < DATEADD(day, -14, GETDATE())
+  DELETE [MtData] FROM [MtData]  INNER JOIN [MtTypes] on [MtData].[Type] = [MtTypes].[id] Where [MtTypes].[ManifestId] = 37 and [MtData].[CreatedAt] < DATEADD(day, -3, GETDATE())
 GO
-
-select * from mttypes
